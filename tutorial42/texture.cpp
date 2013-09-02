831a95bd6446c78b13a1afc72f1033e81a648fad
Etay Meiri <etay.meiri@gmail.com>
2013-09-02 22:33:13 +0300

    Use GL_RGB for internal format instead of GL_RGBA

--- tutorial42/texture.cpp
+++ tutorial42/texture.cpp
@@ -40,7 +40,7 @@ bool Texture::Load()
 
     glGenTextures(1, &m_textureObj);
     glBindTexture(m_textureTarget, m_textureObj);
-    glTexImage2D(m_textureTarget, 0, GL_RGBA, m_pImage->columns(), m_pImage->rows(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_blob.data());
+    glTexImage2D(m_textureTarget, 0, GL_RGB, m_pImage->columns(), m_pImage->rows(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_blob.data());
     glTexParameterf(m_textureTarget, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
 	glTexParameterf(m_textureTarget, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 
