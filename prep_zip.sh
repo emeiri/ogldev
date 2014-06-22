0772f2894bd92bd46aec8a41fde6f21f4e6d17bf
Etay Meiri <etay.meiri@gmail.com>
2014-06-22 06:38:14 +0300

    cleanup

--- prep_zip.sh
+++ prep_zip.sh
@@ -4,7 +4,4 @@ rm -rf ogldev-source
 rm -f ogldev-source.zip
 git clone https://etay.meiri@code.google.com/p/ogldev/ ogldev-source
 rm -rf ogldev-source/.git
-rm -ff ogldev-source/tutorial43
 zip -r ogldev-source ogldev-source
-
-echo Done - no tutorial 43!!!
