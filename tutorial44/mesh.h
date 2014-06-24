bff0ebcef6a0c37759b4e61572d9bafe055e7db3
Etay Meiri <ogldev1@gmail.com>
2014-06-24 09:55:55 +0300

    initial commit of tutorial 44.

---
+++ tutorial44/mesh.h
@@ -0,0 +1,151 @@
+/*
+
+	Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MESH_H
+#define	MESH_H
+
+#include <map>
+#include <vector>
+#include <assert.h>
+#include <GL/glew.h>
+#include <Importer.hpp>      // C++ importer interface
+#include <scene.h>       // Output data structure
+#include <postprocess.h> // Post processing flags
+
+#include "ogldev_util.h"
+#include "math_3d.h"
+#include "ogldev_texture.h"
+
+using namespace std;
+
+class Mesh
+{
+public:
+    Mesh();
+
+    ~Mesh();
+
+    bool LoadMesh(const string& Filename);
+
+    void Render();
+	
+    uint NumBones() const
+    {
+        return m_NumBones;
+    }
+    
+    void BoneTransform(float TimeInSeconds, vector<Matrix4f>& Transforms);
+    
+private:
+    #define NUM_BONES_PER_VEREX 4
+
+    struct BoneInfo
+    {
+        Matrix4f BoneOffset;
+        Matrix4f FinalTransformation;        
+
+        BoneInfo()
+        {
+            BoneOffset.SetZero();
+            FinalTransformation.SetZero();            
+        }
+    };
+    
+    struct VertexBoneData
+    {        
+        uint IDs[NUM_BONES_PER_VEREX];
+        float Weights[NUM_BONES_PER_VEREX];
+
+        VertexBoneData()
+        {
+            Reset();
+        };
+        
+        void Reset()
+        {
+            ZERO_MEM(IDs);
+            ZERO_MEM(Weights);        
+        }
+        
+        void AddBoneData(uint BoneID, float Weight);
+    };
+
+    void CalcInterpolatedScaling(aiVector3D& Out, float AnimationTime, const aiNodeAnim* pNodeAnim);
+    void CalcInterpolatedRotation(aiQuaternion& Out, float AnimationTime, const aiNodeAnim* pNodeAnim);
+    void CalcInterpolatedPosition(aiVector3D& Out, float AnimationTime, const aiNodeAnim* pNodeAnim);    
+    uint FindScaling(float AnimationTime, const aiNodeAnim* pNodeAnim);
+    uint FindRotation(float AnimationTime, const aiNodeAnim* pNodeAnim);
+    uint FindPosition(float AnimationTime, const aiNodeAnim* pNodeAnim);
+    const aiNodeAnim* FindNodeAnim(const aiAnimation* pAnimation, const string NodeName);
+    void ReadNodeHeirarchy(float AnimationTime, const aiNode* pNode, const Matrix4f& ParentTransform);
+    bool InitFromScene(const aiScene* pScene, const string& Filename);
+    void InitMesh(uint MeshIndex,
+                  const aiMesh* paiMesh,
+                  vector<Vector3f>& Positions,
+                  vector<Vector3f>& Normals,
+                  vector<Vector2f>& TexCoords,
+                  vector<VertexBoneData>& Bones,
+                  vector<unsigned int>& Indices);
+    void LoadBones(uint MeshIndex, const aiMesh* paiMesh, vector<VertexBoneData>& Bones);
+    bool InitMaterials(const aiScene* pScene, const string& Filename);
+    void Clear();
+
+#define INVALID_MATERIAL 0xFFFFFFFF
+  
+enum VB_TYPES {
+    INDEX_BUFFER,
+    POS_VB,
+    NORMAL_VB,
+    TEXCOORD_VB,
+    BONE_VB,
+    NUM_VBs            
+};
+
+    GLuint m_VAO;
+    GLuint m_Buffers[NUM_VBs];
+
+    struct MeshEntry {
+        MeshEntry()
+        {
+            NumIndices    = 0;
+            BaseVertex    = 0;
+            BaseIndex     = 0;
+            MaterialIndex = INVALID_MATERIAL;
+        }
+        
+        unsigned int NumIndices;
+        unsigned int BaseVertex;
+        unsigned int BaseIndex;
+        unsigned int MaterialIndex;
+    };
+    
+    vector<MeshEntry> m_Entries;
+    vector<Texture*> m_Textures;
+     
+    map<string,uint> m_BoneMapping; // maps a bone name to its index
+    uint m_NumBones;
+    vector<BoneInfo> m_BoneInfo;
+    Matrix4f m_GlobalInverseTransform;
+    
+    const aiScene* m_pScene;
+    Assimp::Importer m_Importer;
+};
+
+
+#endif	/* MESH_H */
+
---
+++ tutorial44/nbproject/Makefile-Debug.mk
@@ -0,0 +1,136 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/camera.o \
+	${OBJECTDIR}/glut_backend.o \
+	${OBJECTDIR}/intermediate_buffer.o \
+	${OBJECTDIR}/math_3d.o \
+	${OBJECTDIR}/mesh.o \
+	${OBJECTDIR}/motion_blur_technique.o \
+	${OBJECTDIR}/pipeline.o \
+	${OBJECTDIR}/skinning_technique.o \
+	${OBJECTDIR}/tutorial41.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=`pkg-config --cflags glew ImageMagick++ assimp freetype2 fontconfig` 
+CXXFLAGS=`pkg-config --cflags glew ImageMagick++ assimp freetype2 fontconfig` 
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=-lglut ../Common/dist/Debug/GNU-Linux-x86/libcommon.a
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial41
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial41: ../Common/dist/Debug/GNU-Linux-x86/libcommon.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial41: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial41 ${OBJECTFILES} ${LDLIBSOPTIONS} `pkg-config --libs glew ImageMagick++ assimp freetype2 fontconfig gl`
+
+${OBJECTDIR}/camera.o: camera.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/camera.o camera.cpp
+
+${OBJECTDIR}/glut_backend.o: glut_backend.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/glut_backend.o glut_backend.cpp
+
+${OBJECTDIR}/intermediate_buffer.o: intermediate_buffer.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/intermediate_buffer.o intermediate_buffer.cpp
+
+${OBJECTDIR}/math_3d.o: math_3d.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/math_3d.o math_3d.cpp
+
+${OBJECTDIR}/mesh.o: mesh.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/mesh.o mesh.cpp
+
+${OBJECTDIR}/motion_blur_technique.o: motion_blur_technique.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/motion_blur_technique.o motion_blur_technique.cpp
+
+${OBJECTDIR}/pipeline.o: pipeline.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/pipeline.o pipeline.cpp
+
+${OBJECTDIR}/skinning_technique.o: skinning_technique.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/skinning_technique.o skinning_technique.cpp
+
+${OBJECTDIR}/tutorial41.o: tutorial41.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/tutorial41.o tutorial41.cpp
+
+# Subprojects
+.build-subprojects:
+	cd ../Common && ${MAKE}  -f Makefile CONF=Debug
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial41
+
+# Subprojects
+.clean-subprojects:
+	cd ../Common && ${MAKE}  -f Makefile CONF=Debug clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
