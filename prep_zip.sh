569a2ca161069056af85a8b82ef27be1b664c80b
Etay Meiri <etay.meiri@gmail.com>
2014-10-04 13:20:09 +0300

    Updates to publish 44

--- prep_zip.sh
+++ prep_zip.sh
@@ -4,4 +4,5 @@ rm -rf ogldev-source
 rm -f ogldev-source.zip
 git clone https://etay.meiri@code.google.com/p/ogldev/ ogldev-source
 rm -rf ogldev-source/.git
+rm -rf ogldev-source/web
 zip -r ogldev-source ogldev-source
