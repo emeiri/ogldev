ffcc89ff4990c38ce75e50c679efc2195fb7229b
Etay Meiri <etay.meiri@gmail.com>
2014-10-20 08:14:23 +0300

    Initial commit of tutorial 45

---
+++ tutorial45/engine_common.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   engine_common.h
+ * Author: cl
+ *
+ * Created on June 10, 2011, 8:38 PM
+ */
+
+#ifndef ENGINE_COMMON_H
+#define	ENGINE_COMMON_H
+
+#define COLOR_TEXTURE_UNIT              GL_TEXTURE0
+#define COLOR_TEXTURE_UNIT_INDEX        0
+#define SHADOW_TEXTURE_UNIT             GL_TEXTURE1
+#define SHADOW_TEXTURE_UNIT_INDEX       1
+#define NORMAL_TEXTURE_UNIT             GL_TEXTURE2
+#define NORMAL_TEXTURE_UNIT_INDEX       2
+#define RANDOM_TEXTURE_UNIT             GL_TEXTURE3
+#define RANDOM_TEXTURE_UNIT_INDEX       3
+#define DISPLACEMENT_TEXTURE_UNIT       GL_TEXTURE4
+#define DISPLACEMENT_TEXTURE_UNIT_INDEX 4
+
+
+
+#endif	/* ENGINE_COMMON_H */
+
