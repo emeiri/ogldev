46fcef6bd05898ebf5fbe6a8c98ff79df1747b5d
Etay Meiri <etay.meiri@gmail.com>
2014-11-29 11:12:52 +0200

    First SSAO working version

--- tutorial45/shaders/lighting.fs
+++ tutorial45/shaders/lighting.fs
@@ -1,24 +1,39 @@
-#version 330                                                                        
-                                                                                                                                                                      
-                                                                                    
-out vec4 FragColor;                                                                 
-                                                                                    
-                                                                                            
+#version 330
+
+out vec4 FragColor;
+
 uniform sampler2D gPositionMap;
 uniform sampler2D gNormalMap;
-uniform vec2 gScreenSize;                                                                                            
-                                                                                            
+uniform vec2 gScreenSize;
+
 vec2 CalcTexCoord()
 {
     return gl_FragCoord.xy / gScreenSize;
 }
-                                                                             
-                                                                                            
-void main()                                                                                 
-{                                                                                           
+
+
+float CalcAmbientOcclusion(vec2 TexCoord, vec3 WorldPos, vec3 Normal)
+{
+    vec3 OccludeePos = texture(gPositionMap, TexCoord).xyz;
+    vec3 v = OccludeePos - WorldPos;
+    float distance = length(v);
+    return max(0.0, dot(Normal, v) * 1.0/(1.0 + distance));
+}
+
+void main()
+{
     vec2 TexCoord = CalcTexCoord();
     vec3 WorldPos = texture(gPositionMap, TexCoord).xyz;
     vec3 Normal = texture(gNormalMap, TexCoord).xyz;
-                                                                                            
-    FragColor = vec4(WorldPos, 1.0) + vec4(Normal, 1.0);
+
+    float AO = 0.0;
+
+    AO += CalcAmbientOcclusion(TexCoord + vec2(-1.0, -1.0) * gScreenSize, WorldPos, Normal);
+    AO += CalcAmbientOcclusion(TexCoord + vec2(1.0, -1.0) * gScreenSize, WorldPos, Normal);
+    AO += CalcAmbientOcclusion(TexCoord + vec2(-1.0, 1.0) * gScreenSize, WorldPos, Normal);
+    AO += CalcAmbientOcclusion(TexCoord + vec2(1.0, 1.0) * gScreenSize, WorldPos, Normal);
+    
+    AO /= 4.0;
+
+    FragColor = vec4(AO);
 }
