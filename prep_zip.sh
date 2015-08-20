1b108fa757bb4fd233de6c5ede8a9568231c97fa
Etay Meiri <etay.meiri@gmail.com>
2015-08-20 21:55:56 +0300

    Update script to github.

--- prep_zip.sh
+++ prep_zip.sh
@@ -2,7 +2,7 @@
 
 rm -rf ogldev-source
 rm -f ogldev-source.zip
-git clone https://etay.meiri@code.google.com/p/ogldev/ ogldev-source
+git clone https://github.com/emeiri/ogldev.git ogldev-source
 rm -rf ogldev-source/.git
 rm -rf ogldev-source/web
 zip -r ogldev-source ogldev-source
