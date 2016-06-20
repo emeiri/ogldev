1efbad0600a6eb4ee241c27fd29152a2709f536d
Etay Meiri <etay.meiri@gmail.com>
2016-06-20 11:55:58 +0300

    Fix build problem on VS 2015

--- Include/ogldev_util.h
+++ Include/ogldev_util.h
@@ -46,7 +46,11 @@ void OgldevFileError(const char* pFileName, uint line, const char* pFileError);
 #define SNPRINTF _snprintf_s
 #define RANDOM rand
 #define SRANDOM srand((unsigned)time(NULL))
+#if (_MSC_VER == 1900)
+#elif (_MSC_VER == 1800)
+#else
 float fmax(float a, float b);
+#endif
 #else
 #define SNPRINTF snprintf
 #define RANDOM random
