61653d2c63ac2c01184f0ed74911215c80889939
Etay Meiri <etay.meiri@gmail.com>
2016-10-29 11:11:55 +0300

    Tutorial 51 clears the screen

--- tutorial51/tutorial51.cpp
+++ tutorial51/tutorial51.cpp
@@ -60,16 +60,11 @@ private:
     void CreateSwapChain();
     void CreateCommandBuffer();
     void CreateSemaphore();
-    void CreateRenderPass();
-    void CreateFramebuffer();
-    void CreateShaders();
-    void CreatePipeline();
     void RecordCommandBuffers();
     void Draw();
 
     VulkanWindowControl* m_pWindowControl;
     OgldevVulkanCore m_core;    
- //   std::vector<std::string> m_devExt;    
     std::vector<VkImage> m_images;
     std::vector<VkImageView> m_views;
     VkSwapchainKHR m_swapChainKHR;
@@ -78,13 +73,6 @@ private:
     VkSemaphore m_rendCompSem;
     std::vector<VkCommandBuffer> m_presentQCmdBuffs;
     VkCommandPool m_presentQCmdPool;
-    VkRenderPass m_renderPass;
-    std::vector<VkFramebuffer> m_fbs;
-    VkShaderModule m_vsModule;
-    VkShaderModule m_fsModule;
-    VkPipeline m_pipeline;
- //   VkPhysicalDeviceMemoryProperties m_memProps;
-    VkPipelineLayout m_pipelineLayout;
 };
 
 
@@ -262,82 +250,6 @@ void OgldevVulkanApp::CreateSemaphore()
 }
 
 
-void OgldevVulkanApp::CreateRenderPass()
-{
-    VkAttachmentDescription attachDesc = {};
-    attachDesc.format = m_core.GetSurfaceFormat().format;
-    attachDesc.samples = VK_SAMPLE_COUNT_1_BIT;
-    attachDesc.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-    attachDesc.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-    attachDesc.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-    attachDesc.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-    attachDesc.initialLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-    attachDesc.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-
-    VkAttachmentReference attachRef = {};
-    attachRef.attachment = 0;
-    attachRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
-
-    VkSubpassDescription subpassDesc = {};
-    subpassDesc.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-    subpassDesc.colorAttachmentCount = 1;
-    subpassDesc.pColorAttachments = &attachRef;
-
-    VkRenderPassCreateInfo renderPassCreateInfo = {};
-    renderPassCreateInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-    renderPassCreateInfo.attachmentCount = 1;
-    renderPassCreateInfo.pAttachments = &attachDesc;
-    renderPassCreateInfo.subpassCount = 1;
-    renderPassCreateInfo.pSubpasses = &subpassDesc;
-
-    VkResult res = vkCreateRenderPass(m_core.GetDevice(), &renderPassCreateInfo, NULL, &m_renderPass);
-    CHECK_VULKAN_ERROR("vkCreateRenderPass error %d\n", res);
-
-    printf("Created a render pass\n");
-}
-
-
-void OgldevVulkanApp::CreateFramebuffer()
-{
-    m_fbs.resize(m_images.size());
-    
-    VkResult res;
-            
-    for (uint i = 0 ; i < m_images.size() ; i++) {
-        VkImageViewCreateInfo ViewCreateInfo = {};
-        ViewCreateInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
-        ViewCreateInfo.image = m_images[i];
-        ViewCreateInfo.format = m_core.GetSurfaceFormat().format;
-        ViewCreateInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
-        ViewCreateInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
-        ViewCreateInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
-        ViewCreateInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
-        ViewCreateInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
-        ViewCreateInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
-        ViewCreateInfo.subresourceRange.baseMipLevel = 0;
-        ViewCreateInfo.subresourceRange.levelCount = 1;
-        ViewCreateInfo.subresourceRange.baseArrayLayer = 0;
-        ViewCreateInfo.subresourceRange.layerCount = 1;    
-
-        res = vkCreateImageView(m_core.GetDevice(), &ViewCreateInfo, NULL, &m_views[i]);
-        CHECK_VULKAN_ERROR("vkCreateImageView error %d\n", res);
-        
-        VkFramebufferCreateInfo fbCreateInfo = {};
-        fbCreateInfo.sType = VK_STRUCTURE_TYPE_FRAMEBUFFER_CREATE_INFO;
-        fbCreateInfo.renderPass = m_renderPass;
-        fbCreateInfo.attachmentCount = 1;
-        fbCreateInfo.pAttachments = &m_views[i];
-        fbCreateInfo.width = WINDOW_WIDTH;
-        fbCreateInfo.height = WINDOW_HEIGHT;
-        fbCreateInfo.layers = 1;
-
-        res = vkCreateFramebuffer(m_core.GetDevice(), &fbCreateInfo, NULL, &m_fbs[i]);
-        CHECK_VULKAN_ERROR("vkCreateFramebuffer error %d\n", res);
-    }
-   
-    printf("Frame buffers created\n");
-}
-
 void OgldevVulkanApp::Draw()
 {
     uint ImageIndex = 0;
@@ -384,176 +296,6 @@ void OgldevVulkanApp::Draw()
 }
 
 
-
-void OgldevVulkanApp::CreateShaders()
-{
-    m_vsModule = VulkanCreateShaderModule(m_core.GetDevice(), "Shaders/vs.spv");
-    assert(m_vsModule);
-
-    m_fsModule = VulkanCreateShaderModule(m_core.GetDevice(), "Shaders/fs.spv");
-    assert(m_fsModule);
-}
-
-
-static bool memory_type_from_properties(VkPhysicalDeviceMemoryProperties& memProps, 
-                                        uint32_t typeBits,
-                                        VkFlags requirements_mask,
-                                        uint32_t *typeIndex) 
-{
-    // Search memtypes to find first index with those properties
-    for (uint32_t i = 0; i < 32; i++) {
-        if ((typeBits & 1) == 1) {
-            // Type is available, does it match user properties?
-            if ((memProps.memoryTypes[i].propertyFlags &
-                 requirements_mask) == requirements_mask) {
-                *typeIndex = i;
-                return true;
-            }
-        }
-        typeBits >>= 1;
-    }
-    // No memory types matched, return failure
-    return false;
-}
-
-#define VERTEX_BUFFER_BIND_ID 0
-
-void OgldevVulkanApp::CreatePipeline()
-{
-    VkPipelineShaderStageCreateInfo shaderStageCreateInfo[2] = {};
-    
-    shaderStageCreateInfo[0].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-    shaderStageCreateInfo[0].stage = VK_SHADER_STAGE_VERTEX_BIT;
-    shaderStageCreateInfo[0].module = m_vsModule;
-    shaderStageCreateInfo[0].pName = "main";
-    shaderStageCreateInfo[1].sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO;
-    shaderStageCreateInfo[1].stage = VK_SHADER_STAGE_FRAGMENT_BIT;
-    shaderStageCreateInfo[1].module = m_fsModule;
-    shaderStageCreateInfo[1].pName = "main";   
-	    
-    VkPipelineVertexInputStateCreateInfo vertexInputInfo = {};
-    vertexInputInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
-    
-    VkPipelineInputAssemblyStateCreateInfo pipelineIACreateInfo = {};
-    pipelineIACreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO;
-    pipelineIACreateInfo.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST;
-    
-    VkViewport vp = {};
-    vp.x = 0.0f;
-    vp.y = 0.0f;
-    vp.width  = (float)WINDOW_WIDTH;
-    vp.height = (float)WINDOW_HEIGHT;
-    vp.minDepth = 0.0f;
-    vp.maxDepth = 1.0f;
-    
-    VkRect2D scissor = {};
-    scissor.offset.x = 0;
-    scissor.offset.y = 0;
-    scissor.extent.width  = WINDOW_WIDTH;
-    scissor.extent.height = WINDOW_HEIGHT;
-    
-    VkDynamicState dynamicStateEnables[VK_DYNAMIC_STATE_RANGE_SIZE] = {};
-    VkPipelineDynamicStateCreateInfo dynamicState = {};
-    dynamicState.sType = VK_STRUCTURE_TYPE_PIPELINE_DYNAMIC_STATE_CREATE_INFO;
-    dynamicState.pDynamicStates = dynamicStateEnables;
-
-    VkPipelineViewportStateCreateInfo vpCreateInfo = {};
-    vpCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO;
-    vpCreateInfo.viewportCount = 1;
-    vpCreateInfo.pViewports = &vp;
-    vpCreateInfo.scissorCount = 1;
-    vpCreateInfo.pScissors = &scissor;
-    dynamicStateEnables[dynamicState.dynamicStateCount++] = VK_DYNAMIC_STATE_VIEWPORT;
-    dynamicStateEnables[dynamicState.dynamicStateCount++] = VK_DYNAMIC_STATE_SCISSOR;
-    
-    VkPipelineDepthStencilStateCreateInfo dsInfo = {};
-    dsInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO;
-    dsInfo.depthTestEnable = VK_TRUE;
-    dsInfo.depthWriteEnable = VK_TRUE;
-    dsInfo.depthCompareOp = VK_COMPARE_OP_LESS_OR_EQUAL;
-    dsInfo.back.failOp = VK_STENCIL_OP_KEEP;
-    dsInfo.back.passOp = VK_STENCIL_OP_KEEP;
-    dsInfo.back.compareOp = VK_COMPARE_OP_ALWAYS;
-    dsInfo.front = dsInfo.back;
-    
-    VkPipelineRasterizationStateCreateInfo rastCreateInfo = {};
-    rastCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO;
-    rastCreateInfo.polygonMode = VK_POLYGON_MODE_FILL;
-    rastCreateInfo.cullMode = VK_CULL_MODE_BACK_BIT;
-    rastCreateInfo.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE;
-    rastCreateInfo.lineWidth = 1.0f;
-    
-    VkPipelineMultisampleStateCreateInfo pipelineMSCreateInfo = {};
-    pipelineMSCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO;
-    pipelineMSCreateInfo.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT;
-    //pipelineMSCreateInfo.minSampleShading = 1.0f;
-    
-    VkPipelineColorBlendAttachmentState blendAttachState = {};
-    /*blendAttachState.srcColorBlendFactor = VK_BLEND_FACTOR_ONE;
-    blendAttachState.dstColorBlendFactor = VK_BLEND_FACTOR_ZERO;
-    blendAttachState.colorBlendOp = VK_BLEND_OP_ADD;
-    blendAttachState.srcAlphaBlendFactor = VK_BLEND_FACTOR_ONE;
-    blendAttachState.dstAlphaBlendFactor = VK_BLEND_FACTOR_ZERO;
-    blendAttachState.alphaBlendOp = VK_BLEND_OP_ADD;
-    blendAttachState.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | 
-                                      VK_COLOR_COMPONENT_G_BIT | 
-                                      VK_COLOR_COMPONENT_B_BIT | 
-                                      VK_COLOR_COMPONENT_A_BIT;*/
-    blendAttachState.colorWriteMask = 0xf;
-    
-    VkPipelineColorBlendStateCreateInfo blendCreateInfo = {};
-    blendCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO;
-    blendCreateInfo.logicOp = VK_LOGIC_OP_COPY;
-    blendCreateInfo.attachmentCount = 1;
-    blendCreateInfo.pAttachments = &blendAttachState;
-
-    VkDescriptorSetLayoutBinding layoutBinding = {};
-    layoutBinding.descriptorType = VK_DESCRIPTOR_TYPE_UNIFORM_BUFFER;
-    layoutBinding.descriptorCount = 1;
-    layoutBinding.stageFlags = VK_SHADER_STAGE_VERTEX_BIT;
-    layoutBinding.pImmutableSamplers = NULL;
-
-    VkDescriptorSetLayoutCreateInfo descriptorLayout = {};
-    descriptorLayout.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_LAYOUT_CREATE_INFO;
-    descriptorLayout.pNext = NULL;
-    descriptorLayout.bindingCount = 1;
-    descriptorLayout.pBindings = &layoutBinding;
-
-    VkDescriptorSetLayout descriptorSetLayout;
-    VkResult res = vkCreateDescriptorSetLayout(m_core.GetDevice(), &descriptorLayout, NULL, &descriptorSetLayout);    
-    CHECK_VULKAN_ERROR("vkCreateDescriptorSetLayout error %d\n", res);
-     
-    VkPipelineLayoutCreateInfo layoutInfo = {};
-    layoutInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO;
-    layoutInfo.setLayoutCount = 1;
-    layoutInfo.pSetLayouts = &descriptorSetLayout;
-        
-    res = vkCreatePipelineLayout(m_core.GetDevice(), &layoutInfo, NULL, &m_pipelineLayout);
-    CHECK_VULKAN_ERROR("vkCreatePipelineLayout error %d\n", res);
-   
-    VkGraphicsPipelineCreateInfo pipelineInfo = {};
-    pipelineInfo.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO;
-    pipelineInfo.stageCount = ARRAY_SIZE_IN_ELEMENTS(shaderStageCreateInfo);
-    pipelineInfo.pStages = &shaderStageCreateInfo[0];
-    pipelineInfo.pVertexInputState = &vertexInputInfo;
-    pipelineInfo.pInputAssemblyState = &pipelineIACreateInfo;
-    pipelineInfo.pViewportState = &vpCreateInfo;
-    pipelineInfo.pDepthStencilState = &dsInfo;
-    pipelineInfo.pRasterizationState = &rastCreateInfo;
-    pipelineInfo.pMultisampleState = &pipelineMSCreateInfo;
-    pipelineInfo.pColorBlendState = &blendCreateInfo;
-   pipelineInfo.layout = m_pipelineLayout;
-    pipelineInfo.renderPass = m_renderPass;
-    pipelineInfo.pDynamicState = &dynamicState;
-    pipelineInfo.basePipelineIndex = -1;
-    
-    res = vkCreateGraphicsPipelines(m_core.GetDevice(), VK_NULL_HANDLE, 1, &pipelineInfo, NULL, &m_pipeline);
-    CHECK_VULKAN_ERROR("vkCreateGraphicsPipelines error %d\n", res);
-    
-    printf("Graphics pipeline created\n");
-}
-
-
 void OgldevVulkanApp::RecordCommandBuffers() 
 {
     VkCommandBufferBeginInfo beginInfo = {};
@@ -568,28 +310,18 @@ void OgldevVulkanApp::RecordCommandBuffers()
     imageRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
     imageRange.levelCount = 1;
     imageRange.layerCount = 1;
-    
-    VkRenderPassBeginInfo renderPassInfo = {};
-    renderPassInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_BEGIN_INFO;
-    renderPassInfo.renderPass = m_renderPass;   
-    renderPassInfo.renderArea.offset.x = 0;
-    renderPassInfo.renderArea.offset.y = 0;
-    renderPassInfo.renderArea.extent.width = WINDOW_WIDTH;
-    renderPassInfo.renderArea.extent.height = WINDOW_HEIGHT;
-    renderPassInfo.clearValueCount = 1;
-    renderPassInfo.pClearValues = &clearValue;
-    
+       
     VkResult res;
     
     for (uint i = 0 ; i < m_presentQCmdBuffs.size() ; i++) {
-     /*   VkImageMemoryBarrier memBarrier_PresentToClear = {};
+        VkImageMemoryBarrier memBarrier_PresentToClear = {};
         memBarrier_PresentToClear.sType = VK_STRUCTURE_TYPE_IMAGE_MEMORY_BARRIER;
         memBarrier_PresentToClear.srcAccessMask = VK_ACCESS_MEMORY_READ_BIT;
         memBarrier_PresentToClear.dstAccessMask = VK_ACCESS_TRANSFER_WRITE_BIT;
         memBarrier_PresentToClear.oldLayout = VK_IMAGE_LAYOUT_UNDEFINED;
         memBarrier_PresentToClear.newLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
-        memBarrier_PresentToClear.srcQueueFamilyIndex = m_gfxQueueFamily;
-        memBarrier_PresentToClear.dstQueueFamilyIndex = m_gfxQueueFamily;
+        memBarrier_PresentToClear.srcQueueFamilyIndex = m_core.GetQueueFamily();
+        memBarrier_PresentToClear.dstQueueFamilyIndex = m_core.GetQueueFamily();
         memBarrier_PresentToClear.image = m_images[i];
         memBarrier_PresentToClear.subresourceRange = imageRange;        
         
@@ -599,45 +331,23 @@ void OgldevVulkanApp::RecordCommandBuffers()
         memBarrier_PresentToClear.dstAccessMask = VK_ACCESS_MEMORY_READ_BIT;               
         memBarrier_PresentToClear.oldLayout = VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL;
         memBarrier_PresentToClear.newLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;                
-        memBarrier_PresentToClear.srcQueueFamilyIndex = m_gfxQueueFamily;
-        memBarrier_PresentToClear.dstQueueFamilyIndex = m_gfxQueueFamily;
+        memBarrier_PresentToClear.srcQueueFamilyIndex = m_core.GetQueueFamily();
+        memBarrier_PresentToClear.dstQueueFamilyIndex = m_core.GetQueueFamily();
         memBarrier_PresentToClear.image = m_images[i];
-        memBarrier_PresentToClear.subresourceRange = imageRange;  */      
+        memBarrier_PresentToClear.subresourceRange = imageRange;    
               
         res = vkBeginCommandBuffer(m_presentQCmdBuffs[i], &beginInfo);
         CHECK_VULKAN_ERROR("vkBeginCommandBuffer error %d\n", res);
                 
-      //  vkCmdPipelineBarrier(m_presentQCmdBuffs[i], VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT,
-      //                      0, 0, NULL, 0, NULL, 1, &memBarrier_PresentToClear);                
-        renderPassInfo.framebuffer = m_fbs[i];
+        vkCmdPipelineBarrier(m_presentQCmdBuffs[i], VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_TRANSFER_BIT,
+                             0, 0, NULL, 0, NULL, 1, &memBarrier_PresentToClear);
 
-        vkCmdBeginRenderPass(m_presentQCmdBuffs[i], &renderPassInfo, VK_SUBPASS_CONTENTS_INLINE);
-
-        vkCmdBindPipeline(m_presentQCmdBuffs[i], VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
+                     
+        vkCmdClearColorImage(m_presentQCmdBuffs[i], m_images[i], VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, &clearColor, 1, &imageRange);                
         
-        VkViewport viewport = { 0 };
-        viewport.height = (float)WINDOW_HEIGHT;
-        viewport.width = (float)WINDOW_WIDTH;
-        viewport.minDepth = (float)0.0f;
-        viewport.maxDepth = (float)1.0f;
-        vkCmdSetViewport(m_presentQCmdBuffs[i], 0, 1, &viewport);
-
-        VkRect2D scissor = { 0 };
-        scissor.extent.width = WINDOW_WIDTH;
-        scissor.extent.height = WINDOW_HEIGHT;
-        scissor.offset.x = 0;
-        scissor.offset.y = 0;
-        vkCmdSetScissor(m_presentQCmdBuffs[i], 0, 1, &scissor);
-
-        vkCmdDraw(m_presentQCmdBuffs[i], 3, 1, 0, 0);
         
-        vkCmdEndRenderPass(m_presentQCmdBuffs[i]);
-        
-      //  vkCmdClearColorImage(m_presentQCmdBuffs[i], m_images[i], VK_IMAGE_LAYOUT_TRANSFER_DST_OPTIMAL, &clearColor,
-           //                  1, &imageRange);                
-        
-      //  vkCmdPipelineBarrier(m_presentQCmdBuffs[i], VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
-      //                       0, 0, NULL, 0, NULL, 1, &memBarrier_ClearToPresent);
+        vkCmdPipelineBarrier(m_presentQCmdBuffs[i], VK_PIPELINE_STAGE_TRANSFER_BIT, VK_PIPELINE_STAGE_BOTTOM_OF_PIPE_BIT,
+                             0, 0, NULL, 0, NULL, 1, &memBarrier_ClearToPresent);
 
         res = vkEndCommandBuffer(m_presentQCmdBuffs[i]);
         CHECK_VULKAN_ERROR("vkEndCommandBuffer error %d\n", res);
@@ -645,8 +355,6 @@ void OgldevVulkanApp::RecordCommandBuffers()
     
     printf("Command buffers recorded\n");    
 }
-
-
 
 
 bool OgldevVulkanApp::Init()
@@ -666,11 +374,11 @@ bool OgldevVulkanApp::Init()
     EnumPhysDeviceExtProps();
     CreateSwapChain();
     CreateSemaphore();    
-    CreateRenderPass();
-    CreateFramebuffer();
+ //  CreateRenderPass();
+  //  CreateFramebuffer();
     CreateCommandBuffer();
-    CreateShaders();
-    CreatePipeline();
+   // CreateShaders();
+    //CreatePipeline();
     RecordCommandBuffers();
 
     return true;
@@ -731,4 +439,3 @@ int main(int argc, char** argv)
     
     return 0;
 }
-
---
+++ tutorial52/nbproject/Makefile-Debug.mk
@@ -0,0 +1,88 @@
+#
+# Generated Makefile - do not edit!
+#
+# Edit the Makefile in the project folder instead (../Makefile). Each target
+# has a -pre and a -post target defined where you can add customized code.
+#
+# This makefile implements configuration specific macros and targets.
+
+
+# Environment
+MKDIR=mkdir
+CP=cp
+GREP=grep
+NM=nm
+CCADMIN=CCadmin
+RANLIB=ranlib
+CC=gcc
+CCC=g++
+CXX=g++
+FC=gfortran
+AS=as
+
+# Macros
+CND_PLATFORM=GNU-Linux-x86
+CND_DLIB_EXT=so
+CND_CONF=Debug
+CND_DISTDIR=dist
+CND_BUILDDIR=build
+
+# Include project Makefile
+include Makefile
+
+# Object Directory
+OBJECTDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}
+
+# Object Files
+OBJECTFILES= \
+	${OBJECTDIR}/tutorial52.o
+
+
+# C Compiler Flags
+CFLAGS=
+
+# CC Compiler Flags
+CCFLAGS=`pkg-config --cflags glew ImageMagick++ assimp freetype2 fontconfig glfw3` 
+CXXFLAGS=`pkg-config --cflags glew ImageMagick++ assimp freetype2 fontconfig glfw3` 
+
+# Fortran Compiler Flags
+FFLAGS=
+
+# Assembler Flags
+ASFLAGS=
+
+# Link Libraries and Options
+LDLIBSOPTIONS=../Common/dist/Debug/GNU-Linux-x86/libcommon.a -lglut -lxcb -lvulkan
+
+# Build Targets
+.build-conf: ${BUILD_SUBPROJECTS}
+	"${MAKE}"  -f nbproject/Makefile-${CND_CONF}.mk ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial52
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial52: ../Common/dist/Debug/GNU-Linux-x86/libcommon.a
+
+${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial52: ${OBJECTFILES}
+	${MKDIR} -p ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}
+	${LINK.cc} -o ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial52 ${OBJECTFILES} ${LDLIBSOPTIONS} `pkg-config --libs glew ImageMagick++ assimp freetype2 fontconfig gl glfw3`
+
+${OBJECTDIR}/tutorial52.o: tutorial52.cpp 
+	${MKDIR} -p ${OBJECTDIR}
+	${RM} "$@.d"
+	$(COMPILE.cc) -g -I../Include -I../Common/FreetypeGL -MMD -MP -MF "$@.d" -o ${OBJECTDIR}/tutorial52.o tutorial52.cpp
+
+# Subprojects
+.build-subprojects:
+	cd ../Common && ${MAKE}  -f Makefile CONF=Debug
+
+# Clean Targets
+.clean-conf: ${CLEAN_SUBPROJECTS}
+	${RM} -r ${CND_BUILDDIR}/${CND_CONF}
+	${RM} ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/tutorial52
+
+# Subprojects
+.clean-subprojects:
+	cd ../Common && ${MAKE}  -f Makefile CONF=Debug clean
+
+# Enable dependency checking
+.dep.inc: .depcheck-impl
+
+include .dep.inc
