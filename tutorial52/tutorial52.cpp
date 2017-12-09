942f85050d9b5767cb53681f6dc7488c79b5d907
Etay Meiri <etay.meiri@gmail.com>
2017-12-09 11:31:34 +0200

    wip

--- tutorial52/tutorial52.cpp
+++ tutorial52/tutorial52.cpp
@@ -1,6 +1,6 @@
 /*
 
-	Copyright 2016 Etay Meiri
+	Copyright 2017 Etay Meiri
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
