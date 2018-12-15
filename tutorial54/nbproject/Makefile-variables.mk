6d36daae7b15aece1f58cf4f61dc72f620817305
Etay Meiri <etay.meiri@gmail.com>
2018-12-15 23:21:15 +0200

    copied the original tutorial53 to 54

---
+++ tutorial54/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=tutorial53
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/tutorial53
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=tutorial53.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/tutorial53.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=tutorial53
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/tutorial53
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=tutorial53.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/tutorial53.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
