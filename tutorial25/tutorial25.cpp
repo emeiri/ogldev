d843514af80a37977a0c67d722b15a5196f87b37
Etay Meiri <etay.meiri@gmail.com>
2020-10-23 11:15:45 +0300

    build.sh 25

--- tutorial25/tutorial25.cpp
+++ tutorial25/tutorial25.cpp
@@ -1,6 +1,6 @@
 /*
 
-	Copyright 2011 Etay Meiri
+        Copyright 2011 Etay Meiri
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -22,10 +22,16 @@
 #include <GL/glew.h>
 #include <GL/freeglut.h>
 
-#include "../Common/ogldev_all.cpp"
-#include "mesh.cpp"
-#include "skybox.cpp"
-#include "skybox_technique.cpp"
+#include "ogldev_pipeline.h"
+#include "ogldev_math_3d.h"
+#include "ogldev_glut_backend.h"
+#include "ogldev_texture.h"
+#include "ogldev_lights_common.h"
+#include "ogldev_app.h"
+#include "ogldev_basic_lighting.h"
+#include "mesh.h"
+#include "skybox.h"
+#include "skybox_technique.h"
 
 #define WINDOW_WIDTH  1920
 #define WINDOW_HEIGHT 1200
@@ -37,8 +43,8 @@ public:
 
     Tutorial25()
     {
-        m_pLightingTechnique = NULL;        
-        m_pGameCamera = NULL;        
+        m_pLightingTechnique = NULL;
+        m_pGameCamera = NULL;
         m_pTankMesh = NULL;
         m_scale = 0.0f;
         m_pSkyBox = NULL;
@@ -47,24 +53,24 @@ public:
         m_dirLight.DiffuseIntensity = 0.8f;
         m_dirLight.Color = Vector3f(1.0f, 1.0f, 1.0f);
         m_dirLight.Direction = Vector3f(1.0f, -1.0f, 0.0f);
-        
+
         m_persProjInfo.FOV = 60.0f;
         m_persProjInfo.Height = WINDOW_HEIGHT;
         m_persProjInfo.Width = WINDOW_WIDTH;
         m_persProjInfo.zNear = 1.0f;
-        m_persProjInfo.zFar = 100.0f;        
+        m_persProjInfo.zFar = 100.0f;
     }
-    
+
 
     virtual ~Tutorial25()
     {
         SAFE_DELETE(m_pLightingTechnique);
-        SAFE_DELETE(m_pGameCamera);        
-        SAFE_DELETE(m_pTankMesh);        
+        SAFE_DELETE(m_pGameCamera);
+        SAFE_DELETE(m_pTankMesh);
         SAFE_DELETE(m_pSkyBox);
     }
 
-    
+
     bool Init()
     {
         Vector3f Pos(0.0f, 1.0f, -20.0f);
@@ -72,7 +78,7 @@ public:
         Vector3f Up(0.0, 1.0f, 0.0f);
 
         m_pGameCamera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT, Pos, Target, Up);
-     
+
         m_pLightingTechnique = new BasicLightingTechnique();
 
         if (!m_pLightingTechnique->Init()) {
@@ -83,86 +89,86 @@ public:
         m_pLightingTechnique->Enable();
         m_pLightingTechnique->SetDirectionalLight(m_dirLight);
         m_pLightingTechnique->SetColorTextureUnit(0);
-              
+
         m_pTankMesh = new Mesh();
-        
-		if (!m_pTankMesh->LoadMesh("../Content/phoenix_ugv.md2")) {
+
+                if (!m_pTankMesh->LoadMesh("../Content/phoenix_ugv.md2")) {
             return false;
         }
-        
+
         m_pSkyBox = new SkyBox(m_pGameCamera, m_persProjInfo);
 
-		if (!m_pSkyBox->Init(".",
-			"../Content/sp3right.jpg",
-			"../Content/sp3left.jpg",
-			"../Content/sp3top.jpg",
-			"../Content/sp3bot.jpg",
-			"../Content/sp3front.jpg",
-			"../Content/sp3back.jpg")) {
+                if (!m_pSkyBox->Init(".",
+                        "../Content/sp3right.jpg",
+                        "../Content/sp3left.jpg",
+                        "../Content/sp3top.jpg",
+                        "../Content/sp3bot.jpg",
+                        "../Content/sp3front.jpg",
+                        "../Content/sp3back.jpg")) {
             return false;
         }
-        
+
         return true;
     }
 
-    
+
     void Run()
     {
         GLUTBackendRun(this);
     }
 
-    
+
     virtual void RenderSceneCB()
     {
         m_pGameCamera->OnRender();
         m_scale += 0.05f;
-        
+
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         m_pLightingTechnique->Enable();
-       
-        Pipeline p;        
+
+        Pipeline p;
         p.Scale(0.1f, 0.1f, 0.1f);
         p.Rotate(0.0f, m_scale, 0.0f);
         p.WorldPos(0.0f, -5.0f, 3.0f);
         p.SetCamera(m_pGameCamera->GetPos(), m_pGameCamera->GetTarget(), m_pGameCamera->GetUp());
         p.SetPerspectiveProj(m_persProjInfo);
-        
+
         m_pLightingTechnique->SetWVP(p.GetWVPTrans());
         m_pLightingTechnique->SetWorldMatrix(p.GetWorldTrans());
         m_pTankMesh->Render();
-        
+
         m_pSkyBox->Render();
-      
+
         glutSwapBuffers();
     }
 
 
-	void KeyboardCB(OGLDEV_KEY OgldevKey, OGLDEV_KEY_STATE State)
-	{
-		switch (OgldevKey) {
-		case OGLDEV_KEY_ESCAPE:
-		case OGLDEV_KEY_q:
-			GLUTBackendLeaveMainLoop();
-			break;
-		default:
-			m_pGameCamera->OnKeyboard(OgldevKey);
-		}
-	}
+        void KeyboardCB(OGLDEV_KEY OgldevKey, OGLDEV_KEY_STATE State)
+        {
+                switch (OgldevKey) {
+                case OGLDEV_KEY_ESCAPE:
+                case OGLDEV_KEY_q:
+                        GLUTBackendLeaveMainLoop();
+                        break;
+                default:
+                        m_pGameCamera->OnKeyboard(OgldevKey);
+                }
+        }
 
 
-	virtual void PassiveMouseCB(int x, int y)
-	{
-		m_pGameCamera->OnMouse(x, y);
-	}
+        virtual void PassiveMouseCB(int x, int y)
+        {
+                m_pGameCamera->OnMouse(x, y);
+        }
 
  private:
 
     BasicLightingTechnique* m_pLightingTechnique;
     Camera* m_pGameCamera;
     float m_scale;
-    DirectionalLight m_dirLight;    
-    Mesh* m_pTankMesh;    
+    DirectionalLight m_dirLight;
+    Mesh* m_pTankMesh;
     SkyBox* m_pSkyBox;
     PersProjInfo m_persProjInfo;
 };
@@ -185,6 +191,6 @@ int main(int argc, char** argv)
     pApp->Run();
 
     delete pApp;
- 
+
     return 0;
 }
