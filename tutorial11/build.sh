eed02737b8539fa50bccfb1179d90e25aa60e469
Etay Meiri <etay.meiri@gmail.com>
2020-11-18 21:43:26 +0200

    replaced libglfw with glfw3

--- tutorial11/build.sh
+++ tutorial11/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 CC=g++
-CPPFLAGS=`pkg-config --cflags glew ImageMagick freetype2 libglfw`
+CPPFLAGS=`pkg-config --cflags glew ImageMagick freetype2 glfw3`
 CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL"
-LDFLAGS=`pkg-config --libs glew ImageMagick freetype2 libglfw`
+LDFLAGS=`pkg-config --libs glew ImageMagick freetype2 glfw3`
 LDFLAGS="$LDFLAGS -lglut"
 
 $CC tutorial11.cpp ../Common/ogldev_util.cpp ../Common/pipeline.cpp ../Common/math_3d.cpp $CPPFLAGS $LDFLAGS -o tutorial11
--- tutorial17/build.sh
+++ tutorial17/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 CC=g++
-CPPFLAGS=`pkg-config --cflags glew ImageMagick++ freetype2 libglfw`
+CPPFLAGS=`pkg-config --cflags glew ImageMagick++ freetype2 glfw3`
 CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL"
-LDFLAGS=`pkg-config --libs glew ImageMagick++ freetype2 libglfw`
+LDFLAGS=`pkg-config --libs glew ImageMagick++ freetype2 glfw3`
 LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11"
 
 $CC tutorial17.cpp lighting_technique.cpp ../Common/ogldev_util.cpp ../Common/pipeline.cpp ../Common/math_3d.cpp ../Common/camera.cpp ../Common/ogldev_atb.cpp ../Common/glut_backend.cpp ../Common/ogldev_texture.cpp ../Common/ogldev_basic_lighting.cpp ../Common/technique.cpp ../Common/ogldev_app.cpp $CPPFLAGS $LDFLAGS -o tutorial17
