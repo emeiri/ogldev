db0dc6c177f8723175a9e0d4f83aa9f8f09ef72f
Etay Meiri <etay.meiri@gmail.com>
2020-11-18 22:05:50 +0200

    removed system freetype-gl from build.sh

--- tutorial35/build.sh
+++ tutorial35/build.sh
@@ -4,6 +4,6 @@ CC=g++
 CPPFLAGS=`pkg-config --cflags glew ImageMagick++ freetype2 glfw3 fontconfig assimp`
 CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL -ggdb3"
 LDFLAGS=`pkg-config --libs glew ImageMagick++ freetype2 glfw3 fontconfig assimp`
-LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11 -L/usr/local/lib/freetype-gl-cpp -lfreetype-gl-cpp"
+LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11  "
 
 $CC tutorial35.cpp gbuffer.cpp ds_geom_pass_tech.cpp ../Common/ogldev_basic_mesh.cpp ../Common/ogldev_util.cpp ../Common/pipeline.cpp ../Common/math_3d.cpp ../Common/camera.cpp ../Common/ogldev_atb.cpp ../Common/glut_backend.cpp ../Common/ogldev_texture.cpp ../Common/ogldev_basic_lighting.cpp ../Common/technique.cpp ../Common/ogldev_app.cpp $CPPFLAGS $LDFLAGS -o tutorial35
--- tutorial43/build.sh
+++ tutorial43/build.sh
@@ -4,6 +4,6 @@ CC=g++
 CPPFLAGS=`pkg-config --cflags glew ImageMagick++ freetype2 glfw3 fontconfig assimp`
 CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL -ggdb3"
 LDFLAGS=`pkg-config --libs glew ImageMagick++ freetype2 glfw3 fontconfig assimp`
-LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11 -L/usr/local/lib/freetype-gl-cpp -lfreetype-gl-cpp"
+LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11  "
 
 $CC tutorial43.cpp lighting_technique.cpp shadow_cube_map_fbo.cpp shadow_map_technique.cpp ../Common/ogldev_basic_mesh.cpp ../Common/ogldev_util.cpp ../Common/pipeline.cpp ../Common/math_3d.cpp ../Common/camera.cpp ../Common/ogldev_atb.cpp ../Common/ogldev_backend.cpp ../Common/ogldev_glfw_backend.cpp ../Common/glut_backend.cpp ../Common/ogldev_texture.cpp  ../Common/technique.cpp ../Common/ogldev_app.cpp $CPPFLAGS $LDFLAGS -o tutorial43
