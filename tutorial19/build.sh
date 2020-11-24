5cb8a710e7ed2db1b9cbe3c6775bb33d8d4cdaff
Etay Meiri <etay.meiri@gmail.com>
2020-11-24 23:21:44 +0200

    fixed build problem

--- tutorial19/build.sh
+++ tutorial19/build.sh
@@ -6,4 +6,4 @@ CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL"
 LDFLAGS=`pkg-config --libs glew ImageMagick++ freetype2 glfw3 fontconfig assimp`
 LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11  "
 
-$CC tutorial19.cpp  lighting_technique.cpp ../Common/ogldev_util.cpp ../Common/pipeline.cpp ../Common/math_3d.cpp ../Common/camera.cpp ../Common/ogldev_atb.cpp ../Common/glut_backend.cpp ../Common/ogldev_texture.cpp ../Common/ogldev_basic_lighting.cpp ../Common/technique.cpp ../Common/ogldev_app.cpp $CPPFLAGS $LDFLAGS -o tutorial19
+$CC tutorial19.cpp  lighting_technique.cpp ../Common/ogldev_util.cpp ../Common/pipeline.cpp ../Common/math_3d.cpp ../Common/camera.cpp ../Common/ogldev_atb.cpp ../Common/glut_backend.cpp ../Common/ogldev_texture.cpp ../Common/ogldev_basic_lighting.cpp ../Common/technique.cpp ../Common/ogldev_app.cpp ../Common/FreetypeGL/freetypeGL.cpp $CPPFLAGS $LDFLAGS -o tutorial19
