9652fadbddebe15a14612e7f8e256d4f0b39b0cb
Etay Meiri <etay.meiri@gmail.com>
2021-02-28 20:55:22 +0200

    Fix build error on Windows.

--- Common/ogldev_util.cpp
+++ Common/ogldev_util.cpp
@@ -1,6 +1,6 @@
 /*
 
-	Copyright 2014 Etay Meiri
+        Copyright 2014 Etay Meiri
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
@@ -36,24 +36,24 @@
 bool ReadFile(const char* pFileName, string& outFile)
 {
     ifstream f(pFileName);
-    
+
     bool ret = false;
-    
+
     if (f.is_open()) {
         string line;
         while (getline(f, line)) {
             outFile.append(line);
             outFile.append("\n");
         }
-        
+
         f.close();
-        
+
         ret = true;
     }
     else {
         OGLDEV_FILE_ERROR(pFileName);
     }
-    
+
     return ret;
 }
 
@@ -66,20 +66,20 @@ char* ReadBinaryFile(const char* pFileName, int& size)
 
     if (f == INVALID_HANDLE_VALUE) {
         OGLDEV_FILE_ERROR(pFileName);
-        return false;
+        return NULL;
     }
 
     size = GetFileSize(f, NULL);
 
     if (size == INVALID_FILE_SIZE) {
         OGLDEV_ERROR("Invalid file size %s\n", pFileName);
-        return false;
+        return NULL;
     }
 
-	char* p = (char*)malloc(size);
+    char* p = (char*)malloc(size);
 
-	DWORD bytes_read = 0;
-	bool b = ReadFile(f, p, size, &bytes_read, NULL);
+    DWORD bytes_read = 0;
+    bool b = ReadFile(f, p, size, &bytes_read, NULL);
 
     return p;
 }
@@ -88,53 +88,53 @@ char* ReadBinaryFile(const char* pFileName, int& size)
 char* ReadBinaryFile(const char* pFileName, int& size)
 {
     int f = open(pFileName, O_RDONLY);
-    
+
     if (f == -1) {
         OGLDEV_ERROR("Error opening '%s': %s\n", pFileName, strerror(errno));
-        return NULL;        
+        return NULL;
     }
-    
+
     struct stat stat_buf;
     int error = stat(pFileName, &stat_buf);
-    
+
     if (error) {
         OGLDEV_ERROR("Error getting file stats: %s\n", strerror(errno));
         return NULL;
     }
-    
+
     size = stat_buf.st_size;
-       
+
     char* p = (char*)malloc(size);
     assert(p);
-    
+
     int read_len = read(f, p, size);
-    
+
     if (read_len != size) {
         OGLDEV_ERROR("Error reading file: %s\n", strerror(errno));
-        return NULL;        
+        return NULL;
     }
-    
+
     close(f);
-    
+
     return p;
 }
 #endif
 
 void OgldevError(const char* pFileName, uint line, const char* format, ...)
 {
-    char msg[1000];    
+    char msg[1000];
     va_list args;
     va_start(args, format);
     VSNPRINTF(msg, sizeof(msg), format, args);
     va_end(args);
 
-#ifdef WIN32 
+#ifdef WIN32
     char msg2[1000];
     _snprintf_s(msg2, sizeof(msg2), "%s:%d: %s", pFileName, line, msg);
     MessageBoxA(NULL, msg2, NULL, 0);
 #else
     fprintf(stderr, "%s:%d - %s", pFileName, line, msg);
-#endif    
+#endif
 }
 
 
@@ -146,13 +146,13 @@ void OgldevFileError(const char* pFileName, uint line, const char* pFileError)
     MessageBoxA(NULL, msg, NULL, 0);
 #else
     fprintf(stderr, "%s:%d: unable to open file `%s`\n", pFileName, line, pFileError);
-#endif    
+#endif
 }
 
 
 long long GetCurrentTimeMillis()
 {
-#ifdef WIN32    
+#ifdef WIN32
     return GetTickCount();
 #else
     timeval t;
@@ -160,5 +160,5 @@ long long GetCurrentTimeMillis()
 
     long long ret = t.tv_sec * 1000 + t.tv_usec / 1000;
     return ret;
-#endif    
+#endif
 }
