6f39d805a0207ba0afc93959c498518e2aeb4443
Etay Meiri <etay.meiri@gmail.com>
2021-04-23 21:59:30 +0300

    minor cleanups

--- tutorial06/tutorial06.cpp
+++ tutorial06/tutorial06.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Tutorial 06 - translation transform
+    Tutorial 06 - translation transformation
 */
 
 #include <stdio.h>
