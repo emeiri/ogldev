0b0057354fe6746aba735e2385a331f723f234fc
Etay Meiri <etay.meiri@gmail.com>
2021-04-25 23:21:33 +0300

    07 cleanups

--- tutorial07/build.sh
+++ tutorial07/build.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
 CC=g++
-CPPFLAGS=`pkg-config --cflags glew ImageMagick freetype2 glfw3`
-CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL"
-LDFLAGS=`pkg-config --libs glew ImageMagick freetype2 glfw3`
+CPPFLAGS="-I../Include"
+LDFLAGS=`pkg-config --libs glew`
 LDFLAGS="$LDFLAGS -lglut"
 
 $CC tutorial07.cpp ../Common/ogldev_util.cpp $CPPFLAGS $LDFLAGS -o tutorial07
--- tutorial07/shader.vs
+++ tutorial07/shader.vs
@@ -2,9 +2,9 @@
 
 layout (location = 0) in vec3 Position;
 
-uniform mat4 gWorld;
+uniform mat4 gRotation;
 
 void main()
 {
-    gl_Position = gWorld * vec4(Position, 1.0);
+    gl_Position = gRotation * vec4(Position, 1.0);
 }
