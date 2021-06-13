db18c3b51457427d4b8645697c42bb6bca02b436
Etay Meiri <etay.meiri@gmail.com>
2021-06-13 22:47:16 +0300

    Updated title

--- tutorial11_youtube/tutorial11.cpp
+++ tutorial11_youtube/tutorial11.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Tutorial 11 - Perspective Projection
+    Tutorial 11 - Perspective Projection - part 1
 */
 
 #include <stdio.h>
