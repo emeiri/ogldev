ade1cd5a52e886212ad9dbedd6963279ba1278bd
Etay Meiri <etay.meiri@gmail.com>
2021-07-12 21:41:38 +0300

    tutorial 15 youtube initial commit

---
+++ tutorial15_youtube/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CC=g++
+CPPFLAGS=`pkg-config --cflags glew ImageMagick freetype2 glfw3`
+CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL"
+LDFLAGS=`pkg-config --libs glew ImageMagick freetype2 glfw3`
+LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11"
+
+$CC tutorial15.cpp ../Common/ogldev_util.cpp  ../Common/math_3d.cpp world_transform.cpp camera.cpp $CPPFLAGS $LDFLAGS -o tutorial15
---
+++ tutorial15_youtube/camera.cpp
@@ -0,0 +1,100 @@
+/*
+
+        Copyright 2021 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <GL/freeglut.h>
+
+#include "camera.h"
+
+
+Camera::Camera()
+{
+    m_pos          = Vector3f(0.0f, 0.0f, 0.0f);
+    m_target       = Vector3f(0.0f, 0.0f, 1.0f);
+    m_up           = Vector3f(0.0f, 1.0f, 0.0f);
+}
+
+
+void Camera::SetPosition(float x, float y, float z)
+{
+    m_pos.x = x;
+    m_pos.y = y;
+    m_pos.z = z;
+}
+
+
+void Camera::OnKeyboard(unsigned char Key)
+{
+    switch (Key) {
+
+    case GLUT_KEY_UP:
+        m_pos += (m_target * m_speed);
+        break;
+
+    case GLUT_KEY_DOWN:
+        m_pos -= (m_target * m_speed);
+        break;
+
+    case GLUT_KEY_LEFT:
+        {
+            Vector3f Left = m_target.Cross(m_up);
+            Left.Normalize();
+            Left *= m_speed;
+            m_pos += Left;
+        }
+        break;
+
+    case GLUT_KEY_RIGHT:
+        {
+            Vector3f Right = m_up.Cross(m_target);
+            Right.Normalize();
+            Right *= m_speed;
+            m_pos += Right;
+        }
+        break;
+
+    case GLUT_KEY_PAGE_UP:
+        m_pos.y += m_speed;
+        break;
+
+    case GLUT_KEY_PAGE_DOWN:
+        m_pos.y -= m_speed;
+        break;
+
+    case '+':
+        m_speed += 0.1f;
+        printf("Speed changed to %f\n", m_speed);
+        break;
+
+    case '-':
+        m_speed -= 0.1f;
+        if (m_speed < 0.1f) {
+            m_speed = 0.1f;
+        }
+        printf("Speed changed to %f\n", m_speed);
+        break;
+    }
+}
+
+
+Matrix4f Camera::GetMatrix()
+{
+    Matrix4f CameraTransformation;
+    CameraTransformation.InitCameraTransform(m_pos, m_target, m_up);
+
+    return CameraTransformation;
+}
