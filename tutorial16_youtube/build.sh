db48b691704e2650decc0347dccbd1e86ba67391
Etay Meiri <etay.meiri@gmail.com>
2021-07-27 23:10:52 +0300

    added tutorial16 for youtube

---
+++ tutorial16_youtube/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CC=g++
+CPPFLAGS=`pkg-config --cflags glew ImageMagick++`
+CPPFLAGS="$CPPFLAGS -I../Include"
+LDFLAGS=`pkg-config --libs glew ImageMagick++`
+LDFLAGS="$LDFLAGS -lglut -lX11"
+
+$CC tutorial16.cpp ../Common/ogldev_util.cpp  ../Common/math_3d.cpp ../Common/ogldev_texture.cpp world_transform.cpp camera.cpp $CPPFLAGS $LDFLAGS -o tutorial16
