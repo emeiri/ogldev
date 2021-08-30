0186834ea1ad2ebfd751baa5cc85992a9397bac1
Etay Meiri <etay.meiri@gmail.com>
2021-08-30 22:39:09 +0300

    tutorial19 youtube initial commit

---
+++ tutorial19_youtube/tutorial19.cpp
@@ -0,0 +1,348 @@
+/*
+
+        Copyright 2021 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Tutorial 18 - Loading models with Assimp
+*/
+
+#include <stdio.h>
+#include <string.h>
+
+#include <math.h>
+#include <GL/glew.h>
+#include <GL/freeglut.h>
+
+#include "ogldev_math_3d.h"
+#include "ogldev_texture.h"
+#include "ogldev_world_transform.h"
+#include "ogldev_basic_mesh.h"
+#include "camera.h"
+
+#define WINDOW_WIDTH  2560
+#define WINDOW_HEIGHT 1440
+
+
+
+class Tutorial18
+{
+public:
+    Tutorial18();
+    ~Tutorial18();
+
+    bool Init();
+
+    void RenderSceneCB();
+    void KeyboardCB(unsigned char key, int mouse_x, int mouse_y);
+    void SpecialKeyboardCB(int key, int mouse_x, int mouse_y);
+    void PassiveMouseCB(int x, int y);
+
+private:
+
+    void CompileShaders();
+    void AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType);
+
+    GLuint WVPLocation;
+    GLuint SamplerLocation;
+    Camera* pGameCamera = NULL;
+    BasicMesh* pMesh = NULL;
+    PersProjInfo persProjInfo;
+};
+
+
+Tutorial18::Tutorial18()
+{
+    GLclampf Red = 0.0f, Green = 0.0f, Blue = 0.0f, Alpha = 0.0f;
+    glClearColor(Red, Green, Blue, Alpha);
+
+    glEnable(GL_CULL_FACE);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
+
+    glEnable(GL_DEPTH_TEST);
+
+    float FOV = 45.0f;
+    float zNear = 1.0f;
+    float zFar = 100.0f;
+
+    persProjInfo = { FOV, (float)WINDOW_WIDTH, (float)WINDOW_HEIGHT, zNear, zFar };
+}
+
+
+Tutorial18::~Tutorial18()
+{
+    if (pGameCamera) {
+        delete pGameCamera;
+    }
+
+    if (pMesh) {
+        delete pMesh;
+    }
+}
+
+
+bool Tutorial18::Init()
+{
+    CompileShaders();
+
+    Vector3f CameraPos(0.0f, 0.0f, -1.0f);
+    Vector3f CameraTarget(0.0f, 0.0f, 1.0f);
+    Vector3f CameraUp(0.0f, 1.0f, 0.0f);
+
+    pGameCamera = new Camera(WINDOW_WIDTH, WINDOW_HEIGHT, CameraPos, CameraTarget, CameraUp);
+
+    pMesh = new BasicMesh();
+
+    if (!pMesh->LoadMesh("/home/emeiri/Downloads/wine_barrel_01_4k.blend/wine_barrel_01_4k.obj")) {
+        return false;
+    }
+
+    return true;
+}
+
+
+void Tutorial18::RenderSceneCB()
+{
+    glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+    pGameCamera->OnRender();
+
+#ifdef _WIN64
+    float YRotationAngle = 0.1f;
+#else
+    float YRotationAngle = 1.0f;
+#endif
+
+    WorldTrans& worldTransform = pMesh->GetWorldTransform();
+
+    //    worldTransform.SetScale(0.01f);
+    worldTransform.SetPosition(0.0f, 0.0f, 2.0f);
+    worldTransform.Rotate(0.0f, YRotationAngle, 0.0f);
+
+    Matrix4f World = worldTransform.GetMatrix();
+
+    Matrix4f View = pGameCamera->GetMatrix();
+
+    Matrix4f Projection;
+    Projection.InitPersProjTransform(persProjInfo);
+
+    Matrix4f WVP = Projection * View * World;
+    glUniformMatrix4fv(WVPLocation, 1, GL_TRUE, &WVP.m[0][0]);
+
+    pMesh->Render();
+
+    glutPostRedisplay();
+
+    glutSwapBuffers();
+}
+
+
+void Tutorial18::KeyboardCB(unsigned char key, int mouse_x, int mouse_y)
+{
+    switch (key) {
+    case 'q':
+    case 27:    // escape key code
+        exit(0);
+    }
+
+    pGameCamera->OnKeyboard(key);
+}
+
+
+void Tutorial18::SpecialKeyboardCB(int key, int mouse_x, int mouse_y)
+{
+    pGameCamera->OnKeyboard(key);
+}
+
+
+void Tutorial18::PassiveMouseCB(int x, int y)
+{
+    pGameCamera->OnMouse(x, y);
+}
+
+
+void Tutorial18::AddShader(GLuint ShaderProgram, const char* pShaderText, GLenum ShaderType)
+{
+    GLuint ShaderObj = glCreateShader(ShaderType);
+
+    if (ShaderObj == 0) {
+        fprintf(stderr, "Error creating shader type %d\n", ShaderType);
+        exit(1);
+    }
+
+    const GLchar* p[1];
+    p[0] = pShaderText;
+
+    GLint Lengths[1];
+    Lengths[0] = (GLint)strlen(pShaderText);
+
+    glShaderSource(ShaderObj, 1, p, Lengths);
+
+    glCompileShader(ShaderObj);
+
+    GLint success;
+    glGetShaderiv(ShaderObj, GL_COMPILE_STATUS, &success);
+
+    if (!success) {
+        GLchar InfoLog[1024];
+        glGetShaderInfoLog(ShaderObj, 1024, NULL, InfoLog);
+        fprintf(stderr, "Error compiling shader type %d: '%s'\n", ShaderType, InfoLog);
+        exit(1);
+    }
+
+    glAttachShader(ShaderProgram, ShaderObj);
+}
+
+
+void Tutorial18::CompileShaders()
+{
+    GLuint ShaderProgram = glCreateProgram();
+
+    if (ShaderProgram == 0) {
+        fprintf(stderr, "Error creating shader program\n");
+        exit(1);
+    }
+
+    std::string vs, fs;
+
+    if (!ReadFile("shader.vs", vs)) {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, vs.c_str(), GL_VERTEX_SHADER);
+
+    if (!ReadFile("shader.fs", fs)) {
+        exit(1);
+    };
+
+    AddShader(ShaderProgram, fs.c_str(), GL_FRAGMENT_SHADER);
+
+    GLint Success = 0;
+    GLchar ErrorLog[1024] = { 0 };
+
+    glLinkProgram(ShaderProgram);
+
+    glGetProgramiv(ShaderProgram, GL_LINK_STATUS, &Success);
+    if (Success == 0) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Error linking shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    WVPLocation = glGetUniformLocation(ShaderProgram, "gWVP");
+    if (WVPLocation == -1) {
+        printf("Error getting uniform location of 'gWVP'\n");
+        exit(1);
+    }
+
+    SamplerLocation = glGetUniformLocation(ShaderProgram, "gSampler");
+    if (SamplerLocation == -1) {
+        printf("Error getting uniform location of 'gSampler'\n");
+        exit(1);
+    }
+
+    glValidateProgram(ShaderProgram);
+    glGetProgramiv(ShaderProgram, GL_VALIDATE_STATUS, &Success);
+    if (!Success) {
+        glGetProgramInfoLog(ShaderProgram, sizeof(ErrorLog), NULL, ErrorLog);
+        fprintf(stderr, "Invalid shader program: '%s'\n", ErrorLog);
+        exit(1);
+    }
+
+    glUseProgram(ShaderProgram);
+}
+
+
+Tutorial18* pTutorial18 = NULL;
+
+
+void RenderSceneCB()
+{
+    pTutorial18->RenderSceneCB();
+}
+
+
+void KeyboardCB(unsigned char key, int mouse_x, int mouse_y)
+{
+    pTutorial18->KeyboardCB(key, mouse_x, mouse_y);
+}
+
+
+void SpecialKeyboardCB(int key, int mouse_x, int mouse_y)
+{
+    pTutorial18->SpecialKeyboardCB(key, mouse_x, mouse_y);
+}
+
+
+void PassiveMouseCB(int x, int y)
+{
+    pTutorial18->PassiveMouseCB(x, y);
+}
+
+
+void InitializeGlutCallbacks()
+{
+    glutDisplayFunc(RenderSceneCB);
+    glutKeyboardFunc(KeyboardCB);
+    glutSpecialFunc(SpecialKeyboardCB);
+    glutPassiveMotionFunc(PassiveMouseCB);
+}
+
+int main(int argc, char** argv)
+{
+#ifdef _WIN64
+    srand(GetCurrentProcessId());
+#else
+    srandom(getpid());
+#endif
+
+    glutInit(&argc, argv);
+    glutInitContextVersion(3, 3);
+    glutInitContextProfile(GLUT_CORE_PROFILE);
+    glutInitDisplayMode(GLUT_DOUBLE|GLUT_RGBA);
+    glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+
+    int x = 200;
+    int y = 100;
+    glutInitWindowPosition(x, y);
+    int win = glutCreateWindow("Tutorial 18");
+    printf("window id: %d\n", win);
+
+    char game_mode_string[64];
+    // Game mode string example: 1920x1080@32
+    // Enable the following three lines for full screen
+    // snprintf(game_mode_string, sizeof(game_mode_string), "%dx%d@32", WINDOW_WIDTH, WINDOW_HEIGHT);
+    // glutGameModeString(game_mode_string);
+    // glutEnterGameMode();
+
+    // Must be done after glut is initialized!
+    GLenum res = glewInit();
+    if (res != GLEW_OK) {
+        fprintf(stderr, "Error: '%s'\n", glewGetErrorString(res));
+        return 1;
+    }
+
+    InitializeGlutCallbacks();
+
+    pTutorial18 = new Tutorial18();
+
+    if (!pTutorial18->Init()) {
+        return 1;
+    }
+
+    glutMainLoop();
+
+    return 0;
+}
