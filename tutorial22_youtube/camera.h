b2c6733605fa118f60389d22a7b5244e0f3d482d
Etay Meiri <etay.meiri@gmail.com>
2021-10-09 22:42:09 +0300

    initial commit of tutorial 22 for youtube

---
+++ tutorial22_youtube/camera.h
@@ -0,0 +1,68 @@
+/*
+
+        Copyright 2021 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef CAMERA_H
+#define CAMERA_H
+
+#include "ogldev_math_3d.h"
+
+class Camera
+{
+public:
+
+    Camera(int WindowWidth, int WindowHeight);
+
+    Camera(int WindowWidth, int WindowHeight, const Vector3f& Pos, const Vector3f& Target, const Vector3f& Up);
+
+    void SetPosition(float x, float y, float z);
+
+    void OnKeyboard(unsigned char key);
+
+    void OnMouse(int x, int y);
+
+    void OnRender();
+
+    Matrix4f GetMatrix();
+
+    const Vector3f& GetPos() { return m_pos; }
+
+private:
+
+    void Init();
+    void Update();
+
+    Vector3f m_pos;
+    Vector3f m_target;
+    Vector3f m_up;
+    float m_speed = 1.0f;
+
+    int m_windowWidth;
+    int m_windowHeight;
+
+    float m_AngleH;
+    float m_AngleV;
+
+    bool m_OnUpperEdge;
+    bool m_OnLowerEdge;
+    bool m_OnLeftEdge;
+    bool m_OnRightEdge;
+
+    Vector2i m_mousePos;
+};
+
+#endif  /* CAMERA_H */
