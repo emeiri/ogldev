ed041a71e1da79333b93b39898e98231006e62b4
Etay Meiri <etay.meiri@gmail.com>
2021-11-20 22:47:18 +0200

    example 1 wip

--- tutorial24_youtube/assimp_sandbox.cpp
+++ tutorial24_youtube/assimp_sandbox.cpp
@@ -6,108 +6,62 @@
 #define ASSIMP_LOAD_FLAGS (aiProcess_Triangulate | aiProcess_GenNormals |  aiProcess_JoinIdenticalVertices )
 
 
-void print_assimp_matrix_4x4(const aiMatrix4x4& m)
-{
-    printf("%f %f %f %f\n", m.a1, m.a2, m.a3, m.a4);
-    printf("%f %f %f %f\n", m.b1, m.b2, m.b3, m.b4);
-    printf("%f %f %f %f\n", m.c1, m.c2, m.c3, m.c4);
-    printf("%f %f %f %f\n", m.d1, m.d2, m.d3, m.d4);
-}
-
-
-void count_vertices_and_indices(const aiScene* pScene)
-{
-    uint NumVertices = 0;
-    uint NumIndices = 0;
-
-    // Count the number of vertices and indices
-    for (int i = 0 ; i < pScene->mNumMeshes ; i++) {
-        printf("Mesh %s\n", pScene->mMeshes[i]->mName.C_Str());
-        printf("Num animations %d\n", pScene->mNumAnimations);
-        NumVertices += pScene->mMeshes[i]->mNumVertices;
-        NumIndices  += pScene->mMeshes[i]->mNumFaces * 3;
-        for (int v = 0 ; v < NumVertices ; v++) {
-            const aiVector3D& vertex = pScene->mMeshes[i]->mVertices[v];
-            //   printf("%f %f %f\n", vertex.x, vertex.y, vertex.z);
-        }
-    }
-
-    printf("Num vertices %d num indices %d\n", NumVertices, NumIndices);
-}
-
-
 void parse_single_bone(int bone_index, const aiBone* pBone)
 {
     printf(" Bone %d: '%s'\n", bone_index, pBone->mName.C_Str());
     printf("   Num vertices affected by this bone: %d\n", pBone->mNumWeights);
-    printf("   Offset matrix\n");
-    print_assimp_matrix_4x4(pBone->mOffsetMatrix);
 }
 
 
-void parse_bones(const aiMesh* paiMesh)
+void parse_mesh_bones(const aiMesh* pMesh)
 {
-    printf("Parsing bones mesh '%s'\n", paiMesh->mName.C_Str());
+    printf("*******************************************************\n");
+    printf("Parsing %d bones in mesh '%s'\n", pMesh->mNumBones, pMesh->mName.C_Str());
 
-    for (int i = 0 ; i < paiMesh->mNumBones ; i++) {
-        parse_single_bone(i, paiMesh->mBones[i]);
+    for (int i = 0 ; i < pMesh->mNumBones ; i++) {
+        parse_single_bone(i, pMesh->mBones[i]);
     }
 }
 
 
 void parse_bones(const aiScene* pScene)
 {
-    printf("Parsing the bones\n");
     for (uint i = 0 ; i < pScene->mNumMeshes ; i++) {
         const aiMesh* pMesh = pScene->mMeshes[i];
 
         if (pMesh->HasBones()) {
-            parse_bones(pMesh);
+            parse_mesh_bones(pMesh);
         }
     }
     printf("\n");
 }
 
 
-int depth = 0;
-
-
-void parse_heirarchy(const aiNode* pNode)
+void parse_meshes(const aiScene* pScene)
 {
-    if (!pNode) {
-        return;
+    printf("*******************************************************\n");
+    printf("Parsing %d meshes\n\n", pScene->mNumMeshes);
+
+    uint total_vertices = 0;
+    uint total_indices = 0;
+
+    for (int i = 0 ; i < pScene->mNumMeshes ; i++) {
+        int num_vertices = pScene->mMeshes[i]->mNumVertices;
+        int num_indices = pScene->mMeshes[i]->mNumFaces * 3;
+        printf("Mesh %s: vertices %d indices %d\n", pScene->mMeshes[i]->mName.C_Str(), num_vertices, num_indices);
+        total_vertices += num_vertices;
+        total_indices  += num_indices;
     }
 
-    for (int d = 0 ; d < depth ; d++) {
-        printf(" ");
-    }
-
-    printf("Node: '%s (", pNode->mName.C_Str());
-    //    printf("  ");
-    for (int i = 0 ; i < pNode->mNumChildren ; i++) {
-        printf("%s ", pNode->mChildren[i]->mName.C_Str());
-    }
-    printf(")\n");
-
-    depth++;
-    for (int i = 0 ; i < pNode->mNumChildren ; i++) {
-        parse_heirarchy(pNode->mChildren[i]);
-    }
-    depth--;
+    printf("\nTotal vertices %d total indices %d\n", total_vertices, total_indices);
 }
+
 
 void parse_scene(const aiScene* pScene)
 {
-    printf("Global inverse transpose\n");
-    print_assimp_matrix_4x4(pScene->mRootNode->mTransformation);
-
-    count_vertices_and_indices(pScene);
+    parse_meshes(pScene);
 
     parse_bones(pScene);
-
-    printf("Parse the heirarchy\n");
-    parse_heirarchy(pScene->mRootNode);
-    printf("\n");
 }
 
 int main(int argc, char* argv[])
