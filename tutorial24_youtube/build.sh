0cb6d98239448bc6973f3d1bef973122c9f99788
Etay Meiri <etay.meiri@gmail.com>
2021-11-20 22:30:11 +0200

    Initial commit of tutorial24 for youtube

---
+++ tutorial24_youtube/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CC=g++
+CPPFLAGS=`pkg-config --cflags glew ImageMagick++ assimp`
+CPPFLAGS="$CPPFLAGS -I../Include -ggdb3"
+LDFLAGS=`pkg-config --libs glew assimp`
+LDFLAGS="$LDFLAGS -lglut -lX11"
+
+$CC tutorial23.cpp ../Common/ogldev_util.cpp  ../Common/math_3d.cpp ../Common/ogldev_texture.cpp ../Common/3rdparty/stb_image.cpp ../Common/ogldev_world_transform.cpp camera.cpp ../Common/ogldev_basic_mesh.cpp lighting_technique.cpp ../Common/technique.cpp $CPPFLAGS $LDFLAGS -o tutorial23
---
+++ tutorial24_youtube/lighting_technique.h
@@ -0,0 +1,160 @@
+/*
+
+        Copyright 2011 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef LIGHTING_TECHNIQUE_17_H
+#define LIGHTING_TECHNIQUE_17_H
+
+#include "technique.h"
+#include "ogldev_math_3d.h"
+#include "ogldev_material.h"
+#include "ogldev_world_transform.h"
+
+class BaseLight
+{
+public:
+    Vector3f Color = Vector3f(1.0f, 1.0f, 1.0f);
+    float AmbientIntensity = 0.0f;
+    float DiffuseIntensity = 0.0f;
+};
+
+
+class DirectionalLight : public BaseLight
+{
+public:
+    Vector3f WorldDirection = Vector3f(0.0f, 0.0f, 0.0f);
+
+    void CalcLocalDirection(const WorldTrans& worldTransform);
+
+    const Vector3f& GetLocalDirection() const { return LocalDirection; }
+
+private:
+    Vector3f LocalDirection = Vector3f(0.0f, 0.0f, 0.0f);
+};
+
+struct LightAttenuation
+{
+    float Constant = 1.0f;
+    float Linear = 0.0f;
+    float Exp = 0.0f;
+};
+
+
+class PointLight: public BaseLight
+{
+public:
+    Vector3f WorldPosition = Vector3f(0.0f, 0.0f, 0.0f);
+    LightAttenuation Attenuation;
+
+    void CalcLocalPosition(const WorldTrans& worldTransform);
+
+    const Vector3f& GetLocalPosition() const { return LocalPosition; }
+
+private:
+    Vector3f LocalPosition = Vector3f(0.0f, 0.0f, 0.0f);
+};
+
+class SpotLight : public PointLight
+{
+public:
+    Vector3f WorldDirection = Vector3f(0.0f, 0.0f, 0.0f);
+    float Cutoff = 0.0f;
+
+    void CalcLocalDirectionAndPosition(const WorldTrans& worldTransform);
+
+    const Vector3f& GetLocalDirection() const { return LocalDirection; }
+
+private:
+    Vector3f LocalDirection = Vector3f(0.0f, 0.0f, 0.0f);
+
+
+};
+
+
+class LightingTechnique : public Technique
+{
+public:
+
+    static const unsigned int MAX_POINT_LIGHTS = 2;
+    static const unsigned int MAX_SPOT_LIGHTS = 2;
+
+    LightingTechnique();
+
+    virtual bool Init();
+
+    void SetWVP(const Matrix4f& WVP);
+    void SetTextureUnit(unsigned int TextureUnit);
+    void SetSpecularExponentTextureUnit(unsigned int TextureUnit);
+    void SetDirectionalLight(const DirectionalLight& Light);
+    void SetPointLights(unsigned int NumLights, const PointLight* pLights);
+    void SetSpotLights(unsigned int NumLights, const SpotLight* pLights);
+    void SetCameraLocalPos(const Vector3f& CameraLocalPos);
+    void SetMaterial(const Material& material);
+
+private:
+
+    GLuint WVPLoc;
+    GLuint samplerLoc;
+    GLuint samplerSpecularExponentLoc;
+    GLuint CameraLocalPosLoc;
+    GLuint NumPointLightsLocation;
+    GLuint NumSpotLightsLocation;
+
+    struct {
+        GLuint AmbientColor;
+        GLuint DiffuseColor;
+        GLuint SpecularColor;
+    } materialLoc;
+
+    struct {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint Direction;
+        GLuint DiffuseIntensity;
+    } dirLightLoc;
+
+    struct {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint Position;
+        GLuint DiffuseIntensity;
+
+        struct
+        {
+            GLuint Constant;
+            GLuint Linear;
+            GLuint Exp;
+        } Atten;
+    } PointLightsLocation[MAX_POINT_LIGHTS];
+
+struct {
+        GLuint Color;
+        GLuint AmbientIntensity;
+        GLuint DiffuseIntensity;
+        GLuint Position;
+        GLuint Direction;
+        GLuint Cutoff;
+        struct {
+            GLuint Constant;
+            GLuint Linear;
+            GLuint Exp;
+        } Atten;
+    } SpotLightsLocation[MAX_SPOT_LIGHTS];
+};
+
+
+#endif  /* LIGHTING_TECHNIQUE_H */
