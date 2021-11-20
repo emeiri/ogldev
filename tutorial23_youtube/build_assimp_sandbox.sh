aee1ad6ffdff36e96245573f5e357eb02343b795
Etay Meiri <etay.meiri@gmail.com>
2021-11-20 22:46:52 +0200

    cleanup

--- tutorial23_youtube/build_assimp_sandbox.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CC=g++
-CPPFLAGS=`pkg-config --cflags assimp`
-CPPFLAGS="$CPPFLAGS -I../Include -ggdb3"
-LDFLAGS=`pkg-config --libs assimp`
-
-
-$CC assimp_sandbox.cpp  $CPPFLAGS $LDFLAGS -o assimp_sandbox
