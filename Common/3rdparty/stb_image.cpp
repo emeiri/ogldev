bc6832e570fc2a9981f3d49143b9b13630022509
Etay Meiri <etay.meiri@gmail.com>
2022-03-14 15:03:33 +0200

    Added stb_image_write.

--- Common/3rdparty/stb_image.cpp
+++ Common/3rdparty/stb_image.cpp
@@ -1,3 +1,5 @@
 #define STB_IMAGE_IMPLEMENTATION
 #define STBI_FAILURE_USERMSG // optional
 #include "3rdparty/stb_image.h"
+ #define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "3rdparty/stb_image_write.h"
