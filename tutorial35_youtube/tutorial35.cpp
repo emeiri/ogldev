6ae3e3d5c62cd98b60367c251e3c0bb76a5f3f25
Etay Meiri <etay.meiri@gmail.com>
2022-05-20 10:04:43 +0300

    render the terrain

--- tutorial35_youtube/tutorial35.cpp
+++ tutorial35_youtube/tutorial35.cpp
@@ -150,8 +150,9 @@ public:
         static float foo = 0.0f;
         foo += 0.002f;
 
+        // Render the main object
         WorldTrans& worldTransform = m_pMesh1->GetWorldTransform();
-        worldTransform.SetPosition(0.0f, 0.0f, 10.0f);
+        worldTransform.SetPosition(0.0f, 1.0f, 10.0f);
 
         Matrix4f World = m_pMesh1->GetWorldMatrix();
         Matrix4f View = m_pGameCamera->GetMatrix();
@@ -172,11 +173,22 @@ public:
         //Vector3f Target(m_pMesh1->GetPosition() - m_pGameCamera->GetPos() + Vector3f(0.0f, 3.0f, 0.0f));
         //m_pGameCamera->SetTarget(Target);
 
-        //m_dirLight.WorldDirection = Vector3f(sinf(foo), -0.5f, cosf(foo));
-        //        m_phongRenderer.UpdateDirLightDir(m_dirLight.WorldDirection);
-        //m_phongRenderer.Render(m_pMesh1);
+        // Render the terrain
+        WorldTrans& worldTransformTerrain = m_pTerrain->GetWorldTransform();
+        worldTransformTerrain.SetPosition(0.0f, 0.0f, 0.0f);
 
-        //m_phongRenderer.Render(m_pTerrain);
+        World = m_pTerrain->GetWorldMatrix();
+        WVP = Projection * View * World;
+        m_lightingTech.SetWVP(WVP);
+
+        m_spotLight.CalcLocalDirectionAndPosition(worldTransformTerrain);
+        m_lightingTech.SetSpotLights(1, &m_spotLight);
+        m_lightingTech.SetMaterial(m_pTerrain->GetMaterial());
+
+        CameraLocalPos3f = worldTransformTerrain.WorldPosToLocalPos(m_pGameCamera->GetPos());
+        m_lightingTech.SetCameraLocalPos(CameraLocalPos3f);
+
+        m_pTerrain->Render();
     }
 
 
@@ -264,7 +276,7 @@ private:
 
     void InitCamera()
     {
-        Vector3f Pos(0.0f, 0.0f, 0.0f);
+        Vector3f Pos(0.0f, 1.0f, 0.0f);
         Vector3f Target(0.0f, 0.0f, 1.0f);
         Vector3f Up(0.0, 1.0f, 0.0f);
 
@@ -302,7 +314,6 @@ private:
         //m_pMesh1->LoadMesh("../Content/ordinary_house/ordinary_house.obj");
         m_pMesh1->LoadMesh("../Content/box.obj");
 
-        m_pMesh1->SetPosition(0.0f, 0.0f, 10.0f);
         //        m_pMesh1->SetRotation(270.0f, 180.0f, 0.0f);
 
         m_pTerrain = new BasicMesh();
