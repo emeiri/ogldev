c59d00435ad73f2077ea194ebcd6e2928abdb5c1
Etay Meiri <etay.meiri@gmail.com>
2022-08-09 22:38:29 +0300

    Fixed comment

--- tutorial39_youtube/tutorial39.cpp
+++ tutorial39_youtube/tutorial39.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Toon Shading & Rim Lighting
+    Fog Rendering
 */
 
 #include <stdio.h>
