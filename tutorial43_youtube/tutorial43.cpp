d48fdcb6cc0860c96fb4bc466412a3d5984c9f6a
Etay Meiri <etay.meiri@gmail.com>
2022-10-16 00:41:55 +0300

    Added tutorial 43 for youtube

---
+++ tutorial43_youtube/tutorial43.cpp
@@ -0,0 +1,337 @@
+/*
+
+        Copyright 2022 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+    Physically Based Rendering #1 (intro)
+*/
+
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <GL/glew.h>
+
+
+#include "ogldev_engine_common.h"
+#include "ogldev_util.h"
+#include "ogldev_basic_glfw_camera.h"
+#include "ogldev_new_lighting.h"
+#include "ogldev_glfw.h"
+#include "ogldev_basic_mesh.h"
+#include "ogldev_world_transform.h"
+#include "ogldev_phong_renderer.h"
+
+#define WINDOW_WIDTH  1920
+#define WINDOW_HEIGHT 1080
+
+static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods);
+static void CursorPosCallback(GLFWwindow* window, double x, double y);
+static void MouseButtonCallback(GLFWwindow* window, int Button, int Action, int Mode);
+
+
+class Tutorial43
+{
+public:
+
+    Tutorial43()
+    {
+        m_dirLight.WorldDirection = Vector3f(0.0f, -1.0f, 1.0f);
+        m_dirLight.DiffuseIntensity = 1.0f;
+        m_dirLight.AmbientIntensity = 0.5f;
+    }
+
+    virtual ~Tutorial43()
+    {
+        SAFE_DELETE(m_pGameCamera);
+    }
+
+
+    void Init()
+    {
+        CreateWindow();
+
+        InitCallbacks();
+
+        InitCamera();
+
+        InitMesh();
+
+        InitRenderer();
+
+        m_startTime = GetCurrentTimeMillis();
+        m_currentTime = m_startTime;
+    }
+
+
+    void Run()
+    {
+        while (!glfwWindowShouldClose(window)) {
+            RenderSceneCB();
+            glfwSwapBuffers(window);
+            glfwPollEvents();
+        }
+    }
+
+
+    void RenderSceneCB()
+    {
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        m_pGameCamera->OnRender();
+
+        static float foo = 0.0f;
+        foo += 0.003f;
+
+        //Vector3f CameraTarget = m_pMesh->GetPosition() - m_pGameCamera->GetPos();
+        //m_pGameCamera->SetPosition(45.0f * sinf(foo), 12.0f, 45.0 * cosf(foo));
+        //m_pGameCamera->SetTarget(CameraTarget);
+
+        if (m_runAnimation) {
+            m_currentTime = GetCurrentTimeMillis();
+        }
+
+        float AnimationTimeSec = (float)((double)m_currentTime - (double)m_startTime) / 1000.0f;
+
+        float TotalPauseTimeSec = (float)((double)m_totalPauseTime / 1000.0f);
+        AnimationTimeSec -= TotalPauseTimeSec;
+
+        static float BlendFactor = 0.0f;
+        static float BlendDirection = 0.0001f;
+
+        m_phongRenderer.RenderAnimationBlended(m_pMesh, AnimationTimeSec, 0, 3, m_blendFactor);
+        //m_phongRenderer.RenderAnimation(m_pMesh, AnimationTimeSec, m_animationIndex);
+
+        BlendFactor += BlendDirection;
+
+        if (BlendDirection > 0.0f) {
+            if (BlendFactor >= 0.9f || BlendFactor <= 0.1f) {
+                BlendDirection = 0.0002f;
+            } else {
+                BlendDirection = 0.001f;
+            }
+        } else {
+            if (BlendFactor >= 0.9f || BlendFactor <= 0.1f) {
+                BlendDirection = -0.0002f;
+            } else {
+                BlendDirection = -0.001f;
+            }
+        }
+
+        if (BlendFactor > 1.0f || BlendFactor < 0.0f) {
+            BlendDirection *= -1.0f;
+        }
+
+        if (BlendFactor < 0.0f) {
+            BlendFactor = 0.0f;
+        } else if (BlendFactor > 1.0f) {
+            BlendFactor = 1.0f;
+        }
+
+        m_phongRenderer.Render(m_pTerrain);
+    }
+
+
+#define ATTEN_STEP 0.01f
+
+#define ANGLE_STEP 1.0f
+
+    void PassiveMouseCB(int x, int y)
+    {
+        m_pGameCamera->OnMouse(x, y);
+    }
+
+    void KeyboardCB(uint key, int state)
+    {
+        if (key == GLFW_KEY_A) {
+            m_blendFactor += 0.005f;
+            if (m_blendFactor > 1.0f) {
+                m_blendFactor = 1.0f;
+            }
+            printf("%f\n", m_blendFactor);
+            return;
+        } else if (key == GLFW_KEY_Z) {
+            m_blendFactor -= 0.005f;
+            if (m_blendFactor < 0.0f) {
+                m_blendFactor = 0.0f;
+            }
+            printf("%f\n", m_blendFactor);
+            return;
+        }
+
+        if (state == GLFW_PRESS) {
+            switch (key) {
+            case GLFW_KEY_0:
+                m_animationIndex = 0;
+                break;
+
+            case GLFW_KEY_1:
+                m_animationIndex = 1;
+                break;
+
+            case GLFW_KEY_2:
+                m_animationIndex = 2;
+                break;
+
+            case GLFW_KEY_3:
+                m_animationIndex = 3;
+                break;
+
+            case GLFW_KEY_SPACE:
+                m_runAnimation = !m_runAnimation;
+                if (m_runAnimation) {
+                    long long CurrentTime = GetCurrentTimeMillis();
+                    // printf("Resumed at %lld\n", CurrentTime);
+                    m_totalPauseTime += (CurrentTime - m_pauseStart);
+                    // printf("Total pause time %lld\n", m_totalPauseTime);
+                } else {
+                    m_pauseStart = GetCurrentTimeMillis();
+                    // printf("Paused at %lld\n", GetCurrentTimeMillis());
+                }
+                break;
+
+            case GLFW_KEY_ESCAPE:
+            case GLFW_KEY_Q:
+                glfwDestroyWindow(window);
+                glfwTerminate();
+                exit(0);
+            }
+        }
+
+        m_pGameCamera->OnKeyboard(key);
+    }
+
+
+    void MouseCB(int button, int action, int x, int y)
+    {
+    }
+
+
+private:
+
+    void CreateWindow()
+    {
+        int major_ver = 0;
+        int minor_ver = 0;
+        bool is_full_screen = false;
+        window = glfw_init(major_ver, minor_ver, WINDOW_WIDTH, WINDOW_HEIGHT, is_full_screen, "Tutorial 43");
+
+        glfwSetCursorPos(window, WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2);
+    }
+
+
+    void InitCallbacks()
+    {
+        glfwSetKeyCallback(window, KeyCallback);
+        glfwSetCursorPosCallback(window, CursorPosCallback);
+        glfwSetMouseButtonCallback(window, MouseButtonCallback);
+    }
+
+
+    void InitCamera()
+    {
+        Vector3f Pos(0.0f, 5.0f, -30.0f);
+        Vector3f Target(0.0f, 0.0f, 1.0f);
+        Vector3f Up(0.0, 1.0f, 0.0f);
+
+        float FOV = 45.0f;
+        float zNear = 0.1f;
+        float zFar = 1000.0f;
+        PersProjInfo persProjInfo = { FOV, (float)WINDOW_WIDTH, (float)WINDOW_HEIGHT, zNear, zFar };
+
+        m_pGameCamera = new BasicCamera(persProjInfo, Pos, Target, Up);
+    }
+
+
+    void InitRenderer()
+    {
+        m_phongRenderer.InitPhongRenderer();
+        m_phongRenderer.SetCamera(m_pGameCamera);
+        m_phongRenderer.SetDirLight(m_dirLight);
+    }
+
+
+    void InitMesh()
+    {
+        m_pMesh = new SkinnedMesh();
+        m_pMesh->LoadMesh("../Content/iclone-7-raptoid-mascot/scene.gltf");
+        m_pMesh->SetRotation(90.0f, 0.0f, 0.0f);
+        m_pMesh->SetPosition(0.0f, 1.2f, 0.0f);
+        m_pMesh->SetScale(0.05f);
+
+        m_pTerrain = new BasicMesh();
+        if (!m_pTerrain->LoadMesh("../Content/terrain_rock_boulder_cracked.obj")) {
+            printf("Error loading mesh ../Content/terrain_rock_boulder_cracked.obj");
+            exit(0);
+        }
+    }
+
+    GLFWwindow* window = NULL;
+    BasicCamera* m_pGameCamera = NULL;
+    PhongRenderer m_phongRenderer;
+    SkinnedMesh* m_pMesh = NULL;
+    BasicMesh* m_pTerrain = NULL;
+    PersProjInfo m_persProjInfo;
+    DirectionalLight m_dirLight;
+    long long m_startTime = 0;
+    long long m_currentTime = 0;
+    bool m_runAnimation = true;
+    long long m_totalPauseTime = 0;
+    long long m_pauseStart = 0;
+    int m_animationIndex = 0;
+    float m_blendFactor = 0.0f;
+};
+
+Tutorial43* app = NULL;
+
+static void KeyCallback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    app->KeyboardCB(key, action);
+}
+
+
+static void CursorPosCallback(GLFWwindow* window, double x, double y)
+{
+    app->PassiveMouseCB((int)x, (int)y);
+}
+
+
+static void MouseButtonCallback(GLFWwindow* window, int Button, int Action, int Mode)
+{
+    double x, y;
+
+    glfwGetCursorPos(window, &x, &y);
+
+    app->MouseCB(Button, Action, (int)x, (int)y);
+}
+
+
+int main(int argc, char** argv)
+{
+    app = new Tutorial43();
+
+    app->Init();
+
+    glClearColor(0.1f, 0.2f, 0.1f, 0.0f);
+    glFrontFace(GL_CW);
+    glCullFace(GL_BACK);
+    glEnable(GL_CULL_FACE);
+    glEnable(GL_DEPTH_TEST);
+
+    app->Run();
+
+    delete app;
+
+    return 0;
+}
