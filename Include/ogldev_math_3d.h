3666f891a3b3232808f29d7e68d412d80e10d990
Etay Meiri <etay.meiri@gmail.com>
2022-10-30 08:51:17 +0200

    Added operator* for Matrix3f

--- Include/ogldev_math_3d.h
+++ Include/ogldev_math_3d.h
@@ -578,6 +578,21 @@ public:
         return r;
     }
 
+    inline Matrix3f operator*(const Matrix3f& Right) const
+    {
+        Matrix3f Ret;
+
+        for (unsigned int i = 0 ; i < 3 ; i++) {
+            for (unsigned int j = 0 ; j < 3 ; j++) {
+                Ret.m[i][j] = m[i][0] * Right.m[0][j] +
+                              m[i][1] * Right.m[1][j] +
+                              m[i][2] * Right.m[2][j];
+            }
+        }
+
+        return Ret;
+    }
+
     Matrix3f Transpose() const
     {
         Matrix3f n;
