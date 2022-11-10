55b7cc2c6c10e80fd5507c239fc2b36626e4d1e1
Etay Meiri <etay.meiri@gmail.com>
2022-11-10 14:41:05 +0200

    Added the skydome technique

---
+++ Common/Shaders/skydome.vs
@@ -0,0 +1,15 @@
+#version 410
+
+layout (location = 0) in vec3 Position;
+layout (location = 1) in vec2 TexCoords;
+
+uniform mat4 gWVP = mat4(1.0);
+
+out vec2 TexCoords0;
+
+void main()
+{
+    gl_Position = gWVP * vec4(Position, 1.0);
+    gl_Position.z = gl_Position.w;
+    TexCoords0 = TexCoords;
+}
---
+++ Common/ogldev_skydome_technique.cpp
@@ -0,0 +1,64 @@
+/*
+    Copyright 2022 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ogldev_skydome_technique.h"
+
+
+SkydomeTechnique::SkydomeTechnique()
+{
+}
+
+bool SkydomeTechnique::Init()
+{
+    if (!Technique::Init()) {
+        return false;
+    }
+
+    if (!AddShader(GL_VERTEX_SHADER, "../Common/Shaders/skydome.vs")) {
+        return false;
+    }
+
+    if (!AddShader(GL_FRAGMENT_SHADER, "../Common/Shaders/tex.fs")) {
+        return false;
+    }
+
+    if (!Finalize()) {
+        return false;
+    }
+
+    m_WVPLoc = GetUniformLocation("gWVP");
+    m_samplerLoc = GetUniformLocation("gSampler");
+
+    if (m_WVPLoc == INVALID_UNIFORM_LOCATION ||
+        m_samplerLoc == INVALID_UNIFORM_LOCATION) {
+        return false;
+    }
+
+    return true;
+}
+
+
+void SkydomeTechnique::SetTextureUnit(unsigned int TextureUnit)
+{
+    glUniform1i(m_samplerLoc, TextureUnit);
+}
+
+
+void SkydomeTechnique::SetWVP(const Matrix4f& WVP)
+{
+    glUniformMatrix4fv(m_WVPLoc, 1, GL_TRUE, (const GLfloat*)WVP.m);
+}
