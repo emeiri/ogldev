02d474d29a79dd8f4dbc0dab2b99b44e641fcf35
Etay Meiri <etay.meiri@gmail.com>
2023-03-04 15:39:59 +0200

    Added powi function

--- Include/ogldev_math_3d.h
+++ Include/ogldev_math_3d.h
@@ -38,6 +38,8 @@
 #include <assimp/matrix3x3.h>
 #include <assimp/matrix4x4.h>
 
+// powf wrapper for integer params to avoid crazy casting
+#define powi(base,exp) (int)powf((float)(base), (float)(exp))
 
 #define ToRadian(x) (float)(((x) * M_PI / 180.0f))
 #define ToDegree(x) (float)(((x) * 180.0f / M_PI))
