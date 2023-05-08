4cdf04912691f3e8e4b25d32b46d9449d0ba36e3
Etay Meiri <etay.meiri@gmail.com>
2023-05-09 01:56:40 +0300

    Added a vulkan tutorial

---
+++ VulkanTutorials/Tutorial01/build.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+CC=g++
+CPPFLAGS=`pkg-config --cflags glew ImageMagick++ freetype2 glfw3 fontconfig assimp`
+CPPFLAGS="$CPPFLAGS -I../Include -I../Common/FreetypeGL -ggdb3 -DVULKAN"
+LDFLAGS=`pkg-config --libs glew ImageMagick++ freetype2 glfw3 fontconfig assimp vulkan xcb`
+LDFLAGS="$LDFLAGS -lglut ../Lib/libAntTweakBar.a -lX11  "
+
+$CC tutorial50.cpp ../Common/ogldev_vulkan.cpp ../Common/ogldev_vulkan_core.cpp ../Common/ogldev_xcb_control.cpp ../Common/ogldev_basic_mesh.cpp ../Common/ogldev_basic_lighting.cpp ../Common/ogldev_util.cpp ../Common/pipeline.cpp ../Common/math_3d.cpp ../Common/camera.cpp ../Common/ogldev_atb.cpp ../Common/ogldev_backend.cpp ../Common/ogldev_glfw_backend.cpp ../Common/glut_backend.cpp ../Common/ogldev_texture.cpp  ../Common/technique.cpp ../Common/ogldev_app.cpp ../Common/FreetypeGL/freetypeGL.cpp ../Common/3rdparty/stb_image.cpp $CPPFLAGS $LDFLAGS -o tutorial50
