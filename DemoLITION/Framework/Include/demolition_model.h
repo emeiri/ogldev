fa7b8d127a41b3b4f2604845d8d88c0cc5217789
Etay Meiri <etay.meiri@gmail.com>
2023-06-10 23:03:24 +0300

    Added skeletal animation stuff

--- DemoLITION/Framework/Include/demolition_model.h
+++ DemoLITION/Framework/Include/demolition_model.h
@@ -73,6 +73,25 @@ public:
 
     const std::vector<BasicCamera> GetCameras() const { return m_cameras; }
 
+    uint NumBones() const
+    {
+        return (uint)m_BoneNameToIndexMap.size();
+    }
+
+    // This is the main function to drive the animation. It receives the animation time
+    // in seconds and a reference to a vector of transformation matrices (one matrix per bone).
+    // It calculates the current transformation for each bone according to the current time
+    // and updates the corresponding matrix in the vector. This must then be updated in the VS
+    // to be accumulated for the final local position (see skinning.vs). The animation index
+    // is an optional param which selects one of the animations.
+    void GetBoneTransforms(float AnimationTimeSec, vector<Matrix4f>& Transforms, unsigned int AnimationIndex = 0);
+
+    // Same as above but this one blends two animations together based on a blending factor
+    void GetBoneTransformsBlended(float AnimationTimeSec,
+                                  vector<Matrix4f>& Transforms,
+                                  unsigned int StartAnimIndex,
+                                  unsigned int EndAnimIndex,
+                                  float BlendFactor);
 private:
 
     void Clear();
@@ -158,5 +177,105 @@ private:
     Assimp::Importer m_Importer;
 
     std::vector<BasicCamera> m_cameras;
+	
+	// Skeletal animation stuff
+    #define MAX_NUM_BONES_PER_VERTEX 4
+	
+    struct VertexBoneData
+    {
+        uint BoneIDs[MAX_NUM_BONES_PER_VERTEX] = { 0 };
+        float Weights[MAX_NUM_BONES_PER_VERTEX] = { 0.0f };
+        int index = 0;  // slot for the next update
+
+        VertexBoneData()
+        {
+        }
+
+        void AddBoneData(uint BoneID, float Weight)
+        {
+            for (int i = 0 ; i < index ; i++) {
+                if (BoneIDs[i] == BoneID) {
+                    //  printf("bone %d already found at index %d old weight %f new weight %f\n", BoneID, i, Weights[i], Weight);
+                    return;
+                }
+            }
+
+            // The iClone 7 Raptoid Mascot (https://sketchfab.com/3d-models/iclone-7-raptoid-mascot-free-download-56a3e10a73924843949ae7a9800c97c7)
+            // has a problem of zero weights causing an overflow and the assertion below. This fixes it.
+            if (Weight == 0.0f) {
+                return;
+            }
+
+            // printf("Adding bone %d weight %f at index %i\n", BoneID, Weight, index);
+
+            if (index == MAX_NUM_BONES_PER_VERTEX) {
+                return;
+                                assert(0);
+            }
+
+            BoneIDs[index] = BoneID;
+            Weights[index] = Weight;
+
+            index++;
+        }
+    };
+
+    void LoadMeshBones(uint MeshIndex, const aiMesh* paiMesh);
+    void LoadSingleBone(uint MeshIndex, const aiBone* pBone);
+    int GetBoneId(const aiBone* pBone);
+    void CalcInterpolatedScaling(aiVector3D& Out, float AnimationTime, const aiNodeAnim* pNodeAnim);
+    void CalcInterpolatedRotation(aiQuaternion& Out, float AnimationTime, const aiNodeAnim* pNodeAnim);
+    void CalcInterpolatedPosition(aiVector3D& Out, float AnimationTime, const aiNodeAnim* pNodeAnim);
+    uint FindScaling(float AnimationTime, const aiNodeAnim* pNodeAnim);
+    uint FindRotation(float AnimationTime, const aiNodeAnim* pNodeAnim);
+    uint FindPosition(float AnimationTime, const aiNodeAnim* pNodeAnim);
+    const aiNodeAnim* FindNodeAnim(const aiAnimation& Animation, const string& NodeName);
+    void ReadNodeHierarchy(float AnimationTime, const aiNode* pNode, const Matrix4f& ParentTransform, const aiAnimation& Animation);
+    void ReadNodeHierarchyBlended(float StartAnimationTimeTicksm, float EndAnimationTimeTicks, const aiNode* pNode, const Matrix4f& ParentTransform,
+                                  const aiAnimation& StartAnimation, const aiAnimation& EndAnimation, float BlendFactor);
+    void MarkRequiredNodesForBone(const aiBone* pBone);
+    void InitializeRequiredNodeMap(const aiNode* pNode);
+    float CalcAnimationTimeTicks(float TimeInSeconds, unsigned int AnimationIndex);
+
+    struct LocalTransform {
+        aiVector3D Scaling;
+        aiQuaternion Rotation;
+        aiVector3D Translation;
+    };
+
+    void CalcLocalTransform(LocalTransform& Transform, float AnimationTimeTicks, const aiNodeAnim* pNodeAnim);
+
+    GLuint m_boneBuffer = 0;
+
+    // Temporary space for vertex stuff before we load them into the GPU
+    vector<VertexBoneData> m_Bones;
+
+    map<string,uint> m_BoneNameToIndexMap;
+
+    struct BoneInfo
+    {
+        Matrix4f OffsetMatrix;
+        Matrix4f FinalTransformation;
+
+        BoneInfo(const Matrix4f& Offset)
+        {
+            OffsetMatrix = Offset;
+            FinalTransformation.SetZero();
+        }
+    };
+
+    vector<BoneInfo> m_BoneInfo;
+
+    struct NodeInfo {
+
+        NodeInfo() {}
+
+        NodeInfo(const aiNode* n) { pNode = n;}
+
+        const aiNode* pNode = NULL;
+        bool isRequired = false;
+    };
+
+    map<string,NodeInfo> m_requiredNodeMap;
 };
 
--- DemoLITION/Framework/Source/demolition_model.cpp
+++ DemoLITION/Framework/Source/demolition_model.cpp
@@ -24,6 +24,9 @@ using namespace std;
 #define POSITION_LOCATION  0
 #define TEX_COORD_LOCATION 1
 #define NORMAL_LOCATION    2
+#define BONE_ID_LOCATION     3
+#define BONE_WEIGHT_LOCATION 4
+
 
 #define DEMOLITION_ASSIMP_LOAD_FLAGS (aiProcess_Triangulate | aiProcess_GenNormals | aiProcess_JoinIdenticalVertices | aiProcess_MakeLeftHanded)
 
@@ -149,6 +152,8 @@ void DemolitionModel::ReserveSpace(unsigned int NumVertices, unsigned int NumInd
     m_Normals.reserve(NumVertices);
     m_TexCoords.reserve(NumVertices);
     m_Indices.reserve(NumIndices);
+    m_Bones.resize(NumVertices); // TODO: only if there are any bones
+    InitializeRequiredNodeMap(m_pScene->mRootNode);	
 }
 
 
@@ -196,6 +201,8 @@ void DemolitionModel::InitSingleMesh(uint MeshIndex, const aiMesh* paiMesh)
         m_Indices.push_back(Face.mIndices[1]);
         m_Indices.push_back(Face.mIndices[2]);
     }
+
+    LoadMeshBones(MeshIndex, paiMesh);
 }
 
 
@@ -393,6 +400,16 @@ void DemolitionModel::PopulateBuffers()
     glBufferData(GL_ARRAY_BUFFER, sizeof(m_Normals[0]) * m_Normals.size(), &m_Normals[0], GL_STATIC_DRAW);
     glEnableVertexAttribArray(NORMAL_LOCATION);
     glVertexAttribPointer(NORMAL_LOCATION, 3, GL_FLOAT, GL_FALSE, 0, 0);
+	
+    glGenBuffers(1, &m_boneBuffer);
+    glBindBuffer(GL_ARRAY_BUFFER, m_boneBuffer);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(m_Bones[0]) * m_Bones.size(), &m_Bones[0], GL_STATIC_DRAW);
+    glEnableVertexAttribArray(BONE_ID_LOCATION);
+    glVertexAttribIPointer(BONE_ID_LOCATION, MAX_NUM_BONES_PER_VERTEX, GL_INT, sizeof(VertexBoneData), (const GLvoid*)0);
+    glEnableVertexAttribArray(BONE_WEIGHT_LOCATION);
+    glVertexAttribPointer(BONE_WEIGHT_LOCATION, MAX_NUM_BONES_PER_VERTEX, GL_FLOAT, GL_FALSE, sizeof(VertexBoneData),
+                          (const GLvoid*)(MAX_NUM_BONES_PER_VERTEX * sizeof(int32_t)));
+
 
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_Buffers[INDEX_BUFFER]);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(m_Indices[0]) * m_Indices.size(), &m_Indices[0], GL_STATIC_DRAW);
@@ -568,6 +585,15 @@ void DemolitionModel::InitSingleCamera(int Index, const aiCamera* pCamera, int W
     persProjInfo.Height = (float)WindowHeight;
     persProjInfo.FOV = pCamera->mHorizontalFOV;
 
+    aiMatrix4x4 CameraMatrix;
+    pCamera->GetCameraMatrix(CameraMatrix);
+
+    Matrix4f foo(CameraMatrix);
+
+    foo.Print();
+
+  //  exit(0);
+
     float AspectRatio = (float)WindowWidth / (float)WindowHeight;
 
     if (AspectRatio != pCamera->mAspect) {
@@ -581,4 +607,451 @@ void DemolitionModel::InitSingleCamera(int Index, const aiCamera* pCamera, int W
 void DemolitionModel::InitLights(const aiScene* pScene)
 {
 
+}
+
+
+void DemolitionModel::LoadMeshBones(uint MeshIndex, const aiMesh* pMesh)
+{
+    if (pMesh->mNumBones > MAX_BONES) {
+        printf("The number of bones in the model (%d) is larger than the maximum supported (%d)\n", pMesh->mNumBones, MAX_BONES);
+        printf("Make sure to increase the macro MAX_BONES in the C++ header as well as in the shader to the same value\n");
+        assert(0);
+    }
+
+    // printf("Loading mesh bones %d\n", MeshIndex);
+    for (uint i = 0 ; i < pMesh->mNumBones ; i++) {
+        // printf("Bone %d %s\n", i, pMesh->mBones[i]->mName.C_Str());
+        LoadSingleBone(MeshIndex, pMesh->mBones[i]);
+    }
+}
+
+
+void DemolitionModel::LoadSingleBone(uint MeshIndex, const aiBone* pBone)
+{
+    int BoneId = GetBoneId(pBone);
+
+    if (BoneId == m_BoneInfo.size()) {
+        BoneInfo bi(pBone->mOffsetMatrix);
+        // bi.OffsetMatrix.Print();
+        m_BoneInfo.push_back(bi);
+    }
+
+    for (uint i = 0 ; i < pBone->mNumWeights ; i++) {
+        const aiVertexWeight& vw = pBone->mWeights[i];
+        uint GlobalVertexID = m_Meshes[MeshIndex].BaseVertex + pBone->mWeights[i].mVertexId;
+        // printf("%d: %d %f\n",i, pBone->mWeights[i].mVertexId, vw.mWeight);
+        m_Bones[GlobalVertexID].AddBoneData(BoneId, vw.mWeight);
+    }
+
+    MarkRequiredNodesForBone(pBone);
+}
+
+
+void DemolitionModel::MarkRequiredNodesForBone(const aiBone* pBone)
+{
+    string NodeName(pBone->mName.C_Str());
+
+    const aiNode* pParent = NULL;
+
+    do {
+        map<string,NodeInfo>::iterator it = m_requiredNodeMap.find(NodeName);
+
+        if (it == m_requiredNodeMap.end()) {
+            printf("Cannot find bone %s in the hierarchy\n", NodeName.c_str());
+            assert(0);
+        }
+
+        it->second.isRequired = true;
+
+        pParent = it->second.pNode->mParent;
+
+        if (pParent) {
+            NodeName = string(pParent->mName.C_Str());
+        }
+
+    } while (pParent);
+}
+
+
+void DemolitionModel::InitializeRequiredNodeMap(const aiNode* pNode)
+{
+    string NodeName(pNode->mName.C_Str());
+
+    NodeInfo info(pNode);
+
+    m_requiredNodeMap[NodeName] = info;
+
+    for (unsigned int i = 0 ; i < pNode->mNumChildren ; i++) {
+        InitializeRequiredNodeMap(pNode->mChildren[i]);
+    }
+}
+
+
+int DemolitionModel::GetBoneId(const aiBone* pBone)
+{
+    int BoneIndex = 0;
+    string BoneName(pBone->mName.C_Str());
+
+    if (m_BoneNameToIndexMap.find(BoneName) == m_BoneNameToIndexMap.end()) {
+        // Allocate an index for a new bone
+        BoneIndex = (int)m_BoneNameToIndexMap.size();
+        m_BoneNameToIndexMap[BoneName] = BoneIndex;
+    }
+    else {
+        BoneIndex = m_BoneNameToIndexMap[BoneName];
+    }
+
+    return BoneIndex;
+}
+
+
+uint DemolitionModel::FindPosition(float AnimationTimeTicks, const aiNodeAnim* pNodeAnim)
+{
+    for (uint i = 0 ; i < pNodeAnim->mNumPositionKeys - 1 ; i++) {
+        float t = (float)pNodeAnim->mPositionKeys[i + 1].mTime;
+        if (AnimationTimeTicks < t) {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+
+void DemolitionModel::CalcInterpolatedPosition(aiVector3D& Out, float AnimationTimeTicks, const aiNodeAnim* pNodeAnim)
+{
+    // we need at least two values to interpolate...
+    if (pNodeAnim->mNumPositionKeys == 1) {
+        Out = pNodeAnim->mPositionKeys[0].mValue;
+        return;
+    }
+
+    uint PositionIndex = FindPosition(AnimationTimeTicks, pNodeAnim);
+    uint NextPositionIndex = PositionIndex + 1;
+    assert(NextPositionIndex < pNodeAnim->mNumPositionKeys);
+    float t1 = (float)pNodeAnim->mPositionKeys[PositionIndex].mTime;
+    if (t1 > AnimationTimeTicks) {
+        Out = pNodeAnim->mPositionKeys[PositionIndex].mValue;
+    } else {
+        float t2 = (float)pNodeAnim->mPositionKeys[NextPositionIndex].mTime;
+        float DeltaTime = t2 - t1;
+        float Factor = (AnimationTimeTicks - t1) / DeltaTime;
+        assert(Factor >= 0.0f && Factor <= 1.0f);
+        const aiVector3D& Start = pNodeAnim->mPositionKeys[PositionIndex].mValue;
+        const aiVector3D& End = pNodeAnim->mPositionKeys[NextPositionIndex].mValue;
+        aiVector3D Delta = End - Start;
+        Out = Start + Factor * Delta;
+    }
+}
+
+
+uint DemolitionModel::FindRotation(float AnimationTimeTicks, const aiNodeAnim* pNodeAnim)
+{
+    assert(pNodeAnim->mNumRotationKeys > 0);
+
+    for (uint i = 0 ; i < pNodeAnim->mNumRotationKeys - 1 ; i++) {
+        float t = (float)pNodeAnim->mRotationKeys[i + 1].mTime;
+        if (AnimationTimeTicks < t) {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+
+void DemolitionModel::CalcInterpolatedRotation(aiQuaternion& Out, float AnimationTimeTicks, const aiNodeAnim* pNodeAnim)
+{
+    // we need at least two values to interpolate...
+    if (pNodeAnim->mNumRotationKeys == 1) {
+        Out = pNodeAnim->mRotationKeys[0].mValue;
+        return;
+    }
+
+    uint RotationIndex = FindRotation(AnimationTimeTicks, pNodeAnim);
+    uint NextRotationIndex = RotationIndex + 1;
+    assert(NextRotationIndex < pNodeAnim->mNumRotationKeys);
+    float t1 = (float)pNodeAnim->mRotationKeys[RotationIndex].mTime;
+    if (t1 > AnimationTimeTicks) {
+        Out = pNodeAnim->mRotationKeys[RotationIndex].mValue;
+    } else {
+        float t2 = (float)pNodeAnim->mRotationKeys[NextRotationIndex].mTime;
+        float DeltaTime = t2 - t1;
+        float Factor = (AnimationTimeTicks - t1) / DeltaTime;
+        assert(Factor >= 0.0f && Factor <= 1.0f);
+        const aiQuaternion& StartRotationQ = pNodeAnim->mRotationKeys[RotationIndex].mValue;
+        const aiQuaternion& EndRotationQ   = pNodeAnim->mRotationKeys[NextRotationIndex].mValue;
+        aiQuaternion::Interpolate(Out, StartRotationQ, EndRotationQ, Factor);
+    }
+
+    Out.Normalize();
+}
+
+
+uint DemolitionModel::FindScaling(float AnimationTimeTicks, const aiNodeAnim* pNodeAnim)
+{
+    assert(pNodeAnim->mNumScalingKeys > 0);
+
+    for (uint i = 0 ; i < pNodeAnim->mNumScalingKeys - 1 ; i++) {
+        float t = (float)pNodeAnim->mScalingKeys[i + 1].mTime;
+        if (AnimationTimeTicks < t) {
+            return i;
+        }
+    }
+
+    return 0;
+}
+
+
+void DemolitionModel::CalcInterpolatedScaling(aiVector3D& Out, float AnimationTimeTicks, const aiNodeAnim* pNodeAnim)
+{
+    // we need at least two values to interpolate...
+    if (pNodeAnim->mNumScalingKeys == 1) {
+        Out = pNodeAnim->mScalingKeys[0].mValue;
+        return;
+    }
+
+    uint ScalingIndex = FindScaling(AnimationTimeTicks, pNodeAnim);
+    uint NextScalingIndex = ScalingIndex + 1;
+    assert(NextScalingIndex < pNodeAnim->mNumScalingKeys);
+    float t1 = (float)pNodeAnim->mScalingKeys[ScalingIndex].mTime;
+    if (t1 > AnimationTimeTicks) {
+        Out = pNodeAnim->mScalingKeys[ScalingIndex].mValue;
+    } else {
+        float t2 = (float)pNodeAnim->mScalingKeys[NextScalingIndex].mTime;
+        float DeltaTime = t2 - t1;
+        float Factor = (AnimationTimeTicks - (float)t1) / DeltaTime;
+        assert(Factor >= 0.0f && Factor <= 1.0f);
+        const aiVector3D& Start = pNodeAnim->mScalingKeys[ScalingIndex].mValue;
+        const aiVector3D& End   = pNodeAnim->mScalingKeys[NextScalingIndex].mValue;
+        aiVector3D Delta = End - Start;
+        Out = Start + Factor * Delta;
+    }
+}
+
+
+void DemolitionModel::ReadNodeHierarchy(float AnimationTimeTicks, const aiNode* pNode, const Matrix4f& ParentTransform, const aiAnimation& Animation)
+{
+    string NodeName(pNode->mName.data);
+
+    Matrix4f NodeTransformation(pNode->mTransformation);
+
+    const aiNodeAnim* pNodeAnim = FindNodeAnim(Animation, NodeName);
+
+    if (pNodeAnim) {
+        LocalTransform Transform;
+        CalcLocalTransform(Transform, AnimationTimeTicks, pNodeAnim);
+
+        Matrix4f ScalingM;
+        ScalingM.InitScaleTransform(Transform.Scaling.x, Transform.Scaling.y, Transform.Scaling.z);
+        //        printf("Scaling %f %f %f\n", Transoform.Scaling.x, Transform.Scaling.y, Transform.Scaling.z);
+
+        Matrix4f RotationM = Matrix4f(Transform.Rotation.GetMatrix());
+
+        Matrix4f TranslationM;
+        TranslationM.InitTranslationTransform(Transform.Translation.x, Transform.Translation.y, Transform.Translation.z);
+        //        printf("Translation %f %f %f\n", Transform.Translation.x, Transform.Translation.y, Transform.Translation.z);
+
+        // Combine the above transformations
+        NodeTransformation = TranslationM * RotationM * ScalingM;
+    }
+
+    Matrix4f GlobalTransformation = ParentTransform * NodeTransformation;
+
+    if (m_BoneNameToIndexMap.find(NodeName) != m_BoneNameToIndexMap.end()) {
+        uint BoneIndex = m_BoneNameToIndexMap[NodeName];
+        m_BoneInfo[BoneIndex].FinalTransformation = m_GlobalInverseTransform * GlobalTransformation * m_BoneInfo[BoneIndex].OffsetMatrix;
+    }
+
+    for (uint i = 0 ; i < pNode->mNumChildren ; i++) {
+        string ChildName(pNode->mChildren[i]->mName.data);
+
+        map<string,NodeInfo>::iterator it = m_requiredNodeMap.find(ChildName);
+
+        if (it == m_requiredNodeMap.end()) {
+            printf("Child %s cannot be found in the required node map\n", ChildName.c_str());
+            assert(0);
+        }
+
+        if (it->second.isRequired) {
+            ReadNodeHierarchy(AnimationTimeTicks, pNode->mChildren[i], GlobalTransformation, Animation);
+        }
+    }
+}
+
+
+void DemolitionModel::ReadNodeHierarchyBlended(float StartAnimationTimeTicks, float EndAnimationTimeTicks, const aiNode* pNode, const Matrix4f& ParentTransform,
+                                           const aiAnimation& StartAnimation, const aiAnimation& EndAnimation, float BlendFactor)
+{
+    string NodeName(pNode->mName.data);
+
+    Matrix4f NodeTransformation(pNode->mTransformation);
+
+    const aiNodeAnim* pStartNodeAnim = FindNodeAnim(StartAnimation, NodeName);
+
+    LocalTransform StartTransform;
+
+    if (pStartNodeAnim) {
+        CalcLocalTransform(StartTransform, StartAnimationTimeTicks, pStartNodeAnim);
+    }
+
+    LocalTransform EndTransform;
+
+    const aiNodeAnim* pEndNodeAnim = FindNodeAnim(EndAnimation, NodeName);
+
+    if ((pStartNodeAnim && !pEndNodeAnim) || (!pStartNodeAnim && pEndNodeAnim)) {
+        printf("On the node %s there is an animation node for only one of the start/end animations.\n", NodeName.c_str());
+        printf("This case is not supported\n");
+        exit(0);
+    }
+
+    if (pEndNodeAnim) {
+        CalcLocalTransform(EndTransform, EndAnimationTimeTicks, pEndNodeAnim);
+    }
+
+    if (pStartNodeAnim && pEndNodeAnim) {
+        // Interpolate scaling
+        const aiVector3D& Scale0 = StartTransform.Scaling;
+        const aiVector3D& Scale1 = EndTransform.Scaling;
+        aiVector3D BlendedScaling = (1.0f - BlendFactor) * Scale0 + Scale1 * BlendFactor;
+        Matrix4f ScalingM;
+        ScalingM.InitScaleTransform(BlendedScaling.x, BlendedScaling.y, BlendedScaling.z);
+
+        // Interpolate rotation
+        const aiQuaternion& Rot0 = StartTransform.Rotation;
+        const aiQuaternion& Rot1 = EndTransform.Rotation;
+        aiQuaternion BlendedRot;
+        aiQuaternion::Interpolate(BlendedRot, Rot0, Rot1, BlendFactor);
+        Matrix4f RotationM = Matrix4f(BlendedRot.GetMatrix());
+
+        // Interpolate translation
+        const aiVector3D& Pos0 = StartTransform.Translation;
+        const aiVector3D& Pos1 = EndTransform.Translation;
+        aiVector3D BlendedTranslation = (1.0f - BlendFactor) * Pos0 + Pos1 * BlendFactor;
+        Matrix4f TranslationM;
+        TranslationM.InitTranslationTransform(BlendedTranslation.x, BlendedTranslation.y, BlendedTranslation.z);
+
+        // Combine it all
+        NodeTransformation = TranslationM * RotationM * ScalingM;
+    }
+
+    Matrix4f GlobalTransformation = ParentTransform * NodeTransformation;
+
+    if (m_BoneNameToIndexMap.find(NodeName) != m_BoneNameToIndexMap.end()) {
+        uint BoneIndex = m_BoneNameToIndexMap[NodeName];
+        m_BoneInfo[BoneIndex].FinalTransformation = m_GlobalInverseTransform * GlobalTransformation * m_BoneInfo[BoneIndex].OffsetMatrix;
+    }
+
+    for (uint i = 0 ; i < pNode->mNumChildren ; i++) {
+        string ChildName(pNode->mChildren[i]->mName.data);
+
+        map<string,NodeInfo>::iterator it = m_requiredNodeMap.find(ChildName);
+
+        if (it == m_requiredNodeMap.end()) {
+            printf("Child %s cannot be found in the required node map\n", ChildName.c_str());
+            assert(0);
+        }
+
+        if (it->second.isRequired) {
+            ReadNodeHierarchyBlended(StartAnimationTimeTicks, EndAnimationTimeTicks,
+                                     pNode->mChildren[i], GlobalTransformation, StartAnimation, EndAnimation, BlendFactor);
+        }
+    }
+}
+
+
+void DemolitionModel::CalcLocalTransform(LocalTransform& Transform, float AnimationTimeTicks, const aiNodeAnim* pNodeAnim)
+{
+    CalcInterpolatedScaling(Transform.Scaling, AnimationTimeTicks, pNodeAnim);
+    CalcInterpolatedRotation(Transform.Rotation, AnimationTimeTicks, pNodeAnim);
+    CalcInterpolatedPosition(Transform.Translation, AnimationTimeTicks, pNodeAnim);
+}
+
+
+void DemolitionModel::GetBoneTransforms(float TimeInSeconds, vector<Matrix4f>& Transforms, unsigned int AnimationIndex)
+{
+    if (AnimationIndex >= m_pScene->mNumAnimations) {
+        printf("Invalid animation index %d, max is %d\n", AnimationIndex, m_pScene->mNumAnimations);
+        assert(0);
+    }
+
+    Matrix4f Identity;
+    Identity.InitIdentity();
+
+    float AnimationTimeTicks = CalcAnimationTimeTicks(TimeInSeconds, AnimationIndex);
+    const aiAnimation& Animation = *m_pScene->mAnimations[AnimationIndex];
+
+    ReadNodeHierarchy(AnimationTimeTicks, m_pScene->mRootNode, Identity, Animation);
+    Transforms.resize(m_BoneInfo.size());
+
+    for (uint i = 0 ; i < m_BoneInfo.size() ; i++) {
+        Transforms[i] = m_BoneInfo[i].FinalTransformation;
+    }
+}
+
+
+void DemolitionModel::GetBoneTransformsBlended(float TimeInSeconds,
+                                           vector<Matrix4f>& BlendedTransforms,
+                                           unsigned int StartAnimIndex,
+                                           unsigned int EndAnimIndex,
+                                           float BlendFactor)
+{
+    if (StartAnimIndex >= m_pScene->mNumAnimations) {
+        printf("Invalid start animation index %d, max is %d\n", StartAnimIndex, m_pScene->mNumAnimations);
+        assert(0);
+    }
+
+    if (EndAnimIndex >= m_pScene->mNumAnimations) {
+        printf("Invalid end animation index %d, max is %d\n", EndAnimIndex, m_pScene->mNumAnimations);
+        assert(0);
+    }
+
+    if ((BlendFactor < 0.0f) || (BlendFactor > 1.0f)) {
+        printf("Invalid blend factor %f\n", BlendFactor);
+        assert(0);
+    }
+
+    float StartAnimationTimeTicks = CalcAnimationTimeTicks(TimeInSeconds, StartAnimIndex);
+    float EndAnimationTimeTicks = CalcAnimationTimeTicks(TimeInSeconds, EndAnimIndex);
+
+    const aiAnimation& StartAnimation = *m_pScene->mAnimations[StartAnimIndex];
+    const aiAnimation& EndAnimation = *m_pScene->mAnimations[EndAnimIndex];
+
+    Matrix4f Identity;
+    Identity.InitIdentity();
+
+    ReadNodeHierarchyBlended(StartAnimationTimeTicks, EndAnimationTimeTicks, m_pScene->mRootNode, Identity, StartAnimation, EndAnimation, BlendFactor);
+
+    BlendedTransforms.resize(m_BoneInfo.size());
+
+    for (uint i = 0 ; i < m_BoneInfo.size() ; i++) {
+        BlendedTransforms[i] = m_BoneInfo[i].FinalTransformation;
+    }
+}
+
+
+float DemolitionModel::CalcAnimationTimeTicks(float TimeInSeconds, unsigned int AnimationIndex)
+{
+    float TicksPerSecond = (float)(m_pScene->mAnimations[AnimationIndex]->mTicksPerSecond != 0 ? m_pScene->mAnimations[AnimationIndex]->mTicksPerSecond : 25.0f);
+    float TimeInTicks = TimeInSeconds * TicksPerSecond;
+    // we need to use the integral part of mDuration for the total length of the animation
+    float Duration = 0.0f;
+    float fraction = modf((float)m_pScene->mAnimations[AnimationIndex]->mDuration, &Duration);
+    float AnimationTimeTicks = fmod(TimeInTicks, Duration);
+    return AnimationTimeTicks;
+}
+
+
+const aiNodeAnim* DemolitionModel::FindNodeAnim(const aiAnimation&
+                                            Animation, const string& NodeName)
+{
+    for (uint i = 0 ; i < Animation.mNumChannels ; i++) {
+        const aiNodeAnim* pNodeAnim = Animation.mChannels[i];
+
+        if (string(pNodeAnim->mNodeName.data) == NodeName) {
+            return pNodeAnim;
+        }
+    }
+
+    return NULL;
 }
