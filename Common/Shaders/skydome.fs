fe090c3816f2ec35186d03b3becf6a2e28696f3a
Etay Meiri <etay.meiri@gmail.com>
2023-08-02 22:30:35 +0300

    Shader cleanup

--- Common/Shaders/skydome.fs
+++ Common/Shaders/skydome.fs
@@ -6,26 +6,8 @@ uniform sampler2D gSampler;
 uniform vec4 gLowColor = vec4(0.6, 0.38, 0.66, 1.0);
 uniform vec4 gHighColor = vec4(0.0, 0.15, 0.66, 1.0);
 
-//uniform vec4 gLowColor = vec4(1.0, 0.05, 0.6, 1.0);
-//uniform vec4 gHighColor = vec4(0.0, 0.5, 0.8, 1.0);
-
-
 in vec2 TexCoords0;
 in float Height;
-in vec3 StarPos;
-
-float Hash( float n ){
-    return fract( (1.0 + sin(n)) * 415.92653);
-}
-
-float Noise3d( vec3 x ){
-    float xhash = Hash(round(400*x.x) * 37.0);
-    float yhash = Hash(round(400*x.y) * 57.0);
-    float zhash = Hash(round(400*x.z) * 67.0);
-    return fract(xhash + yhash + zhash);
-}
-
-
 
 void main()
 {
@@ -37,20 +19,7 @@ void main()
           H = 0.0;
       }
 
-      //float f = mix(1.0, 0.2, H);
-
       vec4 SkyColor = mix(gLowColor, gHighColor, H);
 
-    //  float threshold = 0.999;
-
-     // float star_intensity = Noise3d(normalize(StarPos));
-        //And we apply a threshold to keep only the brightest areas
-    //    if (star_intensity >= threshold){
-            //We compute the star intensity
-   //         float sun_norm = 0.5;
-   //         star_intensity = pow((star_intensity - threshold)/(1.0 - threshold), 6.0)*(sun_norm+0.1);
-   //         SkyColor += vec4(star_intensity);
-   //     }
-
-      FragColor = TexColor * 0.8;// * SkyColor;
+      FragColor = TexColor;// * SkyColor;
 }
--- Common/Shaders/skydome.vs
+++ Common/Shaders/skydome.vs
@@ -7,14 +7,11 @@ uniform mat4 gWVP = mat4(1.0);
 
 out vec2 TexCoords0;
 out float Height;
-//out vec3 StarPos;
 
 void main()
 {
     gl_Position = gWVP * vec4(Position, 1.0);
     gl_Position.z = gl_Position.w;
-   // Height = gl_Position.y / gl_Position.w;
-   Height = Position.y;
+    Height = Position.y;
     TexCoords0 = TexCoords;
-   // StarPos = normalize(Position);
 }
