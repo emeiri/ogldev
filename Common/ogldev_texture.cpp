b57f24a0f462c62eb0f3aa965e929391e18ee2e1
Etay Meiri <etay.meiri@gmail.com>
2023-08-02 22:31:05 +0300

    Removed image magick

--- Common/ogldev_texture.cpp
+++ Common/ogldev_texture.cpp
@@ -46,19 +46,6 @@ void Texture::Load(u32 BufferSize, void* pData)
 
 bool Texture::Load()
 {
-#ifdef USE_IMAGE_MAGICK
-
-    try {
-        m_image.read(m_fileName);
-        m_image.write(&m_blob, "RGBA");
-    }
-    catch (Magick::Error& Error) {
-        std::cout << "Error loading texture '" << m_fileName << "': " << Error.what() << std::endl;
-        return false;
-    }
-
-#else // STB image
-
     stbi_set_flip_vertically_on_load(1);
 
     unsigned char* image_data = stbi_load(m_fileName.c_str(), &m_imageWidth, &m_imageHeight, &m_imageBPP, 0);
@@ -70,13 +57,7 @@ bool Texture::Load()
 
     printf("Width %d, height %d, bpp %d\n", m_imageWidth, m_imageHeight, m_imageBPP);
 
-#endif
-
     LoadInternal(image_data);
-
-#ifndef USE_IMAGE_MAGICK
-    stbi_image_free(image_data);
-#endif
 
     return true;
 }
@@ -108,9 +89,6 @@ void Texture::LoadInternal(void* image_data)
     glBindTexture(m_textureTarget, m_textureObj);
 
     if (m_textureTarget == GL_TEXTURE_2D) {
-#ifdef USE_IMAGE_MAGICK
-        glTexImage2D(m_textureTarget, 0, GL_RGBA, m_image.columns(), m_image.rows(), 0, GL_RGBA, GL_UNSIGNED_BYTE, m_blob.data());
-#else
         switch (m_imageBPP) {
         case 1:
             glTexImage2D(m_textureTarget, 0, GL_RED, m_imageWidth, m_imageHeight, 0, GL_RED, GL_UNSIGNED_BYTE, image_data);
@@ -127,7 +105,6 @@ void Texture::LoadInternal(void* image_data)
         default:
             NOT_IMPLEMENTED;
         }
-#endif
     } else {
         printf("Support for texture target %x is not implemented\n", m_textureTarget);
         exit(1);
