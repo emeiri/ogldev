c837c01b63945333d423b75f09c9bfecac8d6629
Etay Meiri <etay.meiri@gmail.com>
2023-12-21 09:29:32 +0200

    Fixed incorrect face culling

--- tutorial32_youtube/tutorial32.cpp
+++ tutorial32_youtube/tutorial32.cpp
@@ -412,7 +412,7 @@ int main(int argc, char** argv)
     app->Init();
 
     glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-    glFrontFace(GL_CW);
+    glFrontFace(GL_CCW);
     glCullFace(GL_BACK);
     glEnable(GL_CULL_FACE);
     glEnable(GL_DEPTH_TEST);
