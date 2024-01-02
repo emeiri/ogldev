535388b576089e95f633a9dfb312102ef32244ca
Etay Meiri <etay.meiri@gmail.com>
2024-01-02 10:35:48 +0200

    Added script to run apitrace

---
+++ DemoLITION/apitrace.bat
@@ -0,0 +1,3 @@
+del c:\Users\emeir\OneDrive\Desktop\*.trace
+set PATH=..\Windows\DLL
+"C:\Users\emeir\apitrace-latest-win64\bin\apitrace.exe" trace "..\Windows\ogldev_vs_2022\x64\Debug\DemoLITION Test.exe"
