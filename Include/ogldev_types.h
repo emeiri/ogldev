065250d540effe45c9c86fac0501a598200ead17
Etay Meiri <etay.meiri@gmail.com>
2024-02-01 12:20:19 +0200

    Added types

--- Include/ogldev_types.h
+++ Include/ogldev_types.h
@@ -29,10 +29,10 @@ typedef unsigned int uint;
 typedef unsigned short ushort;
 typedef unsigned char uchar;
 
-typedef unsigned char u8;
-typedef char i8;
-typedef unsigned short u16;
-typedef short i16;
+typedef uint8_t u8;
+typedef int8_t i8;
+typedef uint16_t u16;
+typedef int16_t i16;
 typedef int32_t i32;
 typedef uint32_t u32;
 
