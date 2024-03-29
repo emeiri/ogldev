6c79dc2f8ab1e4fac68201bd5bd44e441446ab5d
Etay Meiri <etay.meiri@gmail.com>
2024-03-29 10:49:58 +0300

    Cleanup CreateRenderPass

--- Vulkan/VulkanCore/Source/core.cpp
+++ Vulkan/VulkanCore/Source/core.cpp
@@ -418,24 +418,35 @@ void VulkanCore::CreateSwapChain()
 
 void VulkanCore::CreateRenderPass()
 {
-	VkAttachmentReference attachRef = {};
-	attachRef.attachment = 0;
-	attachRef.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL;
+	VkAttachmentReference AttachRef = {
+		.attachment = 0,
+		.layout = VK_IMAGE_LAYOUT_COLOR_ATTACHMENT_OPTIMAL
+	};
 
-	VkSubpassDescription subpassDesc = {};
-	subpassDesc.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS;
-	subpassDesc.colorAttachmentCount = 1;
-	subpassDesc.pColorAttachments = &attachRef;
+	VkSubpassDescription SubpassDesc = {
+		.flags = 0,
+		.pipelineBindPoint = VK_PIPELINE_BIND_POINT_GRAPHICS,
+		.inputAttachmentCount = 0,
+		.pInputAttachments = NULL,
+		.colorAttachmentCount = 1,
+		.pColorAttachments = &AttachRef,
+		.pResolveAttachments = NULL,
+		.pDepthStencilAttachment = NULL,
+		.preserveAttachmentCount = 0,
+		.pPreserveAttachments = NULL
+	};
 
-	VkAttachmentDescription attachDesc = {};
-	attachDesc.format = m_physDevices.Selected().m_surfaceFormats[0].format;
-	attachDesc.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR;
-	attachDesc.storeOp = VK_ATTACHMENT_STORE_OP_STORE;
-	attachDesc.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE;
-	attachDesc.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE;
-	attachDesc.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED;
-	attachDesc.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR;
-	attachDesc.samples = VK_SAMPLE_COUNT_1_BIT;
+	VkAttachmentDescription AttachDesc = {
+		.flags = 0,
+		.format = m_swapChainSurfaceFormat.format,
+		.samples = VK_SAMPLE_COUNT_1_BIT,
+		.loadOp = VK_ATTACHMENT_LOAD_OP_CLEAR,
+		.storeOp = VK_ATTACHMENT_STORE_OP_STORE,
+		.stencilLoadOp = VK_ATTACHMENT_LOAD_OP_DONT_CARE,
+		.stencilStoreOp = VK_ATTACHMENT_STORE_OP_DONT_CARE,
+		.initialLayout = VK_IMAGE_LAYOUT_UNDEFINED,
+		.finalLayout = VK_IMAGE_LAYOUT_PRESENT_SRC_KHR
+	};
 
 #if 0
 	VkAttachmentDescription depthAttachment = {
@@ -456,20 +467,26 @@ void VulkanCore::CreateRenderPass()
 	};
 #endif
 
-	VkSubpassDependency Dependency = {};
-	Dependency.srcSubpass = VK_SUBPASS_EXTERNAL;
-	Dependency.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-	Dependency.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT;
-	Dependency.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT;
+	VkSubpassDependency Dependency = {
+		.srcSubpass = VK_SUBPASS_EXTERNAL,
+		.dstSubpass = 0,
+		.srcStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+		.dstStageMask = VK_PIPELINE_STAGE_COLOR_ATTACHMENT_OUTPUT_BIT,
+		.srcAccessMask = 0,
+		.dstAccessMask = VK_ACCESS_COLOR_ATTACHMENT_READ_BIT | VK_ACCESS_COLOR_ATTACHMENT_WRITE_BIT
+	};
 
-	VkRenderPassCreateInfo RenderPassCreateInfo = {};
-	RenderPassCreateInfo.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO;
-	RenderPassCreateInfo.attachmentCount = 1;
-	RenderPassCreateInfo.pAttachments = &attachDesc;
-	RenderPassCreateInfo.subpassCount = 1;
-	RenderPassCreateInfo.pSubpasses = &subpassDesc;
-	RenderPassCreateInfo.dependencyCount = 1;
-	RenderPassCreateInfo.pDependencies = &Dependency;
+	VkRenderPassCreateInfo RenderPassCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_RENDER_PASS_CREATE_INFO,
+		.pNext = NULL,
+		.flags = 0,
+		.attachmentCount = 1,
+		.pAttachments = &AttachDesc,
+		.subpassCount = 1,
+		.pSubpasses = &SubpassDesc,
+		.dependencyCount = 1,
+		.pDependencies = &Dependency
+	};
 
 	VkResult res = vkCreateRenderPass(m_device, &RenderPassCreateInfo, NULL, &m_renderPass);
 	CHECK_VK_RESULT(res, "vkCreateRenderPass\n");
