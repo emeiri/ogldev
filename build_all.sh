40daf57e05712213542827ca943caf0bc6c13354
Etay Meiri <etay.meiri@gmail.com>
2024-05-13 15:40:27 +0300

    Build script fix

--- build_all.sh
+++ build_all.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-for filename in tutorial* Terrain*; do
-    echo "Building " $filename "..."
-    cd $filename
+for dirname in Vulkan/Tutorial* ; do
+    echo "Building " $dirname "..."
+    cd $dirname
+    ./build.sh
+    cd ../../
+done
+
+for dirname in tutorial* Terrain* ; do
+    echo "Building " $dirname "..."
+    cd $dirname
     ./build.sh
     cd ..
 done
