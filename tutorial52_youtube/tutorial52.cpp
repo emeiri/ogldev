f2c3da656f8554d6023eea71b3e5494a12cefa9e
Etay Meiri <etay.meiri@gmail.com>
2024-07-22 00:41:11 +0300

    release cleanup

--- tutorial52_youtube/tutorial52.cpp
+++ tutorial52_youtube/tutorial52.cpp
@@ -29,7 +29,7 @@
 #define WINDOW_WIDTH  1920
 #define WINDOW_HEIGHT 1080
 
-#define POINT_LIGHT
+// #define POINT_LIGHT   // works a bit weird...
 
 
 class NormalMapping : public BaseGLApp {
@@ -41,8 +41,8 @@ public:
         m_dirLight.AmbientIntensity = 0.1f;
 
         m_pointLight.WorldPosition = Vector3f(0.25f, 0.25f, 0.0f);
-        m_pointLight.DiffuseIntensity = 1.5f;
-        m_pointLight.AmbientIntensity = 0.4f;
+        m_pointLight.DiffuseIntensity = 2.5f;
+        m_pointLight.AmbientIntensity = 0.5f;
     }
 
     ~NormalMapping() {}
@@ -51,9 +51,7 @@ public:
     {
         m_pScene = m_pRenderingSystem->CreateEmptyScene();
         m_pScene->SetClearColor(Vector4f(0.0f, 1.0f, 0.0f, 0.0f));
-        m_pScene->SetCamera(Vector3f(0.0f, 0.0f, -4.5f), Vector3f(0.0f, 0.0, 1.0f));
-    //    m_pScene->SetCamera(Vector3f(0.0f, 1.15f, -3.5f), Vector3f(0.000823f, -0.331338f, 0.943512f));
-     //   m_pScene->SetCamera(Vector3f(3.5f, 1.15f, 0.0f), Vector3f(-1.000823f, -0.331338f, 0.0f));
+        m_pScene->SetCamera(Vector3f(0.0f, 0.0f, -4.5f), Vector3f(0.0f, 0.0f, 1.0f));
         m_pScene->SetCameraSpeed(0.1f);
 #ifdef POINT_LIGHT
         m_pScene->GetPointLights().push_back(m_pointLight);
@@ -62,56 +60,13 @@ public:
 #endif     
         m_pScene->GetConfig()->ControlShadowMapping(false);
 
-        // Demo 1
-        if (false) {
-            m_pModel = m_pRenderingSystem->LoadModel("../Content/sphere.obj");
-            m_pSceneObject = m_pScene->CreateSceneObject(m_pModel);
-            int ColorTexture = m_pRenderingSystem->LoadTexture2D("../Content/dry-rocky-ground-bl/dry-rocky-ground_albedo.png");
-            m_pModel->SetColorTexture(ColorTexture);
-            m_normalMap = m_pRenderingSystem->LoadTexture2D("../Content/dry-rocky-ground-bl/dry-rocky-ground_normal-ogl.png");
-            m_pModel->SetNormalMap(m_normalMap);
-        } else if (false) {
-            m_pModel = m_pRenderingSystem->LoadModel("../Content/box.obj");
-            m_pSceneObject = m_pScene->CreateSceneObject(m_pModel);
-            m_pSceneObject->SetScale(0.5f);
-            //int ColorTexture = m_pRenderingSystem->LoadTexture2D("../Content/mybrick/mybrick-color.png");
-            int ColorTexture = m_pRenderingSystem->LoadTexture2D("../Content/dry-rocky-ground-bl/dry-rocky-ground_albedo.png");
-            m_pModel->SetColorTexture(ColorTexture);
-           // m_normalMap = m_pRenderingSystem->LoadTexture2D("../Content/mybrick/mybrick-normal.png");
-        //m_normalMap = m_pRenderingSystem->LoadTexture2D("../Content/brickwall_normal.jpg");
-            m_normalMap = m_pRenderingSystem->LoadTexture2D("../Content/dry-rocky-ground-bl/dry-rocky-ground_normal-ogl.png");
-            m_pModel->SetNormalMap(m_normalMap);
-        } else if (false) {
-            m_pModel = m_pRenderingSystem->LoadModel("../Content/bs_ears.obj");
-            m_pSceneObject = m_pScene->CreateSceneObject(m_pModel);
-            m_normalMap = m_pRenderingSystem->LoadTexture2D("../Content/textures/ogre_normalmap.png");
-            m_pModel->SetNormalMap(m_normalMap);
-        } else if (true) {
-            m_pModel = m_pRenderingSystem->LoadModel("../Content/quad.obj");
-            m_pSceneObject = m_pScene->CreateSceneObject(m_pModel);
-            //int ColorTexture = m_pRenderingSystem->LoadTexture2D("G:/PBRTextures/Blender/scifi-bl/filthy-space-panels-bl/filthy-space-panels_albedo.png");
-            int ColorTexture = m_pRenderingSystem->LoadTexture2D("G:/PBRTextures/Blender/scifi-bl/space-cruiser-panels1-bl/space-cruiser-panels_albedo.png");
-            m_pModel->SetColorTexture(ColorTexture);
-            //m_normalMap = m_pRenderingSystem->LoadTexture2D("G:/PBRTextures/Blender/scifi-bl/filthy-space-panels-bl/filthy-space-panels_normal-ogl.png");
-            m_normalMap = m_pRenderingSystem->LoadTexture2D("G:/PBRTextures/Blender/scifi-bl/space-cruiser-panels1-bl/space-cruiser-panels_normal-ogl.png");
-            m_pModel->SetNormalMap(m_normalMap);
-        }
-
-
-      //  m_pModel = m_pRenderingSystem->LoadModel("../Content/dry-rocky-ground-bl/dry-rocky-ground-bl.obj");
-        //m_pModel = m_pRenderingSystem->LoadModel("../Content/mybrick/mybrick.obj");
-         //m_pModel = m_pRenderingSystem->LoadModel("../Content/brickwall2.obj");
-       // m_pModel = pRenderingSystem->LoadModel("../Content/brickwall.obj");
-      //  m_pSceneObject->SetPosition(0.0f, 0.0f, 4.0f);
-        //pSceneObject->SetRotation(Vector3f(-90.0f, 0.0f, 0.0f));
-        //m_pSceneObject->SetRotation(Vector3f(0.0f, 65.0f, 0.0f));
+        m_pModel = m_pRenderingSystem->LoadModel("../Content/sphere.obj");
+        m_pSceneObject = m_pScene->CreateSceneObject(m_pModel);
+        int ColorTexture = m_pRenderingSystem->LoadTexture2D("../Content/dry-rocky-ground-bl/dry-rocky-ground_albedo.png");
+        m_pModel->SetColorTexture(ColorTexture);
+        m_normalMap = m_pRenderingSystem->LoadTexture2D("../Content/dry-rocky-ground-bl/dry-rocky-ground_normal-ogl.png");
+        m_pModel->SetNormalMap(m_normalMap);
         m_pScene->AddToRenderList(m_pSceneObject);
-
-        
-       
-       // m_normalMap = m_pRenderingSystem->LoadTexture2D("../Content/bricks2_normal.jpg");
-        
-
 
         m_pRenderingSystem->SetScene(m_pScene);
 
@@ -124,17 +79,12 @@ public:
         BaseGLApp::OnFrame();      
         m_count += 0.01f;
         m_pSceneObject->ResetRotations();
-        m_pSceneObject->PushRotation(Vector3f(m_rotationAngle, 0.0f, 0.0f));
-        //m_pSceneObject->PushRotation(Vector3f(0.0f, 90.0f, 0.0f));
+        m_pSceneObject->PushRotation(Vector3f(0.0f, m_count * 5.0f, 0.0f));
 
 #ifdef POINT_LIGHT      
-        m_pScene->GetPointLights()[0].WorldPosition.x = sinf(m_count);
-        m_pScene->GetPointLights()[0].WorldPosition.z = -0.25f;
-        m_pScene->GetPointLights()[0].WorldPosition.y = cosf(m_count);
-
-        Matrix3f m;
-        m.InitRotateTransform(m_rotationAngle, 0.0f, 0.0f);
-        m_pScene->GetPointLights()[0].WorldPosition = m * m_pScene->GetPointLights()[0].WorldPosition;
+        m_pScene->GetPointLights()[0].WorldPosition.x = sinf(m_count) * 3.0f;
+        m_pScene->GetPointLights()[0].WorldPosition.y = 0.0f;
+        m_pScene->GetPointLights()[0].WorldPosition.z = cosf(m_count) * 3.0f;
 #else
         m_pScene->GetDirLights()[0].WorldDirection = Vector3f(sinf(m_count), cosf(m_count) * 0.5f, 1.0f);
 #endif
