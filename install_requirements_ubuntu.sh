0e68be52a6ac71abcad0581efec63a549c53fe17
Parallels <etay.meiri@gmail.com>
2024-10-21 18:34:42 +0300

    Linux Mint 21 fixes

--- install_requirements_ubuntu.sh
+++ install_requirements_ubuntu.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-apt install g++ make freeglut3 freeglut3-dev libglew-dev libglew2.2 libmagick++-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer1d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev
+apt install g++ make freeglut3 freeglut3-dev libglew-dev libglew2.2 libmagick++-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer1d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
