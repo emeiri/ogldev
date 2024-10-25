b0022b688300f6f6d903fe94183a037f915e33b0
Etay Meiri <etay.meiri@gmail.com>
2024-10-25 09:47:05 +0300

    Use glm RotationBetweenVectors

--- DemoLITION/Tests/Test1/DemoLITION_test_carbonara.cpp
+++ DemoLITION/Tests/Test1/DemoLITION_test_carbonara.cpp
@@ -490,21 +490,55 @@ public:
 };
 
 
+glm::quat RotationBetweenVectors(glm::vec3& start, glm::vec3& dest) {
+    start = normalize(start);
+    dest = normalize(dest);
+
+    float cosTheta = dot(start, dest);
+    glm::vec3 rotationAxis;
+
+    if (cosTheta < -1 + 0.001f) {
+        // special case when vectors in opposite directions :
+        // there is no "ideal" rotation axis
+        // So guess one; any will do as long as it's perpendicular to start
+        // This implementation favors a rotation around the Up axis,
+        // since it's often what you want to do.
+        rotationAxis = glm::cross(glm::vec3(0.0f, 0.0f, 1.0f), start);
+     //   if (glm::length2(rotationAxis) < 0.01) // bad luck, they were parallel, try again!
+      //      rotationAxis = cross(vec3(1.0f, 0.0f, 0.0f), start);
+
+        rotationAxis = normalize(rotationAxis);
+        return glm::angleAxis(glm::radians(180.0f), rotationAxis);
+    }
+
+    // Implementation from Stan Melax's Game Programming Gems 1 article
+    rotationAxis = cross(start, dest);
+
+    float s = sqrt((1 + cosTheta) * 2);
+    float invs = 1 / s;
+
+    return glm::quat(
+        s * 0.5f,
+        rotationAxis.x * invs,
+        rotationAxis.y * invs,
+        rotationAxis.z * invs
+    );
+
+
+}
+
+
+
 static void DirToRotation(const Vector3f& Dir, SceneObject& o)
 {
     Vector3f DirN = Dir;
     DirN.Normalize();
 
-    float AngleH = ToDegree(atan2(DirN.z, DirN.x));
-    float AngleV = ToDegree(asin(DirN.y));
+    glm::vec3 Start(1.0f, 0.0f, 0.0f);
+    glm::vec3 Dest(Dir.x, Dir.y, Dir.z);
 
-    if (AngleH != 0.0f) {
-        o.PushRotation(Vector3f(0.0f, AngleH, 0.0f));
-    }
-
-    if (AngleV != 0.0f) {
-        o.PushRotation(Vector3f(0.0f, 0.0f, AngleV));
-    }
+    glm::quat q = RotationBetweenVectors(Start, Dest);
+    o.SetQuaternion(q);
 }
 
 
