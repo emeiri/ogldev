622fd0b7a702c28aef2f88c614a34c39da8fabc3
Etay Meiri <etay.meiri@gmail.com>
2024-12-04 10:55:07 +0200

    cleanup

--- Common/cubemap_texture.cpp
+++ Common/cubemap_texture.cpp
@@ -80,6 +80,7 @@ glm::vec3 FaceCoordsToXYZ(int i, int j, int FaceID, int FaceSize)
     return Ret;
 }
 
+
 enum eBitmapType
 {
     eBitmapType_2D,
@@ -93,25 +94,48 @@ enum eBitmapFormat
 };
 
 
-struct Bitmap
+int GetBytesPerComponent(eBitmapFormat Fmt)
 {
-    Bitmap() = default;
+    int BytesPerComponent = 0;
+
+    switch (Fmt) {
+    case eBitmapFormat_UnsignedByte:
+        BytesPerComponent = 1;
+        break;
+
+    case eBitmapFormat_Float:
+        BytesPerComponent = 4;
+        break;
+    };
+
+    return BytesPerComponent;
+}
+
+
+class Bitmap {
+
+public:
+    Bitmap() {};
+
     Bitmap(int w, int h, int comp, eBitmapFormat fmt)
-        :w_(w), h_(h), comp_(comp), fmt_(fmt), data_(w* h* comp* getBytesPerComponent(fmt))
+        :w_(w), h_(h), comp_(comp), fmt_(fmt), data_(w * h * comp * GetBytesPerComponent(fmt))
     {
         initGetSetFuncs();
     }
+
     Bitmap(int w, int h, int d, int comp, eBitmapFormat fmt)
-        :w_(w), h_(h), d_(d), comp_(comp), fmt_(fmt), data_(w* h* d* comp* getBytesPerComponent(fmt))
+        :w_(w), h_(h), d_(d), comp_(comp), fmt_(fmt), data_(w * h * d * comp * GetBytesPerComponent(fmt))
     {
         initGetSetFuncs();
     }
+
     Bitmap(int w, int h, int comp, eBitmapFormat fmt, void* ptr)
-        :w_(w), h_(h), comp_(comp), fmt_(fmt), data_(w* h* comp* getBytesPerComponent(fmt))
+        :w_(w), h_(h), comp_(comp), fmt_(fmt), data_(w * h * comp * GetBytesPerComponent(fmt))
     {
         initGetSetFuncs();
         memcpy(data_.data(), ptr, data_.size());
     }
+
     int w_ = 0;
     int h_ = 0;
     int d_ = 1;
@@ -120,22 +144,18 @@ struct Bitmap
     eBitmapType type_ = eBitmapType_2D;
     std::vector<uint8_t> data_;
 
-    static int getBytesPerComponent(eBitmapFormat fmt)
-    {
-        if (fmt == eBitmapFormat_UnsignedByte) return 1;
-        if (fmt == eBitmapFormat_Float) return 4;
-        return 0;
-    }
-
     void setPixel(int x, int y, glm::vec4& c)
     {
         (*this.*setPixelFunc)(x, y, c);
     }
+
     glm::vec4 getPixel(int x, int y)
     {
         return ((*this.*getPixelFunc)(x, y));
     }
+
 private:
+
     using setPixel_t = void(Bitmap::*)(int, int, glm::vec4&);
     using getPixel_t = glm::vec4(Bitmap::*)(int, int);
     setPixel_t setPixelFunc = &Bitmap::setPixelUnsignedByte;
@@ -165,6 +185,7 @@ private:
         if (comp_ > 2) data[ofs + 2] = c.z;
         if (comp_ > 3) data[ofs + 3] = c.w;
     }
+
     glm::vec4 getPixelFloat(int x, int y)
     {
         int ofs = comp_ * (y * w_ + x);
@@ -184,6 +205,7 @@ private:
         if (comp_ > 2) data_[ofs + 2] = uint8_t(c.z * 255.0f);
         if (comp_ > 3) data_[ofs + 3] = uint8_t(c.w * 255.0f);
     }
+
     glm::vec4 getPixelUnsignedByte(int x, int y)
     {
         int ofs = comp_ * (y * w_ + x);
@@ -198,36 +220,36 @@ private:
 
 Bitmap convertEquirectangularMapToVerticalCross(Bitmap& b)
 {
-    if (b.type_ != eBitmapType_2D) return Bitmap();
+    if (b.type_ != eBitmapType_2D) {
+        return Bitmap();
+    }
 
     int faceSize = b.w_ / 4;
 
+    // Prepare a cross type bitmap for the result -
+    // three squares horizontal and four vertical
     int w = faceSize * 3;
     int h = faceSize * 4;
 
     Bitmap result(w, h, b.comp_, b.fmt_);
 
-    glm::ivec2 kFaceOffsets[] =
-    {
-        glm::ivec2(faceSize, faceSize * 3),
-        glm::ivec2(0, faceSize),
-        glm::ivec2(faceSize, faceSize),
-        glm::ivec2(faceSize * 2, faceSize),
-        glm::ivec2(faceSize, 0),
-        glm::ivec2(faceSize, faceSize * 2)
+    glm::ivec2 FaceOffsets[] = {
+        glm::ivec2(faceSize, faceSize * 3),     // +Z
+        glm::ivec2(0, faceSize),                // -X
+        glm::ivec2(faceSize, faceSize),         // -Z
+        glm::ivec2(faceSize * 2, faceSize),     // +X
+        glm::ivec2(faceSize, 0),                // +Y
+        glm::ivec2(faceSize, faceSize * 2)      // -Y
     };
 
     int clampW = b.w_ - 1;
     int clampH = b.h_ - 1;
 
-    for (int face = 0; face != 6; face++)
-    {
-        for (int i = 0; i != faceSize; i++)
-        {
-            for (int j = 0; j != faceSize; j++)
-            {
+    for (int face = 0; face != 6; face++) {
+        for (int i = 0; i != faceSize; i++) {
+            for (int j = 0; j != faceSize; j++) {
                 glm::vec3 P = FaceCoordsToXYZ(i, j, face, faceSize);
-                float R = hypot(P.x, P.y);
+                float R = sqrtf(P.x * P.x + P.y * P.y);//  //hypot(P.x, P.y);
                 float theta = atan2f(P.y, P.x);
                 float phi = atan2f(P.z, R);
                 //	float point source coordinates
@@ -248,7 +270,7 @@ Bitmap convertEquirectangularMapToVerticalCross(Bitmap& b)
                 glm::vec4 D = b.getPixel(U2, V2);
                 // bilinear interpolation
                 glm::vec4 color = A * (1 - s) * (1 - t) + B * (s) * (1 - t) + C * (1 - s) * t + D * (s) * (t);
-                result.setPixel(i + kFaceOffsets[face].x, j + kFaceOffsets[face].y, color);
+                result.setPixel(i + FaceOffsets[face].x, j + FaceOffsets[face].y, color);
             }
         };
     }
@@ -281,7 +303,7 @@ Bitmap convertVerticalCrossToCubeMapFaces(const Bitmap& b)
             ------
     */
 
-    int pixelSize = cubemap.comp_ * Bitmap::getBytesPerComponent(cubemap.fmt_);
+    int pixelSize = cubemap.comp_ * GetBytesPerComponent(cubemap.fmt_);
 
     for (int face = 0; face != 6; ++face)
     {
@@ -426,11 +448,19 @@ void CubemapEctTexture::Load()
 {
 //    stbi_set_flip_vertically_on_load(0);
 
-    int w, h, comp;
-    const float* img = stbi_loadf(m_filename.c_str(), & w, & h, & comp, 3);
-    Bitmap in(w, h, comp, eBitmapFormat_Float, (void*)img);
+    int Width, Height, Comp;
+
+    const float* pImg = stbi_loadf(m_filename.c_str(), &Width, &Height, &Comp, 3);
+    
+    if (!pImg) {
+        printf("Error loading '%s'\n", m_filename.c_str());
+        exit(1);
+    }
+
+    Bitmap in(Width, Height, Comp, eBitmapFormat_Float, (void*)pImg);
     Bitmap out = convertEquirectangularMapToVerticalCross(in);
-    stbi_image_free((void*)img);
+
+    stbi_image_free((void*)pImg);
 
     //stbi_write_hdr("screenshot.hdr", out.w_, out.h_, out.comp_, (const float*)out.data_.data());
 
@@ -446,12 +476,12 @@ void CubemapEctTexture::Load()
     glTextureParameteri(m_textureObj, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
     glTextureParameteri(m_textureObj, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTextureStorage2D(m_textureObj, 1, GL_RGB32F, cubemap.w_, cubemap.h_);
+
     const uint8_t* data = cubemap.data_.data();
 
-    for (unsigned i = 0; i != 6; ++i)
-    {
+    for (unsigned i = 0; i != 6; ++i) {
         glTextureSubImage3D(m_textureObj, 0, 0, 0, i, cubemap.w_, cubemap.h_, 1, GL_RGB, GL_FLOAT, data);
-        data += cubemap.w_ * cubemap.h_ * cubemap.comp_ * Bitmap::getBytesPerComponent(cubemap.fmt_);
+        data += cubemap.w_ * cubemap.h_ * cubemap.comp_ * GetBytesPerComponent(cubemap.fmt_);
     }
 }
 
