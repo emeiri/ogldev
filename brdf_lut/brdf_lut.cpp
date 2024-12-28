0050fa8a963dd955e394e4ea05186c974d6b347d
Etay Meiri <etay.meiri@gmail.com>
2024-12-28 23:45:03 +0200

    Cleanup

--- brdf_lut/brdf_lut.cpp
+++ brdf_lut/brdf_lut.cpp
@@ -33,6 +33,88 @@ gli::texture convertLUTtoTexture(const float* data)
 	return lutTexture;
 }
 
+
+class Particles
+{
+public:
+	Particles();
+
+	void Init();
+	void Render();
+	const std::vector<char>& GetOutput();
+
+private:
+
+	void InitBuffers();
+	//  void ExecuteComputeShader(const Vector3f& BlackHolePos1, const Vector3f& BlackHolePos2);
+	 // void RenderParticles(const Vector3f& BlackHolePos1, const Vector3f& BlackHolePos2, const Matrix4f& VP);
+
+	BRDF_LUT_Technique m_brdfTech;
+	std::vector<char> m_outputData;
+	GLuint m_vao;
+	GLuint m_outputBuf = 0;
+};
+
+
+Particles::Particles()
+{
+}
+
+void Particles::Init()
+{
+	m_brdfTech.Init();
+	InitBuffers();
+}
+
+void Particles::Render()
+{
+	m_brdfTech.Enable();
+	glBindVertexArray(m_vao);
+	glDispatchCompute(brdfW, brdfH, 1);
+	glMemoryBarrier(GL_SHADER_STORAGE_BARRIER_BIT);
+	glBindVertexArray(0);
+}
+
+void Particles::InitBuffers()
+{
+	GLuint InputBuf = 0;
+
+	glGenBuffers(1, &InputBuf);
+
+	glGenVertexArrays(1, &m_vao);
+	glBindVertexArray(m_vao);
+
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 0, InputBuf);
+	float f = 0.0f;
+	glBufferData(GL_SHADER_STORAGE_BUFFER, sizeof(float), &f, GL_DYNAMIC_DRAW);
+
+	glCreateBuffers(1, &m_outputBuf);
+	glNamedBufferStorage(m_outputBuf, bufferSize, m_outputData.data(), GL_MAP_WRITE_BIT | GL_MAP_READ_BIT);
+
+	m_outputData.resize(bufferSize);
+	glBindBufferBase(GL_SHADER_STORAGE_BUFFER, 1, m_outputBuf);
+	glBindVertexArray(0);
+}
+
+
+const std::vector<char>& Particles::GetOutput()
+{
+	// Map buffer for reading
+	void* p = glMapNamedBufferRange(m_outputBuf, 0, bufferSize, GL_MAP_READ_BIT);
+
+	if (!p) {
+		printf("Map error\n");
+		exit(1);
+	}
+
+	memcpy(m_outputData.data(), p, bufferSize);
+
+	bool result = glUnmapNamedBuffer(m_outputBuf);
+
+	return m_outputData;
+}
+
+
 int main()
 {
 	int major_ver = 0;
