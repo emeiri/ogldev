64a879212a47027ed290a0cf7011797245a12641
Etay Meiri <etay.meiri@gmail.com>
2025-03-07 18:15:37 +0200

    Add copyright notice

--- tutorial59_youtube/bindless_texture.fs
+++ tutorial59_youtube/bindless_texture.fs
@@ -1,3 +1,21 @@
+/*
+    Copyright 2025 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #version 460 core
 
 #extension GL_ARB_bindless_texture : require
@@ -18,5 +36,4 @@ void main()
     sampler2D tex = textures[gTextureIndex];
     
     FragColor = vec4(texture(tex, TexCoord).rgb, 1.0);
-  //FragColor = vec4(float(Instance) / 350.0);
 }
