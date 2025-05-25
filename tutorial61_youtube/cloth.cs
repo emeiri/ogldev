c3bea6cf5788743d3f0f3708d0a1ed7d1df68b8b
Etay Meiri <etay.meiri@gmail.com>
2025-05-25 14:51:29 +0300

    Cleanup

--- tutorial61_youtube/cloth.cs
+++ tutorial61_youtube/cloth.cs
@@ -14,102 +14,107 @@ uniform float DampingConst = 0.1;
 
 layout(std430, binding = 0) buffer PosIn
 {
-    vec4 PositionIn [];
+    vec4 PositionIn[];
 };
+
 layout(std430, binding = 1) buffer PosOut
 {
-    vec4 PositionOut [];
+    vec4 PositionOut[];
 };
+
 layout(std430, binding = 2) buffer VelIn
 {
-    vec4 VelocityIn [];
+    vec4 VelocityIn[];
 };
+
 layout(std430, binding = 3) buffer VelOut
 {
-    vec4 VelocityOut [];
+    vec4 VelocityOut[];
 };
 
 void main()
 {
-    uvec3 nParticles = gl_NumWorkGroups * gl_WorkGroupSize;
-    uint idx = gl_GlobalInvocationID.y * nParticles.x + gl_GlobalInvocationID.x;
+    uvec3 NumParticles = gl_NumWorkGroups * gl_WorkGroupSize;
+    uint Index = gl_GlobalInvocationID.y * NumParticles.x + gl_GlobalInvocationID.x;
 
-    vec3 p = vec3(PositionIn[idx]);
-    vec3 v = vec3(VelocityIn[idx]), r;
+    vec3 p = vec3(PositionIn[Index]);
+
+    // Pin a few of the top verts
+    if (gl_GlobalInvocationID.y == NumParticles.y - 1 &&
+        (gl_GlobalInvocationID.x == 0 ||
+         gl_GlobalInvocationID.x == NumParticles.x / 4 ||
+         gl_GlobalInvocationID.x == NumParticles.x * 2 / 4 ||
+         gl_GlobalInvocationID.x == NumParticles.x * 3 / 4 ||
+         gl_GlobalInvocationID.x == NumParticles.x - 1))
+    {      
+        PositionOut[Index] = vec4(p, 1.0);
+        return;
+    }
+
+    vec3 v; 
 
     // Start with gravitational acceleration and add the spring
-    // forces from each neighbor
-    vec3 force = Gravity * ParticleMass;
+    // Forces from each neighbor
+    vec3 Force = Gravity * ParticleMass;
 
     // Particle above
-    if (gl_GlobalInvocationID.y < nParticles.y - 1)
-    {
-        r = PositionIn[idx + nParticles.x].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthVert);
+    if (gl_GlobalInvocationID.y < NumParticles.y - 1) {
+        v = PositionIn[Index + NumParticles.x].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthVert);
     }
+
     // Below
-    if (gl_GlobalInvocationID.y > 0)
-    {
-        r = PositionIn[idx - nParticles.x].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthVert);
+    if (gl_GlobalInvocationID.y > 0) {
+        v = PositionIn[Index - NumParticles.x].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthVert);
     }
+
     // Left
-    if (gl_GlobalInvocationID.x > 0)
-    {
-        r = PositionIn[idx - 1].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthHoriz);
+    if (gl_GlobalInvocationID.x > 0) {
+        v = PositionIn[Index - 1].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthHoriz);
     }
+
     // Right
-    if (gl_GlobalInvocationID.x < nParticles.x - 1)
-    {
-        r = PositionIn[idx + 1].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthHoriz);
+    if (gl_GlobalInvocationID.x < NumParticles.x - 1) {
+        v = PositionIn[Index + 1].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthHoriz);
     }
 
     // Diagonals
     // Upper-left
-    if (gl_GlobalInvocationID.x > 0 && gl_GlobalInvocationID.y < nParticles.y - 1)
-    {
-        r = PositionIn[idx + nParticles.x - 1].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthDiag);
+    if (gl_GlobalInvocationID.x > 0 && gl_GlobalInvocationID.y < NumParticles.y - 1) {
+        v = PositionIn[Index + NumParticles.x - 1].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthDiag);
     }
+
     // Upper-right
-    if (gl_GlobalInvocationID.x < nParticles.x - 1 && gl_GlobalInvocationID.y < nParticles.y - 1)
-    {
-        r = PositionIn[idx + nParticles.x + 1].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthDiag);
+    if (gl_GlobalInvocationID.x < NumParticles.x - 1 && gl_GlobalInvocationID.y < NumParticles.y - 1) {
+        v = PositionIn[Index + NumParticles.x + 1].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthDiag);
     }
+
     // lower -left
-    if (gl_GlobalInvocationID.x > 0 && gl_GlobalInvocationID.y > 0)
-    {
-        r = PositionIn[idx - nParticles.x - 1].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthDiag);
+    if (gl_GlobalInvocationID.x > 0 && gl_GlobalInvocationID.y > 0) {
+        v = PositionIn[Index - NumParticles.x - 1].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthDiag);
     }
+
     // lower-right
-    if (gl_GlobalInvocationID.x < nParticles.x - 1 && gl_GlobalInvocationID.y > 0)
-    {
-        r = PositionIn[idx - nParticles.x + 1].xyz - p;
-        force += normalize(r) * SpringK * (length(r) - RestLengthDiag);
+    if (gl_GlobalInvocationID.x < NumParticles.x - 1 && gl_GlobalInvocationID.y > 0) {
+        v = PositionIn[Index - NumParticles.x + 1].xyz - p;
+        Force += normalize(v) * SpringK * (length(v) - RestLengthDiag);
     }
 
-    force += -DampingConst * v;
+    vec3 Vel = vec3(VelocityIn[Index]);
+    Force += -Vel * DampingConst;
 
-    // Apply simple Euler integrator
-    vec3 a = force * ParticleInvMass;
-    PositionOut[idx] = vec4(
-        p + v * DeltaT + 0.5 * a * DeltaT * DeltaT, 1.0);
-    VelocityOut[idx] = vec4(v + a * DeltaT, 0.0);
+    // Apply semi-implicit Euler integrator
+    vec3 a = Force * ParticleInvMass;    
+    VelocityOut[Index] = vec4(Vel + a * DeltaT, 0.0);
 
-    // Pin a few of the top verts
-    if (gl_GlobalInvocationID.y == nParticles.y - 1 &&
-        (gl_GlobalInvocationID.x == 0 ||
-         gl_GlobalInvocationID.x == nParticles.x / 4 ||
-         gl_GlobalInvocationID.x == nParticles.x * 2 / 4 ||
-         gl_GlobalInvocationID.x == nParticles.x * 3 / 4 ||
-         gl_GlobalInvocationID.x == nParticles.x - 1))
-    {
-        PositionOut[idx] = vec4(p, 1.0);
-        VelocityOut[idx] = vec4(0, 0, 0, 0);
-    }
+    Vel = vec3(VelocityIn[Index]);  // get the udpated value
+
+    PositionOut[Index] = vec4(p + Vel * DeltaT + 0.5 * a * DeltaT * DeltaT, 1.0);
 }
 
