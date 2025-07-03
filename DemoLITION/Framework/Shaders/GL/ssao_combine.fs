3b00cdc470dc435a27c5939bc6d9c0a627c99bb0
Etay Meiri <etay.meiri@gmail.com>
2025-07-04 02:01:37 +0300

    Cleanup

--- DemoLITION/Framework/Shaders/GL/ssao_combine.fs
+++ DemoLITION/Framework/Shaders/GL/ssao_combine.fs
@@ -3,21 +3,22 @@
 
 layout(location = 0) in vec2 TexCoords;
 
-layout(location = 0) out vec4 outColor;
+layout(location = 0) out vec4 FragColor;
 
 layout(binding = 0) uniform sampler2D texScene;
 layout(binding = 1) uniform sampler2D texSSAO;
 
 layout(std140, binding = 0) uniform SSAOParams
 {
-	float scale;
-	float bias;
+	float Scale;
+	float Bias;
 };
 
 void main()
 {
-	vec4 color = texture(texScene, TexCoords);
-	float ssao = clamp( texture(texSSAO, TexCoords).r + bias, 0.0, 1.0 );
+	vec4 Color = texture(texScene, TexCoords);
+	float SSAOFactor = texture(texSSAO, TexCoords).x;
+	SSAOFactor = clamp(SSAOFactor + Bias, 0.0, 1.0);
 
-	outColor = vec4(mix(color, color * ssao, scale).rgb, 1.0);
+	FragColor = vec4(mix(Color, Color * SSAOFactor, Scale).rgb, 1.0);
 }
