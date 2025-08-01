65b3241e2c06e7a95da655b3b415f39a786dad31
Etay Meiri <etay.meiri@gmail.com>
2025-08-01 17:00:32 +0300

    Added the tone mapping technique

---
+++ DemoLITION/Framework/Shaders/GL/tone_map.fs
@@ -0,0 +1,76 @@
+/*
+
+        Copyright 2025 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#version 420
+
+
+layout(location = 0) out vec4 FragColor;
+
+layout(binding = 0) uniform sampler2D gHDRSampler;
+
+uniform float AveLum;
+
+in vec2 TexCoords;
+
+// XYZ/RGB conversion matrices from:
+// http://www.brucelindbloom.com/index.html?Eqn_RGB_XYZ_Matrix.html
+
+uniform mat3 rgb2xyz = mat3( 
+  0.4124564, 0.2126729, 0.0193339,
+  0.3575761, 0.7151522, 0.1191920,
+  0.1804375, 0.0721750, 0.9503041 );
+
+uniform mat3 xyz2rgb = mat3(
+  3.2404542, -0.9692660, 0.0556434,
+  -1.5371385, 1.8760108, -0.2040259,
+  -0.4985314, 0.0415560, 1.0572252 );
+
+uniform float Exposure = 10.100717;//0.35;
+uniform float White = 0.928;
+uniform bool DoToneMap = true;
+
+
+
+void main()
+{
+    // Retrieve high-res color from texture
+    vec4 Color = texture(gHDRSampler, TexCoords);
+    
+    // Convert to XYZ
+    vec3 xyzCol = rgb2xyz * vec3(Color);
+
+    // Convert to xyY
+    float xyzSum = xyzCol.x + xyzCol.y + xyzCol.z;
+    vec3 xyYCol = vec3( xyzCol.x / xyzSum, xyzCol.y / xyzSum, xyzCol.y);
+
+    // Apply the tone mapping operation to the luminance (xyYCol.z or xyzCol.y)
+    float L = (Exposure * xyYCol.z) / AveLum;
+    L = (L * (1 + L / (White * White))) / (1 + L);
+
+    // Using the new luminance, convert back to XYZ
+    xyzCol.x = (L * xyYCol.x) / (xyYCol.y);
+    xyzCol.y = L;
+    xyzCol.z = (L * (1 - xyYCol.x - xyYCol.y))/xyYCol.y;
+
+    // Convert back to RGB and send to output buffer
+    if (DoToneMap) {
+        FragColor = vec4( xyz2rgb * xyzCol, 1.0);
+    } else {
+        FragColor = Color;
+    }
+}
---
+++ DemoLITION/Framework/Source/GL/gl_tone_map_technique.cpp
@@ -0,0 +1,65 @@
+/*
+    Copyright 2024 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "GL/gl_tone_map_technique.h"
+
+
+bool ToneMapTechnique::Init()
+{
+    if (!Technique::Init()) {
+        return false;
+    }
+
+    if (!AddShader(GL_VERTEX_SHADER, "Framework/Shaders/GL/full_screen_quad.vs")) {
+        return false;
+    }
+
+    if (!AddShader(GL_FRAGMENT_SHADER, "Framework/Shaders/GL/tone_map.fs")) {
+        return false;
+    }
+
+    if (!Finalize()) {
+        return false;
+    }
+
+    glGenVertexArrays(1, &m_dummyVAO);
+
+    GET_UNIFORM_AND_CHECK(m_avgLumLoc, "AveLum");
+    GET_UNIFORM_AND_CHECK(m_hdrSamplerLoc, "gHDRSampler");
+
+    return true;
+}
+
+
+void ToneMapTechnique::Render()
+{    
+    glBindVertexArray(m_dummyVAO);
+    glDrawArrays(GL_TRIANGLES, 0, 6);
+}
+
+
+void ToneMapTechnique::SetAverageLuminance(float AvgLum)
+{
+    glUniform1f(m_avgLumLoc, AvgLum);
+}
+
+
+void ToneMapTechnique::SetHDRSampler(unsigned int TextureUnit)
+{
+    glUniform1i(m_hdrSamplerLoc, TextureUnit);
+}
+
