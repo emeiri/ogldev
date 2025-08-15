de2e4ba00d1a3a2d96b9731eea286d259b00d3c9
Etay Meiri <etay.meiri@gmail.com>
2025-08-15 18:31:02 +0300

    cleanup

--- DemoLITION/Framework/Shaders/GL/hdr.cs
+++ DemoLITION/Framework/Shaders/GL/hdr.cs
@@ -1,13 +1,11 @@
-
-
 #version 460
 
 layout(local_size_x = 10, local_size_y = 10) in;
 
-layout(binding = 0) uniform sampler2D hdrTex;
+layout(binding = 0) uniform sampler2D HdrTex;
 
 layout(std430, binding = 1) buffer Result {
-    float tileLuminance[]; // One per workgroup
+    float TileLuminance[]; // One per workgroup
 };
 
 shared float localLum[gl_WorkGroupSize.x * gl_WorkGroupSize.y]; 
@@ -15,18 +13,11 @@ shared float localLum[gl_WorkGroupSize.x * gl_WorkGroupSize.y];
 void main() 
 {
     ivec2 texCoord = ivec2(gl_GlobalInvocationID.xy);
-    vec3 hdr = texelFetch(hdrTex, texCoord, 0).rgb;
+    vec3 hdr = texelFetch(HdrTex, texCoord, 0).rgb;
+
     float lum = dot(hdr, vec3(0.2126, 0.7152, 0.0722));
 
-    //float brightThreshold = 2.5;
-  //  if (lum < brightThreshold) {
-        //FragLum = log(luminance + 0.0001);
-        localLum[gl_LocalInvocationIndex] = log(lum + 0.0001);
-   // }
-  //  else
-  //  {
-   //     localLum[gl_LocalInvocationIndex] = 0.0;
-  //  }
+    localLum[gl_LocalInvocationIndex] = log(lum + 0.0001);
     
     barrier();
 
@@ -41,13 +32,6 @@ void main()
             Sum += localLum[i];
         }
 
-        // for tiled tone mapping
-        // float avgLogLum = sum / 100.0f;
-        // float Exposure = 0.18f / exp(avgLogLum);
-        // Exposure = clamp(Exposure, 0.001f, 1.0f);		
-        // tileLuminance[gl_WorkGroupID.y * numGroupsX + gl_WorkGroupID.x] = Exposure;
-		
-		// regular
-        tileLuminance[gl_WorkGroupID.y * gl_NumWorkGroups.x + gl_WorkGroupID.x] = Sum;
+        TileLuminance[gl_WorkGroupID.y * gl_NumWorkGroups.x + gl_WorkGroupID.x] = Sum;
     }
-}// Compute shader
+}
