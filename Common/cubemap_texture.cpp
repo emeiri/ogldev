222a4c00835a8d75bae36bfaf7b8c101ef20ed6f
Etay Meiri <etay.meiri@gmail.com>
2025-10-03 01:05:23 +0300

    Removed incorrect call to gen mipmaps

--- Common/cubemap_texture.cpp
+++ Common/cubemap_texture.cpp
@@ -144,9 +144,7 @@ void CubemapTexture::LoadKTX(const std::string& Filename)
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
     glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);
-    
-    glGenerateMipmap(GL_TEXTURE_CUBE_MAP);
+    glTexParameteri(GL_TEXTURE_CUBE_MAP, GL_TEXTURE_WRAP_R, GL_CLAMP_TO_EDGE);   
 }
 
 
