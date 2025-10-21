4a8e566b592bdb6b4baf3e5112412fe1d01a6b87
Etay Meiri <etay.meiri@gmail.com>
2025-10-21 09:00:01 +0300

    Linux update scripts

---
+++ install_requirements_mint.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Should work on Linux Mint 21.1
+
+apt install g++ make freeglut3 freeglut3-dev libglew-dev libglew2.2 libmagick++-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer1d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
--- install_requirements_ubuntu.sh
+++ install_requirements_ubuntu.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-apt install g++ make freeglut3 freeglut3-dev libglew-dev libglew2.2 libmagick++-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer1d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
+# Should work on Ubuntu 24.4
+
+apt install g++ make freeglut3-dev libglew-dev libglew2.2 libmagick++-dev libassimp-dev libglfw3 libglfw3-dev libmeshoptimizer-dev libmeshoptimizer2d vulkan-tools vulkan-validationlayers vulkan-validationlayers-dev glslang-dev glslang-tools
