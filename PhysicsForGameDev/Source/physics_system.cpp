f120051396ce25c63a5986dedeb1ad4afcd6c9c7
Etay Meiri <etay.meiri@gmail.com>
2025-11-09 22:15:11 +0200

    Fixed var name

--- PhysicsForGameDev/Source/physics_system.cpp
+++ PhysicsForGameDev/Source/physics_system.cpp
@@ -32,7 +32,7 @@ void System::Update(int DeltaTimeMillis)
 {
     float DeltaTime = DeltaTimeMillis / 1000.f;
 
-    for (int i = 0 ; i < m_numPointMasses ; i++) {
+    for (int i = 0 ; i < m_numActivePointMasses; i++) {
         m_pointMasses[i].Update(DeltaTime, m_pUpdateListener);
     }
 }
