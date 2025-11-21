c60507e3be9b7dbe05e9915cede15c735e730385
Etay Meiri <etay.meiri@gmail.com>
2025-11-21 22:54:21 +0200

    Fixed tutorial name

--- Vulkan/Tutorial26/tutorial26.cpp
+++ Vulkan/Tutorial26/tutorial26.cpp
@@ -16,7 +16,7 @@
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 	Vulkan For Beginners - 
-		Tutorial #26: Push constants
+		Tutorial #26: Descriptor Indexing
 */
 
 #include <array>
