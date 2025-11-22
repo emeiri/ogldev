6b33e5cc08b2dfc7d2b36693271b26c8edd7ef4e
Etay Meiri <etay.meiri@gmail.com>
2025-11-22 11:55:54 +0200

    Vulkan tutorial 27

---
+++ Vulkan/Tutorial27/test.vert
@@ -0,0 +1,77 @@
+/*
+
+        Copyright 2024 Etay Meiri
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+#version 460
+
+// Any change here must be reflected in NUM_FLOATS_IN_VERTEX_DATA!
+struct VertexData
+{
+    float pos_x, pos_y, pos_z;
+    float u0, v0;
+    float u1, v1;
+    float normal_x, normal_y, normal_z;
+    float tangent_x, tangent_y, tangent_z;
+    float bitangent_x, bitangent_y, bitangent_z;
+    float color_r, color_g, color_b, color_a;
+};
+
+const int NUM_FLOATS_IN_VERTEX_DATA = 20;
+const int FLOAT_SIZE_IN_BYTES = 4;
+
+layout (std430, set = 0, binding = 0) readonly buffer Vertices { VertexData v[]; } in_Vertices;
+
+layout (set = 0, binding = 1) readonly buffer Indices { int i[]; } in_Indices;
+
+layout (set = 0, binding = 2) readonly uniform UniformBuffer { mat4 WVP; } ubo;
+
+struct MetaData { 
+    uint MaterialIndex; 
+    uint IndexOffset; 
+    uint IndexCount; 
+    uint VertexOffset; 
+};
+
+layout(std430, set = 1, binding = 1) readonly buffer MetaSSBO { MetaData metas[]; } MetaBuf;
+
+layout(location = 0) out vec2 texCoord;
+layout(location = 1) flat out uint MaterialIndex;
+
+const int INDEX_SIZE_IN_BYTES = 4;
+
+void main() 
+{    
+    uint DrawId = uint(gl_BaseInstance);
+
+    MetaData md = MetaBuf.metas[DrawId];
+
+    MaterialIndex = md.MaterialIndex;
+
+    uint BaseIndex = md.IndexOffset / INDEX_SIZE_IN_BYTES;
+    int Index = in_Indices.i[BaseIndex + gl_VertexIndex];
+
+    uint BaseVertex = md.VertexOffset / (NUM_FLOATS_IN_VERTEX_DATA * FLOAT_SIZE_IN_BYTES);
+    VertexData vtx = in_Vertices.v[BaseVertex + Index];
+
+    vec3 pos = vec3(vtx.pos_x, vtx.pos_y, vtx.pos_z);
+
+    gl_Position = ubo.WVP * vec4(pos, 1.0);
+    
+    texCoord = vec2(vtx.u0, vtx.v0);
+}
+
---
+++ Vulkan/VulkanCore/Include/ogldev_vulkan_graphics_pipeline_v4.h
@@ -0,0 +1,78 @@
+/*
+		Copyright 2024 Etay Meiri
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "ogldev_types.h"
+#include "ogldev_vulkan_core.h"
+#include "Int/model_desc.h"
+#include "Int/pipeline_desc.h"
+
+namespace OgldevVK {
+
+// This version support descriptor indexing
+class GraphicsPipelineV4 {
+
+public:
+
+	GraphicsPipelineV4(const PipelineDesc& pd);
+
+	~GraphicsPipelineV4();
+
+	void Bind(VkCommandBuffer CmdBuf);
+
+	void AllocateDescriptorSets(int NumSubmeshes,
+								std::vector< std::vector<VkDescriptorSet> >& DescriptorSets,
+								VkDescriptorSet& TexturesDescriptorSet);
+
+	void UpdateDescriptorSets(const ModelDesc& ModelDesc,
+							  std::vector<std::vector<VkDescriptorSet>>& DescriptorSets,
+							  VkDescriptorSet& TexturesDescriptorSet);
+
+	VkPipelineLayout GetPipelineLayout() const { return m_pipelineLayout; }
+
+private:
+
+	void InitCommon(GLFWwindow* pWindow, VkRenderPass RenderPass, 
+					VkShaderModule vs, VkShaderModule fs,
+					VkFormat ColorFormat, VkFormat DepthFormat, VkCompareOp DepthCompareOp);
+
+	void AllocateDescriptorSetsInternal(int NumSubmeshes, 
+		                                std::vector< std::vector<VkDescriptorSet> >& DescriptorSets);
+	void AllocateTextureDescriptorSet(VkDescriptorSet& TexturesDescriptorSet);
+	void CreateDescriptorPool(u32 TextureCount, u32 UniformBufferCount, u32 StorageBufferCount,	u32 MaxSets);
+	void CreateDescriptorSetLayout(bool IsVB, bool IsIB, bool IsUniform);
+
+	void CreateDescriptorPoolTextures(int MaxSets);
+	void CreateDescriptorSetLayoutTextures();
+
+	void UpdateTexturesDescriptorSet(const ModelDesc& ModelDesc, VkDescriptorSet& TexturesDescriptorSet);
+
+	VkDevice m_device = VK_NULL_HANDLE;
+	VkPipeline m_pipeline = VK_NULL_HANDLE;
+	VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+	VkDescriptorPool m_descriptorPool = VK_NULL_HANDLE;
+	VkDescriptorSetLayout m_descriptorSetLayout = VK_NULL_HANDLE;
+	VkDescriptorSetLayout m_descriptorSetLayoutTextures = VK_NULL_HANDLE;
+	int m_numImages = 0;
+};
+
+}
