ecfbca5c03d64b87673eb4cf88d2e79e01bf7118
Etay Meiri <etay.meiri@gmail.com>
2025-12-05 09:28:18 +0200

    Fixed tutorial name

--- PhysicsForGameDev/Tutorial01_ParticleDynamics/tutorial01.cpp
+++ PhysicsForGameDev/Tutorial01_ParticleDynamics/tutorial01.cpp
@@ -15,7 +15,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-    Physics Tutorial 01 - 1D Kinematics
+    Physics Tutorial 01 - Particle Dynamics
 */
 
 #include <stdio.h>
