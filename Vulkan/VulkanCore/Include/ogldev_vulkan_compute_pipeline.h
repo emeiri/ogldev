d06139eb8e03057527132fabf55b3fd7a62ed9cf
Etay Meiri <etay.meiri@gmail.com>
2026-01-09 13:26:51 +0200

    Added ComputePipeline

---
+++ Vulkan/VulkanCore/Include/ogldev_vulkan_compute_pipeline.h
@@ -0,0 +1,67 @@
+/*
+		Copyright 2026 Etay Meiri
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+#include <vulkan/vulkan.h>
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include "ogldev_types.h"
+#include "ogldev_vulkan_core.h"
+#include "Int/model_desc.h"
+#include "Int/pipeline_desc.h"
+
+namespace OgldevVK {
+
+
+class ComputePipeline {
+
+public:
+
+	ComputePipeline(const ComputePipelineDesc& pd);
+
+	~ComputePipeline();
+
+	void Bind(VkCommandBuffer CmdBuf);
+
+	void AllocateDescriptorSets(std::vector<VkDescriptorSet>& DescriptorSets);
+
+	void UpdateDescriptorSets(const ModelDesc& ModelDesc,
+							  std::vector<VkDescriptorSet>& DescriptorSets);
+
+	VkPipelineLayout GetPipelineLayout() const { return m_pipelineLayout; }
+
+private:
+
+	void InitCommon(GLFWwindow* pWindow, VkRenderPass RenderPass, 
+					VkShaderModule vs, VkShaderModule fs,
+					VkFormat ColorFormat, VkFormat DepthFormat, VkCompareOp DepthCompareOp);
+
+	void AllocateDescriptorSetsInternal(std::vector<VkDescriptorSet>& DescriptorSets);
+	void CreateDescriptorPool(u32 TextureCount, u32 UniformBufferCount, u32 StorageBufferCount,	u32 MaxSets);
+	void CreateDescriptorSetLayout(bool IsVB, bool IsIB, bool IsTex, bool IsUniform);
+
+	VkDevice m_device = VK_NULL_HANDLE;
+	VkPipeline m_pipeline = VK_NULL_HANDLE;
+	VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+	VkDescriptorPool m_descriptorPool = VK_NULL_HANDLE;
+	VkDescriptorSetLayout m_descriptorSetLayout = VK_NULL_HANDLE;
+	int m_numImages = 0;
+};
+
+}
---
+++ Vulkan/VulkanCore/Source/compute_pipeline.cpp
@@ -0,0 +1,51 @@
+/*
+		Copyright 2026 Etay Meiri
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU General Public License as published by
+	the Free Software Foundation, either version 3 of the License, or
+	(at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "ogldev_vulkan_compute_pipeline.h"
+
+namespace OgldevVK {
+
+ComputePipeline::ComputePipeline(const ComputePipelineDesc& pd)
+{
+	m_device = pd.Device;
+
+//	CreateDescriptorSetLayout(pd.IsVB, pd.IsIB, pd.IsTex2D, pd.IsUniform);
+
+	VkPipelineShaderStageCreateInfo ShaderStageCreateInfo = {
+		.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+		.pNext = NULL,
+		.flags = 0,
+		.stage = VK_SHADER_STAGE_COMPUTE_BIT,
+		.module = pd.cs,
+		.pName = "main",
+		.pSpecializationInfo = NULL
+	};
+
+	const VkComputePipelineCreateInfo ci = {
+		.sType = VK_STRUCTURE_TYPE_COMPUTE_PIPELINE_CREATE_INFO,
+		.flags = 0,
+		.stage = ShaderStageCreateInfo,
+		.layout = m_pipelineLayout,
+		.basePipelineHandle = VK_NULL_HANDLE,
+		.basePipelineIndex = -1,
+	};
+
+	VkResult res = vkCreateComputePipelines(m_device, NULL, 1, &ci, NULL, &m_pipeline);
+	CHECK_VK_RESULT(res, "vkCreateComputePipelines\n");
+}
+
+}
