525b2b16a8698dede84d30e1e71f6eef6412a6e4
Etay Meiri <etay.meiri@gmail.com>
2026-01-10 16:33:56 +0200

    Started creating the PipelineProgram

--- Vulkan/Tutorial28/tutorial28.cpp
+++ Vulkan/Tutorial28/tutorial28.cpp
@@ -65,228 +65,270 @@ struct UniformData {
 #endif
 
 
-static int frame = 0;
-
-VkDescriptorSetLayout CreateDescSetLayout(OgldevVK::VulkanCore& vkCore)
-{
-	VkDescriptorSetLayoutBinding Binding = {
-		.binding = 2,
-		.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-		.descriptorCount = 1,
-		.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT,
-		.pImmutableSamplers = NULL
-	};
-
-	return vkCore.CreateDescSetLayout({ Binding });
-}
+class PipelineProgram {
+public:
+	PipelineProgram() {}
 
 
-void AllocDescSets(VkDevice Device,
-				   int DescCount,
-				   std::vector<VkDescriptorSet>& DescriptorSets, 
-			  	   VkDescriptorSetLayout DescSetLayout,
-				   VkDescriptorPool DescPool)
-{
-	std::vector<VkDescriptorSetLayout> Layouts(DescCount, DescSetLayout);
+	void Init(OgldevVK::VulkanCore& vkCore, VkDescriptorPool DescPool, VkShaderModule vs, VkShaderModule fs)
+	{
+		m_descPool = DescPool;
 
-	VkDescriptorSetAllocateInfo AllocInfo = {
-		.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
-		.pNext = NULL,
-		.descriptorPool = DescPool,
-		.descriptorSetCount = (u32)Layouts.size(),
-		.pSetLayouts = Layouts.data()
-	};
-
-	DescriptorSets.resize(DescCount);
-
-	VkResult res = vkAllocateDescriptorSets(Device, &AllocInfo, DescriptorSets.data());
-	CHECK_VK_RESULT(res, "vkAllocateDescriptorSets");
-}
-
-
-void UpdateDescSets(VkDevice Device, std::vector<VkDescriptorSet>& DescriptorSets, const OgldevVK::VulkanTexture& Tex)
-{
-	std::vector<VkWriteDescriptorSet> WriteDescriptorSet(DescriptorSets.size());
-
-	VkDescriptorImageInfo ImageInfo = {
-		.sampler = Tex.m_sampler,
-		.imageView = Tex.m_view,
-		.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL
-	};
-
-	for (int i = 0; i < (int)DescriptorSets.size(); i++) {
-		VkDescriptorSet& DstSet = DescriptorSets[i];
-
-		WriteDescriptorSet[i] = {
-			.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
-			.pNext = NULL,
-			.dstSet = DstSet,
-			.dstBinding = 2,
-			.dstArrayElement = 0,
-			.descriptorCount = 1,
-			.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
-			.pImageInfo = &ImageInfo,
-			.pBufferInfo = NULL,
-			.pTexelBufferView = NULL
-		};
+		m_device = vkCore.GetDevice();
+		
+		m_descSetLayout = CreateDescSetLayout(vkCore);
+		
+		m_pipeline = CreatePipeline(vkCore.GetWindow(), vs, fs,
+						vkCore.GetSwapChainFormat(), vkCore.GetDepthFormat(), VK_COMPARE_OP_LESS);
 	}
 
-	vkUpdateDescriptorSets(Device, (u32)WriteDescriptorSet.size(), WriteDescriptorSet.data(), 0, NULL);
-}
+
+	void Destroy()
+	{
+		vkDestroyDescriptorSetLayout(m_device, m_descSetLayout, NULL);
+		vkDestroyPipelineLayout(m_device, m_pipelineLayout, NULL);		
+		vkDestroyPipeline(m_device, m_pipeline, NULL);
+	}
 
 
-VkPipeline CreatePipeline(VkDevice Device, GLFWwindow* pWindow, VkShaderModule vs, VkShaderModule fs,
-								  VkFormat ColorFormat, VkFormat DepthFormat, VkCompareOp DepthCompareOp,
-								  VkDescriptorSetLayout DescSetLayout, VkPipelineLayout& PipelineLayout)
-{
-	VkPipelineShaderStageCreateInfo ShaderStageCreateInfo[2] = {
-		{
-			.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-			.stage = VK_SHADER_STAGE_VERTEX_BIT,
-			.module = vs,
-			.pName = "main",
-		},
-		{
-			.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
-			.stage = VK_SHADER_STAGE_FRAGMENT_BIT,
-			.module = fs,
-			.pName = "main"
+	void Bind(VkCommandBuffer CmdBuf, VkDescriptorSet DescSet)
+	{
+		vkCmdBindPipeline(CmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
+
+		vkCmdBindDescriptorSets(CmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS,
+								m_pipelineLayout,
+								0,      // firstSet
+								1,      // descriptorSetCount						
+								&DescSet,
+								0,	    // dynamicOffsetCount
+								NULL);	// pDynamicOffsets
+	}
+
+	void AllocDescSets(int DescCount, std::vector<VkDescriptorSet>& DescriptorSets)
+	{
+		std::vector<VkDescriptorSetLayout> Layouts(DescCount, m_descSetLayout);
+
+		VkDescriptorSetAllocateInfo AllocInfo = {
+			.sType = VK_STRUCTURE_TYPE_DESCRIPTOR_SET_ALLOCATE_INFO,
+			.pNext = NULL,
+			.descriptorPool = m_descPool,
+			.descriptorSetCount = (u32)Layouts.size(),
+			.pSetLayouts = Layouts.data()
+		};
+
+		DescriptorSets.resize(DescCount);
+
+		VkResult res = vkAllocateDescriptorSets(m_device, &AllocInfo, DescriptorSets.data());
+		CHECK_VK_RESULT(res, "vkAllocateDescriptorSets");
+	}
+
+
+	void UpdateDescSets(std::vector<VkDescriptorSet>& DescriptorSets, const OgldevVK::VulkanTexture& Tex)
+	{
+		std::vector<VkWriteDescriptorSet> WriteDescriptorSet(DescriptorSets.size());
+
+		VkDescriptorImageInfo ImageInfo = {
+			.sampler = Tex.m_sampler,
+			.imageView = Tex.m_view,
+			.imageLayout = VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL
+		};
+
+		for (int i = 0; i < (int)DescriptorSets.size(); i++) {
+			VkDescriptorSet& DstSet = DescriptorSets[i];
+
+			WriteDescriptorSet[i] = {
+				.sType = VK_STRUCTURE_TYPE_WRITE_DESCRIPTOR_SET,
+				.pNext = NULL,
+				.dstSet = DstSet,
+				.dstBinding = 2,
+				.dstArrayElement = 0,
+				.descriptorCount = 1,
+				.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+				.pImageInfo = &ImageInfo,
+				.pBufferInfo = NULL,
+				.pTexelBufferView = NULL
+			};
 		}
-	};
 
-	VkPipelineVertexInputStateCreateInfo VertexInputInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO
-	};
-
-	VkPipelineInputAssemblyStateCreateInfo PipelineIACreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
-		.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
-		.primitiveRestartEnable = VK_FALSE
-	};
-
-	int WindowWidth, WindowHeight;
-	glfwGetWindowSize(pWindow, &WindowWidth, &WindowHeight);
-
-	VkViewport VP = {
-		.x = 0.0f,
-		.y = 0.0f,
-		.width = (float)WindowWidth,
-		.height = (float)WindowHeight,
-		.minDepth = 0.0f,
-		.maxDepth = 1.0f
-	};
-
-	VkRect2D Scissor = {
-		.offset = {
-			.x = 0,
-			.y = 0,
-		},
-		.extent = {
-			.width = (u32)WindowWidth,
-			.height = (u32)WindowHeight
-		}
-	};
-
-	VkPipelineViewportStateCreateInfo VPCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
-		.viewportCount = 1,
-		.pViewports = &VP,
-		.scissorCount = 1,
-		.pScissors = &Scissor
-	};
-
-	VkPipelineRasterizationStateCreateInfo RastCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
-		.polygonMode = VK_POLYGON_MODE_FILL,
-		.cullMode = VK_CULL_MODE_FRONT_BIT,
-		.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
-		.lineWidth = 1.0f
-	};
-
-	VkPipelineMultisampleStateCreateInfo PipelineMSCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
-		.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
-		.sampleShadingEnable = VK_FALSE,
-		.minSampleShading = 1.0f
-	};
-
-	VkPipelineDepthStencilStateCreateInfo DepthStencilState = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
-		.depthTestEnable = VK_TRUE,
-		.depthWriteEnable = VK_TRUE,
-		.depthCompareOp = DepthCompareOp,
-		.depthBoundsTestEnable = VK_FALSE,
-		.stencilTestEnable = VK_FALSE,
-		.front = {},
-		.back = {},
-		.minDepthBounds = 0.0f,
-		.maxDepthBounds = 1.0f
-	};
-
-	VkPipelineColorBlendAttachmentState BlendAttachState = {
-		.blendEnable = VK_FALSE,
-		.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT
-	};
-
-	VkPipelineColorBlendStateCreateInfo BlendCreateInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
-		.logicOpEnable = VK_FALSE,
-		.logicOp = VK_LOGIC_OP_COPY,
-		.attachmentCount = 1,
-		.pAttachments = &BlendAttachState
-	};
-
-	VkPipelineRenderingCreateInfo RenderingInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO_KHR,
-		.pNext = NULL,
-		.viewMask = 0,
-		.colorAttachmentCount = 1,
-		.pColorAttachmentFormats = &ColorFormat,
-		.depthAttachmentFormat = DepthFormat,
-		.stencilAttachmentFormat = VK_FORMAT_UNDEFINED
-	};
-
-	std::vector<VkDescriptorSetLayout> SetLayouts = { DescSetLayout };
-	VkPipelineLayoutCreateInfo LayoutInfo = {};
-
-	LayoutInfo = {
-		.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
-		.setLayoutCount = (u32)SetLayouts.size(),
-		.pSetLayouts = SetLayouts.data()
-	};
-
-	VkResult res = vkCreatePipelineLayout(Device, &LayoutInfo, NULL, &PipelineLayout);
-	CHECK_VK_RESULT(res, "vkCreatePipelineLayout\n");
-
-	VkGraphicsPipelineCreateInfo PipelineInfo = {
-		.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
-		.pNext = &RenderingInfo,
-		.stageCount = ARRAY_SIZE_IN_ELEMENTS(ShaderStageCreateInfo),
-		.pStages = &ShaderStageCreateInfo[0],
-		.pVertexInputState = &VertexInputInfo,
-		.pInputAssemblyState = &PipelineIACreateInfo,
-		.pViewportState = &VPCreateInfo,
-		.pRasterizationState = &RastCreateInfo,
-		.pMultisampleState = &PipelineMSCreateInfo,
-		.pDepthStencilState = &DepthStencilState,
-		.pColorBlendState = &BlendCreateInfo,
-		.layout = PipelineLayout,
-		.renderPass = VK_NULL_HANDLE,
-		.subpass = 0,
-		.basePipelineHandle = VK_NULL_HANDLE,
-		.basePipelineIndex = -1
-	};
-
-	VkPipeline Pipeline = VK_NULL_HANDLE;
-	res = vkCreateGraphicsPipelines(Device, VK_NULL_HANDLE, 1, &PipelineInfo, NULL, &Pipeline);
-	CHECK_VK_RESULT(res, "vkCreateGraphicsPipelineV3s\n");
-
-	printf("Graphics pipeline created\n");
-
-	return Pipeline;
-}
+		vkUpdateDescriptorSets(m_device, (u32)WriteDescriptorSet.size(), WriteDescriptorSet.data(), 0, NULL);
+	}
 
 
+private:
+
+	VkDescriptorSetLayout CreateDescSetLayout(OgldevVK::VulkanCore& vkCore)
+	{
+		VkDescriptorSetLayoutBinding Binding = {
+			.binding = 2,
+			.descriptorType = VK_DESCRIPTOR_TYPE_COMBINED_IMAGE_SAMPLER,
+			.descriptorCount = 1,
+			.stageFlags = VK_SHADER_STAGE_FRAGMENT_BIT,
+			.pImmutableSamplers = NULL
+		};
+
+		return vkCore.CreateDescSetLayout({ Binding });
+	}
+
+
+	VkPipeline CreatePipeline(GLFWwindow* pWindow, VkShaderModule vs, VkShaderModule fs,
+							  VkFormat ColorFormat, VkFormat DepthFormat, VkCompareOp DepthCompareOp)
+	{
+		VkPipelineShaderStageCreateInfo ShaderStageCreateInfo[2] = {
+			{
+				.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+				.stage = VK_SHADER_STAGE_VERTEX_BIT,
+				.module = vs,
+				.pName = "main",
+			},
+			{
+				.sType = VK_STRUCTURE_TYPE_PIPELINE_SHADER_STAGE_CREATE_INFO,
+				.stage = VK_SHADER_STAGE_FRAGMENT_BIT,
+				.module = fs,
+				.pName = "main"
+			}
+		};
+
+		VkPipelineVertexInputStateCreateInfo VertexInputInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO
+		};
+
+		VkPipelineInputAssemblyStateCreateInfo PipelineIACreateInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_INPUT_ASSEMBLY_STATE_CREATE_INFO,
+			.topology = VK_PRIMITIVE_TOPOLOGY_TRIANGLE_LIST,
+			.primitiveRestartEnable = VK_FALSE
+		};
+
+		int WindowWidth, WindowHeight;
+		glfwGetWindowSize(pWindow, &WindowWidth, &WindowHeight);
+
+		VkViewport VP = {
+			.x = 0.0f,
+			.y = 0.0f,
+			.width = (float)WindowWidth,
+			.height = (float)WindowHeight,
+			.minDepth = 0.0f,
+			.maxDepth = 1.0f
+		};
+
+		VkRect2D Scissor = {
+			.offset = {
+				.x = 0,
+				.y = 0,
+			},
+			.extent = {
+				.width = (u32)WindowWidth,
+				.height = (u32)WindowHeight
+			}
+		};
+
+		VkPipelineViewportStateCreateInfo VPCreateInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_VIEWPORT_STATE_CREATE_INFO,
+			.viewportCount = 1,
+			.pViewports = &VP,
+			.scissorCount = 1,
+			.pScissors = &Scissor
+		};
+
+		VkPipelineRasterizationStateCreateInfo RastCreateInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_RASTERIZATION_STATE_CREATE_INFO,
+			.polygonMode = VK_POLYGON_MODE_FILL,
+			.cullMode = VK_CULL_MODE_FRONT_BIT,
+			.frontFace = VK_FRONT_FACE_COUNTER_CLOCKWISE,
+			.lineWidth = 1.0f
+		};
+
+		VkPipelineMultisampleStateCreateInfo PipelineMSCreateInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_MULTISAMPLE_STATE_CREATE_INFO,
+			.rasterizationSamples = VK_SAMPLE_COUNT_1_BIT,
+			.sampleShadingEnable = VK_FALSE,
+			.minSampleShading = 1.0f
+		};
+
+		VkPipelineDepthStencilStateCreateInfo DepthStencilState = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_DEPTH_STENCIL_STATE_CREATE_INFO,
+			.depthTestEnable = VK_TRUE,
+			.depthWriteEnable = VK_TRUE,
+			.depthCompareOp = DepthCompareOp,
+			.depthBoundsTestEnable = VK_FALSE,
+			.stencilTestEnable = VK_FALSE,
+			.front = {},
+			.back = {},
+			.minDepthBounds = 0.0f,
+			.maxDepthBounds = 1.0f
+		};
+
+		VkPipelineColorBlendAttachmentState BlendAttachState = {
+			.blendEnable = VK_FALSE,
+			.colorWriteMask = VK_COLOR_COMPONENT_R_BIT | VK_COLOR_COMPONENT_G_BIT | VK_COLOR_COMPONENT_B_BIT | VK_COLOR_COMPONENT_A_BIT
+		};
+
+		VkPipelineColorBlendStateCreateInfo BlendCreateInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_COLOR_BLEND_STATE_CREATE_INFO,
+			.logicOpEnable = VK_FALSE,
+			.logicOp = VK_LOGIC_OP_COPY,
+			.attachmentCount = 1,
+			.pAttachments = &BlendAttachState
+		};
+
+		VkPipelineRenderingCreateInfo RenderingInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_RENDERING_CREATE_INFO_KHR,
+			.pNext = NULL,
+			.viewMask = 0,
+			.colorAttachmentCount = 1,
+			.pColorAttachmentFormats = &ColorFormat,
+			.depthAttachmentFormat = DepthFormat,
+			.stencilAttachmentFormat = VK_FORMAT_UNDEFINED
+		};
+
+		std::vector<VkDescriptorSetLayout> SetLayouts = { m_descSetLayout };
+		VkPipelineLayoutCreateInfo LayoutInfo = {};
+
+		LayoutInfo = {
+			.sType = VK_STRUCTURE_TYPE_PIPELINE_LAYOUT_CREATE_INFO,
+			.setLayoutCount = (u32)SetLayouts.size(),
+			.pSetLayouts = SetLayouts.data()
+		};
+
+		VkResult res = vkCreatePipelineLayout(m_device, &LayoutInfo, NULL, &m_pipelineLayout);
+		CHECK_VK_RESULT(res, "vkCreatePipelineLayout\n");
+
+		VkGraphicsPipelineCreateInfo PipelineInfo = {
+			.sType = VK_STRUCTURE_TYPE_GRAPHICS_PIPELINE_CREATE_INFO,
+			.pNext = &RenderingInfo,
+			.stageCount = ARRAY_SIZE_IN_ELEMENTS(ShaderStageCreateInfo),
+			.pStages = &ShaderStageCreateInfo[0],
+			.pVertexInputState = &VertexInputInfo,
+			.pInputAssemblyState = &PipelineIACreateInfo,
+			.pViewportState = &VPCreateInfo,
+			.pRasterizationState = &RastCreateInfo,
+			.pMultisampleState = &PipelineMSCreateInfo,
+			.pDepthStencilState = &DepthStencilState,
+			.pColorBlendState = &BlendCreateInfo,
+			.layout = m_pipelineLayout,
+			.renderPass = VK_NULL_HANDLE,
+			.subpass = 0,
+			.basePipelineHandle = VK_NULL_HANDLE,
+			.basePipelineIndex = -1
+		};
+
+		VkPipeline Pipeline = VK_NULL_HANDLE;
+		res = vkCreateGraphicsPipelines(m_device, VK_NULL_HANDLE, 1, &PipelineInfo, NULL, &Pipeline);
+		CHECK_VK_RESULT(res, "vkCreateGraphicsPipelineV3s\n");
+
+		printf("Graphics pipeline created\n");
+
+		return Pipeline;
+	}
+
+	VkDevice m_device = VK_NULL_HANDLE;
+	VkDescriptorSetLayout m_descSetLayout = VK_NULL_HANDLE;
+	VkDescriptorPool m_descPool = VK_NULL_HANDLE;
+	VkPipeline m_pipeline = VK_NULL_HANDLE;
+	VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+};
+
+
+static int frame = 0;
 
 class VulkanApp : public OgldevVK::GLFWCallbacks
 {
@@ -309,6 +351,9 @@ public:
 		vkDestroyShaderModule(m_device, m_cs, NULL);
 
 		delete m_pComputePipeline;
+
+		m_fsQuadProgram.Destroy();
+		vkDestroyDescriptorPool(m_device, m_descPool, NULL);
 			
 		m_csOutput.Destroy(m_device);
 
@@ -333,12 +378,8 @@ public:
 		VkFormat Format = VK_FORMAT_R8G8B8A8_UNORM;
 
 		m_vkCore.CreateTexture(m_csOutput, CS_OUTPUT_WIDTH, CS_OUTPUT_HEIGHT, Usage, Format);
-
-		m_descSetLayout = CreateDescSetLayout(m_vkCore);
+		
 		m_descPool = m_vkCore.CreateDescPool(1, 0, 0, m_numImages);
-		AllocDescSets(m_device, m_numImages, m_descSets, m_descSetLayout, m_descPool);
-		UpdateDescSets(m_device, m_descSets, m_csOutput);
-
 
 		CreatePipelines();
 		CreateCommandBuffers();
@@ -521,9 +562,9 @@ private:
 
 	void CreateGraphicsPipeline()
 	{
-		m_pipeline = CreatePipeline(m_device, m_pWindow, m_vs, m_fs,
-			m_vkCore.GetSwapChainFormat(), m_vkCore.GetDepthFormat(), VK_COMPARE_OP_LESS,
-			m_descSetLayout, m_pipelineLayout);
+		m_fsQuadProgram.Init(m_vkCore, m_descPool, m_vs, m_fs);
+		m_fsQuadProgram.AllocDescSets(m_numImages, m_descSets);
+		m_fsQuadProgram.UpdateDescSets(m_descSets, m_csOutput);		
 	}
 
 
@@ -573,16 +614,8 @@ private:
 
 			BeginRendering(CmdBuf, i);
 		
-			vkCmdBindPipeline(CmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS, m_pipeline);
+			m_fsQuadProgram.Bind(CmdBuf, m_descSets[i]);
 
-			vkCmdBindDescriptorSets(CmdBuf, VK_PIPELINE_BIND_POINT_GRAPHICS,
-				m_pipelineLayout,
-				0,      // firstSet
-				1,      // descriptorSetCount						
-				&m_descSets[i],
-				0,	    // dynamicOffsetCount
-				NULL);	// pDynamicOffsets
-		
 			u32 VertexCount = 3;
 			u32 InstanceCount = 1;
 			u32 FirstVertex = 0;
@@ -721,12 +754,9 @@ private:
 	float m_scale = 0.1f;	
 	std::vector<OgldevVK::BufferAndMemory> m_ubos;
 	OgldevVK::VulkanTexture m_csOutput;
-
-	VkDescriptorSetLayout m_descSetLayout = VK_NULL_HANDLE;
+	PipelineProgram m_fsQuadProgram;
 	VkDescriptorPool m_descPool = VK_NULL_HANDLE;
-	std::vector<VkDescriptorSet> m_descSets;
-	VkPipeline m_pipeline = VK_NULL_HANDLE;
-	VkPipelineLayout m_pipelineLayout = VK_NULL_HANDLE;
+	std::vector<VkDescriptorSet> m_descSets;	
 };
 
 
