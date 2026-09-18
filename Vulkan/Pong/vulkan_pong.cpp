23a66b8eb1e3cf5924c8a8c6732ccc2869f639d5
Etay Meiri <etay.meiri@gmail.com>
2026-09-18 23:01:05 +0300

    Fixed paddle collision with wall

--- Vulkan/Pong/vulkan_pong.cpp
+++ Vulkan/Pong/vulkan_pong.cpp
@@ -33,10 +33,10 @@ public:
     VulkanPong(int WindowWidth, int WindowHeight) 
 		: m_renderer(WindowWidth, WindowHeight, NAME_OF_THE_GAME, this)
     {
-        m_config.WindowSize = { 20.0f, 20.0f };// { (float)WindowWidth, (float)WindowHeight };
+        m_config.WindowSize = { 20.0f, 19.0f };
         m_config.BallInitialVelocity = { -5.0f, 0.0f };
         m_config.BallSize = 0.75f;
-        m_config.BaseWindowPosition = { -10.0f, -10.0f };
+        m_config.BaseWindowPosition = { -10.0f, -9.5f };
         m_config.PaddleHeight = 4.0f;
         m_config.PaddleWidth = 1.0f;
         m_config.PaddleOffset = 0.5f;
