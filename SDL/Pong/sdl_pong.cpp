08a775c7845889d6bd6d5e18ebfffb6392188908
Etay Meiri <etay.meiri@gmail.com>
2026-09-20 16:47:31 +0300

    Created the SDLPong class

--- SDL/Pong/sdl_pong.cpp
+++ SDL/Pong/sdl_pong.cpp
@@ -34,12 +34,108 @@ private:
 };
 
 
-static SDL_Window* pWindow = NULL;
-static SDL_Renderer* pRenderer = NULL;
-static SDL_Clock GameClock;
-static GameConfig Config;
-static Pong Game;
-static PongInputState Input;
+class SDLPong {
+
+public:
+
+    SDLPong() = default;
+
+    bool Init()
+    {
+        if (!SDL_CreateWindowAndRenderer("Pong", (int)(m_config.WindowSize.x), (int)(m_config.WindowSize.y),
+            SDL_WINDOW_RESIZABLE, &m_pWindow, &m_pRenderer)) {
+            SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+            return false;
+        }
+
+        SDL_SetRenderLogicalPresentation(m_pRenderer, (int)(m_config.WindowSize.x),
+            (int)(m_config.WindowSize.y), SDL_LOGICAL_PRESENTATION_LETTERBOX);
+
+        SDL_SetRenderVSync(m_pRenderer, 1);
+        m_gameClock.Init();
+        m_game.Init(m_config);
+
+        return true;
+    }
+
+
+    SDL_AppResult HandleInput(const SDL_Event* pEvent)
+    {
+        if (pEvent->type == SDL_EVENT_QUIT) {
+            return SDL_APP_SUCCESS;
+        }
+
+        if (pEvent->type == SDL_EVENT_KEY_DOWN) {
+            switch (pEvent->key.scancode) {
+            case SDL_SCANCODE_ESCAPE:
+                SDL_Log("Escape key pressed, quitting");
+                return SDL_APP_SUCCESS;
+            case SDL_SCANCODE_R: m_input.PaddleLUp = true; break;
+            case SDL_SCANCODE_F: m_input.PaddleLDown = true; break;
+            case SDL_SCANCODE_O: m_input.PaddleRUp = true; break;
+            case SDL_SCANCODE_L: m_input.PaddleRDown = true; break;
+            default: break;
+            }
+        } else if (pEvent->type == SDL_EVENT_KEY_UP) {
+            switch (pEvent->key.scancode) {
+            case SDL_SCANCODE_R: m_input.PaddleLUp = false; break;
+            case SDL_SCANCODE_F: m_input.PaddleLDown = false; break;
+            case SDL_SCANCODE_O: m_input.PaddleRUp = false; break;
+            case SDL_SCANCODE_L: m_input.PaddleRDown = false; break;
+            default: break;
+            }
+        }
+
+        return SDL_APP_CONTINUE;
+    }
+
+
+    void UpdateAndRender()
+    {
+        float DeltaTime = m_gameClock.GetDeltaTime();
+
+        // Game updates utilizing the Event-Driven clean input state flags
+        m_game.Update(m_input.PaddleLUp, m_input.PaddleLDown, m_input.PaddleRUp, m_input.PaddleRDown, DeltaTime);
+
+        RenderFrame();
+    }
+
+private:
+
+    void RenderFrame()
+    {
+        // Draw background color
+        SDL_SetRenderDrawColor(m_pRenderer, 16, 16, 16, 255);
+        SDL_RenderClear(m_pRenderer);
+
+        // Set entity drawing color to white
+        SDL_SetRenderDrawColor(m_pRenderer, 255, 255, 255, 255);
+
+        Rect PaddleLRect, PaddleRRect, BallRect;
+        m_game.GetRects(BallRect, PaddleLRect, PaddleRRect);
+
+        SDL_FRect PaddleRect{ PaddleLRect.x, PaddleLRect.y, PaddleLRect.w, PaddleLRect.h };
+        SDL_RenderFillRect(m_pRenderer, &PaddleRect);
+
+        PaddleRect = { PaddleRRect.x, PaddleRRect.y, PaddleRRect.w, PaddleRRect.h };
+        SDL_RenderFillRect(m_pRenderer, &PaddleRect);
+
+        SDL_FRect BallRectF{ BallRect.x, BallRect.y, BallRect.w, BallRect.h };
+        SDL_RenderFillRect(m_pRenderer, &BallRectF);
+        SDL_RenderPresent(m_pRenderer);
+    }
+
+    SDL_Window* m_pWindow = NULL;
+    SDL_Renderer* m_pRenderer = NULL;
+    SDL_Clock m_gameClock;
+    GameConfig m_config;
+    Pong m_game;
+    PongInputState m_input;
+};
+
+
+SDLPong g_sdlPong;
+
 
 /* This function runs once at startup. */
 SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
@@ -51,20 +147,12 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
         return SDL_APP_FAILURE;
     }
 
-    SDL_Log("SDL initialized");
-
-    if (!SDL_CreateWindowAndRenderer("Pong", (int)(Config.WindowSize.x), (int)(Config.WindowSize.y),
-                                     SDL_WINDOW_RESIZABLE, &pWindow, &pRenderer)) {
-        SDL_Log("Couldn't create window/renderer: %s", SDL_GetError());
+    if (!g_sdlPong.Init()) {
+        SDL_Log("Failed to initialize SDL Pong");
         return SDL_APP_FAILURE;
     }
 
-    SDL_SetRenderLogicalPresentation(pRenderer, (int)(Config.WindowSize.x),
-                                    (int)(Config.WindowSize.y), SDL_LOGICAL_PRESENTATION_LETTERBOX);
-
-    SDL_SetRenderVSync(pRenderer, 1);
-    GameClock.Init();
-    Game.Init(Config);
+    SDL_Log("SDL initialized");
 
     return SDL_APP_CONTINUE;
 }
@@ -72,67 +160,14 @@ SDL_AppResult SDL_AppInit(void** appstate, int argc, char* argv[])
 /* This function runs when a new event occurs (Safely handles Input Flags) */
 SDL_AppResult SDL_AppEvent(void* appstate, SDL_Event* event)
 {
-    if (event->type == SDL_EVENT_QUIT) {
-        return SDL_APP_SUCCESS;
-    }
-
-    if (event->type == SDL_EVENT_KEY_DOWN) {
-        switch (event->key.scancode) {
-        case SDL_SCANCODE_ESCAPE:
-            SDL_Log("Escape key pressed, quitting");
-            return SDL_APP_SUCCESS;
-        case SDL_SCANCODE_W: Input.PaddleLUp = true; break;
-        case SDL_SCANCODE_S: Input.PaddleLDown = true; break;
-        case SDL_SCANCODE_O: Input.PaddleRUp = true; break;
-        case SDL_SCANCODE_L: Input.PaddleRDown = true; break;
-        default: break;
-        }
-    } else if (event->type == SDL_EVENT_KEY_UP) {
-        switch (event->key.scancode) {
-        case SDL_SCANCODE_W: Input.PaddleLUp = false; break;
-        case SDL_SCANCODE_S: Input.PaddleLDown = false; break;
-        case SDL_SCANCODE_O: Input.PaddleRUp = false; break;
-        case SDL_SCANCODE_L: Input.PaddleRDown = false; break;
-        default: break;
-        }
-    }
-
-    return SDL_APP_CONTINUE;
+    return g_sdlPong.HandleInput(event);
 }
 
-static void RenderGame()
-{
-    // Draw background color
-    SDL_SetRenderDrawColor(pRenderer, 16, 16, 16, 255);
-    SDL_RenderClear(pRenderer);
-
-    // Set entity drawing color to white
-    SDL_SetRenderDrawColor(pRenderer, 255, 255, 255, 255);
-
-    Rect PaddleLRect, PaddleRRect, BallRect;
-    Game.GetRects(BallRect, PaddleLRect, PaddleRRect);
-
-    SDL_FRect PaddleRect { PaddleLRect.x, PaddleLRect.y, PaddleLRect.w, PaddleLRect.h };
-    SDL_RenderFillRect(pRenderer, &PaddleRect);
-
-    PaddleRect = { PaddleRRect.x, PaddleRRect.y, PaddleRRect.w, PaddleRRect.h };
-    SDL_RenderFillRect(pRenderer, &PaddleRect);
-
-    SDL_FRect BallRectF { BallRect.x, BallRect.y, BallRect.w, BallRect.h };
-    SDL_RenderFillRect(pRenderer, &BallRectF);
-    SDL_RenderPresent(pRenderer);
-}
 
 /* This function runs once per frame. */
 SDL_AppResult SDL_AppIterate(void* appstate)
 {
-    float DeltaTime = GameClock.GetDeltaTime();
-
-    // Game updates utilizing the Event-Driven clean input state flags
-    Game.Update(Input.PaddleLUp, Input.PaddleLDown, Input.PaddleRUp, Input.PaddleRDown, DeltaTime);
-
-    RenderGame();
-
+    g_sdlPong.UpdateAndRender();
     return SDL_APP_CONTINUE;
 }
 
